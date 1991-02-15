7526f03358fb509e16cf86aba371211aa6505626
Rich Neitzel <rich@ucar.edu>
1991-02-15 15:50:02 +0000

    Initial revision
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@611 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

---
+++ vx-eldora/vx/c++/builtin/ctor_dummy.s
@@ -0,0 +1,34 @@
+|
+|	$Id$
+|
+|	Module:   	 ctor_dummy.s
+|	Original Author: Richard E. K. Neitzel
+|       Copywrited by the National Center for Atmospheric Research
+|	Date:		 $Date$
+|
+| revision history
+| ----------------
+| $Log$
+|
+| calling convention:
+|
+| description:
+|        This file makes a dummy CTOR/DTOR list that allows
+| builtin++.o to load if there are no globals in use.
+|
+	.data
+Id:
+	.ascii "$Date$ $RCSfile$ $Revision$"
+
+#NO_APP
+.data
+.globl ___CTOR_LIST__
+___CTOR_LIST__:
+	.long	0
+	.long	0
+.data
+.globl ___DTOR_LIST__
+___DTOR_LIST__:
+	.long	0
+	.long	0
+	.long	0
