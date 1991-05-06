6ac0455c43240bd4a743b9dcf4bbd8a3832d122a
Rich Neitzel <rich@ucar.edu>
1991-05-06 15:19:46 +0000

    Changed from 8x16 cells to 12x24 cells. Moved size constants to public view.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@641 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/display/clock/Clock.hh
+++ vx-eldora/display/clock/Clock.hh
@@ -9,6 +9,9 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.2  1991/05/03  18:06:58  thor
+ * Added commnets, inlined simpler methods.
+ *
  * Revision 1.1  1991/05/03  15:19:37  thor
  * Initial revision
  *
@@ -20,15 +23,15 @@
  * based on user commands - it does not keep time. There is a default
  * set of characters 0-9 & :. You may replace them but they must be
  * defined as follows:
- *    An 8x16 cell, a 1 bit means draw in the foreground color, 0 the
- * background color. Encoding is a byte per line, so you must pass a
- * pointer to a 16 byte array.
+ *    An 16x24 cell, a 1 bit means draw in the foreground color, 0 the
+ * background color. Encoding is 14 bits per line, so you must pass a
+ * pointer to a 24 short array. The top 4 bits of the short are unused.
  *
  * The memory organization is important and is currently very AGV
- * dependent. The displayed window takes up 100x24 pixels. The video
- * memory space for the characters takes 88x16 pixels. This is because
+ * dependent. The displayed window takes up 136x32 pixels. The video
+ * memory space for the characters takes 96x24 pixels. This is because
  * we leave a 4 pixel boarder and 4 pixels between cells. The data
- * space begins at 0,24 relative to the window's 0,0. 
+ * space begins at 0,32 relative to the window's 0,0. 
  *
  * Speed of updating is accomplished by bliting the character required
  * into the proper cell.
@@ -70,6 +73,9 @@
 
 #include "Window.hh"
 
+static const int CLKWIDTH = 12;
+static const int CLKHEIGHT = 24;
+
 class Clock : private Window {
   private:
     short lastHour;
@@ -104,7 +110,9 @@ class Clock : private Window {
 
     void SetDefPatterns(void);
 
-    void Pattern(Point *ptr, unsigned char *pattern);
+    void Pattern(Point *ptr, unsigned short *pattern);
+
+    ~Clock(void) { clear(); }
 };
 
 #endif /* INCClockhh */
