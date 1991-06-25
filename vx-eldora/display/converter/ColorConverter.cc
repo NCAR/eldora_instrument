95cb2d1989c0c2e5b76dbb7c68b45b4fb40ce4f6
Rich Neitzel <rich@ucar.edu>
1991-06-25 14:04:27 +0000

    Fixed step calculation for bins. Rewrote beam sizing routine to handle variable gate sizes.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@648 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/display/converter/ColorConverter.cc
+++ vx-eldora/display/converter/ColorConverter.cc
@@ -9,6 +9,9 @@
  * revision history
  * ----------------
  * $Log$
+ * Fixed step calculation for bins. Rewrote beam sizing routine to handle
+ * variable gate sizes.
+ *
  * Revision 1.2  1991/05/08  20:08:41  thor
  * Major change - dropped floating point, switched to table of levels.
  *
@@ -34,9 +37,6 @@ void ColorConverter::Reset(FAST int bins, float *max, float *min,
 			   FAST int *offsets, int nparams, FAST int nvalues)
 {
     nbins = bins;
-    --bins;			// Since we don't need maximum value
-				// in our table.
-
     for (FAST int i = 0; i < nvalues; i++)
     float fb = 0.0;		// Color offset.
 
@@ -45,25 +45,82 @@ void ColorConverter::Reset(FAST int bins, float *max, float *min,
 	  FAST short *ptr = (short *)malloc(sizeof(short) * bins);
 	  tbl[i] = ptr;
 
-	  float step = mx - mn;
+	  float step = (mx - mn) / (float)(bins - 1);
 
 	  for (FAST int j = 0; j < bins; j++)
-	    *ptr++ = (short)(mn + (j * step)); // Ignore roundoff errors.
+	    *ptr++ = (short)(mn + ((float)j * step)); // Ignore roundoff err.
 
 	  float mx = *max++;
 		*lkup++ = color;
 	    }
 	  fb += fbins;		// Bump up in color lut.
-void ColorConverter::SetBeamSize(FAST int ngates)
+int ColorConverter::SetBeamSize(FAST int nchanges, FAST short *cPts,
+				 FAST int *sizes)
 
-    float skip = (float)ngates / (float)(DISPLAYED_GATES);
-    
+    FAST short *cpts = cPts;
+    FAST int *sz = sizes;
+    FAST int ngates = 0;
+    FAST int maxSize = 0;
+
+    for (FAST int i = 0; i < nchanges; i++) // Compute total # of
+					    // gates & total length of beam.
+      {
+	  FAST int ng = *cpts++;
+	  ngates += ng;
+	  maxSize += ng * *sz++;
+      }
+
+    FAST float *fptr = (float *)malloc(ngates * sizeof(float));
+      {
+    if (fptr == NULL)
+      return(ERROR);
+      (float *)malloc(sizeof(float) * ngates);; // Allocate space for
+    cpts = cPts;
+    sz = sizes;
+						// temporary array to hold
+						// actual distances.
+    for (i = 0; i < nchanges; i++) // For each increment, compute gate ranges.
+    float dist = (float)cs.distToFirst;;
+      {
+	  FAST int gates = *cpts++;
+	  float size = (float)*sz++;
+	  for (FAST int j = 0; j < c; j++)
+	  for (FAST int k = 1; k <= gates; k++)
+	    *fp++ = size * k;
+
+		dist += width;
+
+    float inc = (float)maxSize / (float)(DISPLAYED_GATES);
+
+    FAST int index = 0;
+    FAST int dcells = pgates;
+
+    FAST int *ptr = gateIndex;
+    FAST int j = DISPLAYED_GATES;
 
     FAST int np = numOfParams;
-    // Index values are determined by picking the nearest gate to the
-    // displayed gate.
-    for (FAST int i = 0; i < ngates; i++)
-      *ptr++ = (int)((skip * (float)i) + .5);
+    for (i = 0; i < j; i++)	// For each displayed gate.
+
+	  float dist = inc * i;	// Target distance.
+	  FAST int index = 0;
+
+	  
+	  for (FAST int k = 0; k < ngates; k++)	// Find closest real gate.
+		if (newDiff >= oldDiff)	// If difference increased
+		newDiff = (float)(*fp++ - dist);
+		  {
+		if (newDiff >= oldDiff)
+		      ptr[pgates] = index + valueOffset[1];
+		      *ptr++ = index;
+		  index++;
+	    }
+
+	  if (*ptr < 0)		// Ran off end of beam.
+	    {
+
+    free((char *)fptr);
+
+    return(OK);
 			      FAST unsigned char *colors,
 			      FAST int index)
 void ColorConverter::GetPoint(FAST short *data, FAST DataPoint &dp,
@@ -89,7 +146,7 @@ void ColorConverter::GetPoint(FAST short *data, FAST DataPoint &dp,
 	  while (color < tsize)
 	    {
 		if (datum <= *ptr)
-		  break;
+		      break;
 		else
 		  {
 		      ptr++;
@@ -107,12 +164,13 @@ void ColorConverter::GetBeam(FAST short *data, FAST RadialData &rad)
     FAST unsigned char inc = 0;		 // Offset into color table.
     FAST int count = numOfValues;
     FAST unsigned char *colors = &rad.colors[0];
+    FAST int *offs = valueOffset;
 
     for (FAST int i = 0; i < count; i++, inc += bins)
 	    {
 	  FAST int j = DISPLAYED_GATES;
 	  FAST int *ptr = gateIndex;
-	  FAST int offset = valueOffset[i];
+	  FAST int offset = *offs++;
 
 	  for (FAST int k = 0; k < j; k++)
 	    {
