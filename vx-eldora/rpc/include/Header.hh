3cce7b1ff1e58c1e95036e604da96dbb3bf6f468
Rich Neitzel <rich@ucar.edu>
1991-09-11 16:27:09 +0000

    Added code to send RPC header.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@659 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/rpc/include/Header.hh
+++ vx-eldora/rpc/include/Header.hh
@@ -9,34 +9,45 @@
  * revision history
  * ----------------
  * $Log$
+ * Fixed radar descriptor functions to handle 2 descriptors & added error
+ * reporting for parameter functions.
  *
  * Revision 1.2  1991/09/11  16:27:09  thor
+ * Added code to send RPC header.
  *
- *        
+ * Revision 1.1  1991/09/05  16:41:28  thor
+ * Initial revision
+ *
+ *
+ *
+ * description:
+ *        The Header class provides a means of organizing the tape
  * header for passing via RPC. It also provides routines to
  * pack/unpack a real tape header tp/from the RPC form. Various
  * methods allow acces to the component parts of the tape header.
  * These will be of most use, since only the control system and the
  * recording system are interested in real tape headers.
  *
-#include "TapeHeader.h"
-
  */
 #ifndef INCHeaderhh
+#define INCHeaderhh
 
 #define OK_RPC
+
 extern "C" {
 #include "Header.h"
-    TAPEHEADER *th;
+#include "TapeHeader.h"
 
-    PARAMETER *params;
+    PARAMETER *params;		// Working copy of parameters.
 };
-#include <stream.h>
-    Header(void);
 
-    Header(TAPEHEADER *t);
+#ifdef UNIX
+#include <stream.h>
+#endif // UNIX
+
+class Header {
   private:
-    Header(void *t);
+    TAPEHEADER *th;		// Working copy of tape header.
 
     void Parameter(PARAMETER &param, int paramNum);
 
@@ -58,7 +69,11 @@ extern "C" {
 
     void Waveform(WAVEFORM &w);
     WAVEFORM *Waveform(void);
-    void *GetRealHeader(void);
+    int GetRealHeader(void **header);
+
+    INSITUDESC *Insitu(void);
+
+    TAPEHEADER *GetRpcHeader(void) { return(th); }
 
     int GetRealHeader(void *header);
 
