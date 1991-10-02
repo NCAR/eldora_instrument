f16fbeeb529ee1dc2bc2e86e5c2d4dc27c8687d3
Rich Neitzel <rich@ucar.edu>
1991-10-02 14:23:14 +0000

    Initial revision
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@669 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

---
+++ vx-eldora/vx/c++/Task/Task.cc
@@ -0,0 +1,166 @@
+/*  This looks like C, but it's really C++!!
+ *	$Id$
+ *
+ *	Module:		 Task.cc
+ *	Original Author: Richard E. K. Neitzel
+ *      Copywrited by the National Center for Atmospheric Research
+ *	Date:		 $Date$
+ *
+ * revision history
+ * ----------------
+ * $Log$
+ *
+ *
+ * description:
+ *        This file has methods for construction, some reporting
+ * methods and the flag methods.
+ *
+ */
+static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
+
+#include "Task.hh"
+
+Task::Task(FUNCPTR entry, FAST int *args, int argsize, int pri = 100,
+	   int stacksize = 3000, int options = VX_FP_TASK | VX_STDIO,
+	   int go = 1)
+{
+   
+    if (!go)
+      {
+	  start = entry;
+
+	  priority = pri;
+
+	  FAST int *in = args;
+	  FAST int *out = argv;
+
+	  *out++ = (int)this;	// We must know who we are!
+
+	  for (FAST int i = 1; i <= argsize; i++)
+	    *out++ = *in++;
+
+	  numArgs = argsize;
+
+	  stack = stacksize;
+	  
+	  opts = options;
+
+	  taskId = -1;		// Make sure things fail.
+      }
+    else
+      {
+	  taskId = taskSpawn("",pri,options,stacksize,entry,(int)this,args[0],
+			     args[1],args[2],args[3],args[4],args[5],args[6],
+			     args[7],args[8]);
+      }
+
+    flags = NULL;		// No flags by default.
+}
+
+int Task::Priority(void)
+{
+    int pri;
+
+    if (taskPriorityGet(taskId,&pri) == ERROR)
+      return(ERROR);
+
+    return(pri);
+}
+
+int Task::Options(void)
+{
+    int opt;
+
+    if (taskOptionsGet(taskId,&opt) == ERROR)
+      return(ERROR);
+
+    return(opt);
+}
+
+TASK_DESC *Task::InfoGet(void)
+{
+    FAST TASK_DESC *ptr = (TASK_DESC *)malloc(sizeof(TASK_DESC));
+
+    if (ptr == NULL)
+      return(ptr);
+
+    if (taskInfoGet(taskId,ptr) == ERROR)
+      return(NULL);
+
+    return(ptr);
+}
+
+char *Task::StatusString(void)
+{
+    FAST char *ptr = new char[20];
+
+    if (ptr == NULL)
+      return(ptr);
+
+    if (taskStatusString(taskId,ptr) == ERROR)
+      return(NULL);
+
+    return(ptr);
+}
+	
+int Task::Status(void)
+{
+    FAST WIND_TCB *tcb = taskTcb(taskId);
+
+    if (tcb == NULL)
+      return(-1);
+
+    return(tcb->status);
+}
+
+int Task::Go(void)
+{
+    return(taskId = taskSpawn("",priority,opts,stack,start,argv[0],argv[1],
+			      argv[2],argv[3],argv[4],argv[5],argv[6],argv[7],
+			      argv[8],argv[9]));
+}
+
+STATUS Task::FlagsInit(void)
+{
+    flags = new Flags();
+
+    if (flags == NULL)
+      return(ERROR);
+    
+    return(OK);
+}
+
+void Task::SetFlags(FAST unsigned int mask)
+{
+    if (flags == NULL)		// Protect ourself from pointer error.
+      return;
+
+    flags->set(mask);
+}
+
+void Task::ClearFlags(void)
+{
+    if (flags == NULL)		// Protect ourself from pointer error.
+      return;
+
+    flags->clear();
+}
+
+// The next two methods assume that no one would ever want to use all flags.
+
+unsigned int Task::WaitOnFlags(FAST unsigned int mask, FAST int type,
+			       FAST int tmo = WAIT_FOREVER)
+{
+    if (flags == NULL)		// Protect ourself from pointer error.
+      return(0xffffffff);
+
+    return(flags->wait(mask,type,tmo));
+}
+
+unsigned int Task::QueryFlags(void)
+{
+    if (flags == NULL)		// Protect ourself from pointer error.
+      return(0xffffffff);
+
+    return(flags->query());
+}
---
+++ vx-eldora/vx/c++/Task/Task.hh
@@ -0,0 +1,124 @@
+/*  This looks like C, but it's really C++!!
+ *	$Id$
+ *
+ *	Module:		 Task.hh
+ *	Original Author: Richard E. K. Neitzel
+ *      Copywrited by the National Center for Atmospheric Research
+ *	Date:		 $Date$
+ *
+ * revision history
+ * ----------------
+ * $Log$
+ *
+ *
+ * description:
+ *        This class provides an interface to the majority of VxWorks'
+ * task manipulation and reporting routines. It also provides a means
+ * of integrating local event flags simply. When the constructor is
+ * called a task may be immediately spawned or simply readied for
+ * later spawning. 
+ *
+ * An important note to remember is that each spawned task must have
+ * as the first arguement a reference to its Task. Thus the proper
+ * declaration  looks like this:
+ *
+ * int mytask(Task &self [rest of arg list]);
+ *
+ * This allows the task to use the same interface as outsiders.
+ *
+ * If FlagsInit() is called, the task can use local event flags. See
+ * the Flags class documentation for more explanation. Be aware that
+ * only the task should ever call WaitOnFlags(). If an outsider calls
+ * this routine, the outsider will block!
+ *
+ */
+#ifndef INCTaskhh
+#define INCTaskhh
+
+extern "C" {
+#include "taskLib.h"
+#include "sigLib.h"
+#include "memLib.h"
+#include "string.h"
+};
+
+#include "Flags.hh"
+
+class Task {
+  private:
+    int taskId;			// Guess.
+
+    Flags *flags;		// Local event flags, if any.
+
+    // These are used to stored needed variables if spawning is delayed.
+    FUNCPTR start;		// Entry point.
+    int *argv;			// Arguement vector.
+    int numArgs;		// Vector size.
+    int priority;		// Guess.
+    int stack;			// Stack size.
+    int opts;			// Task options.
+    
+  public:
+    Task(FUNCPTR entry, int *args, int argsize, int pri = 100,
+	 int stacksize = 3000, int options = VX_FP_TASK | VX_STDIO,
+	 int go = 1);
+
+    STATUS Delete(void) { return(taskDelete(taskId)); }
+
+    STATUS DeleteForce(void) { return(taskDeleteForce(taskId)); }
+
+    STATUS Terminate(void) { return(taskTerminate(taskId)); }
+
+    STATUS Suspend(void) { return(taskSuspend(taskId)); }
+
+    STATUS Resume(void) { return(taskResume(taskId)); }
+
+    STATUS Restart(void) { return(taskRestart(taskId)); }
+
+    int Priority(void);
+
+    STATUS Priority(FAST int pri) { return(taskPrioritySet(taskId,pri)); }
+
+    STATUS Options(FAST int mask, FAST int newOpt)
+      { return(taskOptionsSet(taskId, mask, newOpt)); }
+
+    int Options(void);
+
+    char *Name(void) { return(taskName(taskId)); }
+
+    int Tid(void) { return(taskId); }
+
+    BOOL IsReady(void) { return(taskIsReady(taskId)); }
+
+    BOOL IsSuspended(void) { return(taskIsSuspended(taskId)); }
+
+    TASK_DESC *InfoGet(void);
+
+    WIND_TCB *Tcb(void) { return(taskTcb(taskId)); }
+
+    char *StatusString(void);
+
+    int Status(void);
+
+    STATUS Exists(void) { return(taskIdVerify(taskId)); }
+
+    int Go(void);
+
+    STATUS SigRaise(int signal, int code = 0) 
+      { return(sigRaise(taskId,signal,code)); }
+
+    STATUS Kill(int signal) { return(SigRaise(signal)); }
+
+    STATUS FlagsInit(void);
+
+    void SetFlags(unsigned int mask);
+
+    void ClearFlags(void);
+
+    unsigned int WaitOnFlags(unsigned int mask, int type,
+			     int tmo = WAIT_FOREVER);
+
+    unsigned int QueryFlags(void);
+};
+
+#endif // INCTaskhh
