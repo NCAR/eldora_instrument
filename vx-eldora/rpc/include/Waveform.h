6598866e001f3f37a516803e6fbe10006abbe613
Rich Neitzel <rich@ucar.edu>
1991-10-09 15:25:28 +0000

    Removed unneeded wavecount items.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@672 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/rpc/include/Waveform.h
+++ vx-eldora/rpc/include/Waveform.h
@@ -9,6 +9,9 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.2  1991/09/11  16:29:24  thor
+ * Added wave_counts items.
+ *
  * Revision 1.1  1991/08/30  18:39:42  thor
  * Initial revision
  *
@@ -62,57 +65,20 @@ struct waveform_d {
 				/* are listed, nonzero => uniform */
 				/* gates. */
     short padding3;
-#ifndef OK_RPC
-    short gate_dist1<4096>;	/* Distance from radar to data cell #1 */
+    short gate_dist1[2];	/* Distance from radar to data cell #1 */
 				/* in 60 MHz counts. */
-    short gate_dist2<4096>;	/* Ditto. */
-    short gate_dist3<4096>;	/* Ditto. */
-    short gate_dist4<4096>;	/* Ditto. */
-    short gate_dist5<4096>;	/* Ditto. */
-#else
-    struct {
-	u_int gate_dist1_len;	/* This MUST equal the value in */
-				/* num_gates[n]. */
-	short *gate_dist1_val;
-    } gate_dist1;
-    struct {
-	u_int gate_dist2_len;
-	short *gate_dist2_val;
-    } gate_dist2;
-    struct {
-	u_int gate_dist3_len;
-	short *gate_dist3_val;
-    } gate_dist3;
-    struct {
-	u_int gate_dist4_len;
-	short *gate_dist4_val;
-    } gate_dist4;
-    struct {
-	u_int gate_dist5_len;
-	short *gate_dist5_val;
-    } gate_dist5;
-#endif /* OK_RPC */
-}; /* End of Structure */
-
-
-struct wave_counts {
-    int index;
-    int offset;
-    int count;
-    short counts[512];
+    short gate_dist2[2];	/* Ditto. */
+    short gate_dist3[2];	/* Ditto. */
+    short gate_dist4[2];	/* Ditto. */
+    short gate_dist5[2];	/* Ditto. */
 };
-
 
 #ifdef OK_RPC
 
 typedef struct waveform_d waveform_d;
 typedef struct waveform_d WAVEFORM;
 
-typedef struct wave_counts wave_counts;
-typedef struct wave_counts WAVECOUNTS;
-
 bool_t xdr_waveform_d(XDR *, WAVEFORM *);
-bool_t xdr_wave_counts(XDR *, WAVECOUNTS *);
 
 #endif /* OK_RPC */
 
