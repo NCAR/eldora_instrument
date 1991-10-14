c4ffd6905ab55f0384a8a37abf357a42275402b6
Rich Neitzel <rich@ucar.edu>
1991-10-14 19:20:39 +0000

    Added Task control items.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@685 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/display/ge/GeGlobal.hh
+++ vx-eldora/display/ge/GeGlobal.hh
@@ -9,6 +9,9 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.1  1991/10/01  16:15:15  thor
+ * Initial revision
+ *
  *
  *
  * description:
@@ -26,6 +29,7 @@ extern "C" {
 
 #include "Header.hh"
 #include "GeAlarm.hh"
+#include "Task.hh"
 
 
 #ifndef GE_SCOPE
@@ -34,15 +38,36 @@ extern "C" {
 
 GE_SCOPE DispStatus *GeStatus;
 GE_SCOPE DispStatus *AlarmStatus;
+GE_SCOPE DispCommand *GeCommand;
 GE_SCOPE Header *Hdr;
 GE_SCOPE GeAlarm *Alarm;
 GE_SCOPE int system;
 
+GE_SCOPE Task *AlarmTask;
+GE_SCOPE Task *CtrlTask;
+GE_SCOPE Task *DrawingTask;
+
+// Flag masks.
+static const unsigned int TASK_STOP   = 0x1;
+static const unsigned int TASK_START  = 0x2;
+static const unsigned int ATASK_RESET = 0x4;
+static const unsigned int ATASK_SIGNAL = 0x8;
+
 // Functions known locally.
 extern "C" {
 void GeStart(char *server, int system);
+
+// Entry point list.
+extern void AlarmLoop(Task &self, char *server, int sys);
+extern void RpcLoop(Task &self);
+extern void DrawingLoop(Task &self);
 };
 
-extern void AlarmLoop(char *server, int sys);
+// Priorities.
+static const int DRAWING_PRI = 50;
+static const int CTRL_PRI = 3;
+static const int ALARM_PRI = 2;
+static const int GRAPH_PRI = 51;
+static const int MOUSE_PRI = 49;
 
 #endif // INCGeGlobalhh 
