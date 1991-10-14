96841817e240a49977891130ea3781824867145e
Rich Neitzel <rich@ucar.edu>
1991-10-14 20:33:44 +0000

    Added code to compute number of variables, parameters, etc. and plug them in. Also added code to create/draw display.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@690 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/display/ge/RadialLoop.cc
+++ vx-eldora/display/ge/RadialLoop.cc
@@ -9,8 +9,11 @@
  * revision history
  * ----------------
  * $Log$
+ * Fixed set references in makeDisplay.
+ *
  * Revision 1.2  1991/10/14  20:33:44  thor
  * Added code to compute number of variables, parameters, etc. and plug them
+ * in. Also added code to create/draw display.
  *
  * Revision 1.1  1991/10/14  19:22:18  thor
  * Initial revision
@@ -21,11 +24,10 @@ static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
  *        
     asm volatile ("fmove%.l %0,fpcr" : : "dmi" (cm));
 
-// The following are only for uniprocessor systems!!!
-static char whichRadar = 'F';
+    return(i);
+}
 
-#include "ColorConveter.hh"
-static ColorConverter *conv = NULL;
+
 static Radial *makeDisplay(Radial *, GraphicController *);
 void RadialLoop(FAST Task &self, FAST GraphicController *agc)
 #include "ColorConverter.hh"
@@ -51,8 +53,16 @@ static ColorConverter *conv = NULL;
 		    case START:
 		    case RELOAD:
 		    case RESTART:
+		      display = makeDisplay(display,agc);
+		      break;
+
 		    case FORWARD_RADIAL:
+		      whichRadar = FORWARD_RADIAL;
+		      display = makeDisplay(display,agc);
+		      break;
+
 		    case AFT_RADIAL:
+		      whichRadar = AFT_RADIAL;
 		      display = makeDisplay(display,agc);
 		      break;
 		if (!pipe.Empty())
@@ -64,22 +74,23 @@ static ColorConverter *conv = NULL;
 		whichRadar = AFT_RADIAL;
 		radar = whichRadar;
 	  // Color convert it - only on uniprocessor!
-
+//	  conv->GetBeam(theData,radData);
 	  // Draw it.
-
+//	  display->drawBeam(radData);
 	  flag = self.QueryFlags();
 			display->UpdateClock(now->hour,now->minute,
 					     now->second);
 		  }
 
 		lastAngle = radData.angle;
+
+		// Color convert it - only on uniprocessor!
 		display->drawBeam(radData);
 
 		if (pipe.Empty() == FALSE)
 		  {
 		      self.SetFlags(NEW_DATA_FLAG);
 		      continue;
-    int offsets[3];
       }
 }
 
@@ -89,15 +100,109 @@ static Radial *makeDisplay(FAST Radial *old, FAST GraphicController *agc)
 
     if (old != NULL)
       delete(old);   
-    conv = new ColorConverter(DISPLAYED_GATES,max,min,offsets,
-			      x,ptr->numParams);
-//
+
+    // The following is only for uniprocessor systems!!!!!
+    if (conv != NULL)
+      delete(conv);
+
+    float max[3];
+    float min[3];
+    float scales[3];
+    float biases[3];
+
+    max[0] = ptr->max0;
+    max[1] = ptr->max1;
+    max[2] = ptr->max2;
+
+    min[0] = ptr->min0;
+    min[1] = ptr->min1;
+    min[2] = ptr->min2;
+
+    FAST int nv = ptr->numParams;
+
+    FAST RADARDESC *rd = Hdr->Radar(1);
+
+    FAST int np = rd->num_parameter_des;
+
+    FAST int param = ptr->param0;
+
+    if (param != NO_PARAM)
+      {
+	  FAST char *ptr = ParamTapeNames[ParamToNum(param)];
+
+    if (param != NO_PARAM)
+
+	  for (FAST int i = 0; i < np; i++)
+	    {
+		PARAMETER *p = Hdr->Parameter(i);
+
+		if (!strncmp(ptr,p->parameter_name,len))
+		  {
+		      offsets[0] = i;
+		      scales[0] = p->parameter_scale;
+		      biases[0] = p->parameter_bias;
+		      break;
+		  }
+
+    param = ptr->param1;
+
+    if (nv > 1 && param != NO_PARAM)
+      {
+	  FAST char *ptr = ParamTapeNames[ParamToNum(param)];
+
+    if (param != NO_PARAM)
+
+	  for (FAST int i = 0; i < np; i++)
+	    {
+		PARAMETER *p = Hdr->Parameter(i);
+
+		if (!strncmp(ptr,p->parameter_name,len))
+		  {
+		      offsets[1] = i;
+		      scales[1] = p->parameter_scale;
+		      biases[1] = p->parameter_bias;
+		      break;
+		  }
+
+    param = ptr->param2;
+
+    if (nv > 2 && param != NO_PARAM)
+      {
+	  FAST char *ptr = ParamTapeNames[ParamToNum(param)];
+    conv = new ColorConverter(DISPLAYED_GATES,max,min,offsets,np,nv);
+	  FAST int len = strlen(ptr);
+
+	  for (FAST int i = 0; i < np; i++)
+    conv->SetBeamSize(*cs);
+			      
     if (old != NULL)
       delete(old);
 
-    FAST DispCommand *ptr = GeCommand;
+    FAST Radial *New = new Radial(agc,DISPLAYED_GATES,ptr->numParams,0,0);
+      {
+    param = ptr->param0;
 
-    FAST Radial *New = Radial(agc,DISPLAYED_GATES,ptr->numParams,0,0);
+    if (param != NO_PARAM)
+      {
+	  New->drawTable(A_SET,max[0],min[0],param);
+
+    agc->clear();
+
+    param = ptr->param1;
+
+    if (param != NO_PARAM)
+
+	  New->drawTable(B_SET,max[0],min[0],param);
+	  New->drawTitle(A_SET,whichRadar);
+	  agc->setColorMap((long *)colors,256);
+
+    param = ptr->param2;
+
+    if (param != NO_PARAM)
+
+	  New->drawTable(C_SET,max[0],min[0],param);
+	  New->drawTitle(A_SET,whichRadar);
+      {
       {
 	  param = ptr->param1;
 	  New->drawTable(B_SET,max[1],min[1],param);
