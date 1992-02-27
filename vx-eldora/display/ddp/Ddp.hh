e6ad492b02be0d7be491a7739011100b9db18a32
Rich Neitzel <rich@ucar.edu>
1992-02-27 19:37:21 +0000

    Added missing Task.hh include.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@842 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/display/ddp/Ddp.hh
+++ vx-eldora/display/ddp/Ddp.hh
@@ -9,6 +9,9 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.2  1991/11/11  15:34:22  thor
+ * Removed constructor wait code, added repeat count read to Next.
+ *
  * Revision 1.1  1991/10/31  19:50:07  thor
  * Initial revision
  *
@@ -21,6 +24,7 @@
 #define INCDdphh
 
 #include "Pipe.hh"
+#include "Task.hh"
 
 extern "C" {
 #include "string.h"
