9dab17da3aa7b1940b64128d514887ea25a27c20
Shawn Copeland <unknown@ucar.edu>
1992-06-01 21:54:37 +0000

    Added vectors 252 and 251, for ecb-spare and ecb DPRAM-contention interrupts
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@867 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/vx/hskp/include/hskpInt.h
+++ vx-eldora/vx/hskp/include/hskpInt.h
@@ -9,16 +9,21 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.1  1992/05/29  17:54:29  shawn
+ * Initial revision
+ *
  *
  *
  * description: Contains interrupt definitions for the hskp.
  *        
  */
 
-#define vme_vme_vec   255
-#define ecb_cmplt_vec 254
-#define ecb_error_vec 253
+#define vme_vme_vec    255
 
+#define ecb_cmplt_vec  254
+#define ecb_error_vec  253
+#define ecb_spare_vec  252
+#define ecb_dpcntd_vec 251
 
 
 
