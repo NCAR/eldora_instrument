04ff52eb1c07da496c0cc0061687a9ea0f1f977f
Shawn Copeland <unknown@ucar.edu>
1992-06-23 19:39:12 +0000

    Initial revision
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@882 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

---
+++ vx-eldora/vx/hskp/src/ecb/ecbShowTempi.c
@@ -0,0 +1,65 @@
+/*
+ *	$Id$
+ *
+ *	Module:		 ecbShowTempi
+ *	Original Author: Shawn B. Copeland
+ *      Copyright National Center for Atmospheric Research
+ *	Date:		 $Date$
+ *
+ * revision history
+ * ----------------
+ * $Log$
+ *
+ *
+ * description: Read ECB "Temperature Status, scaled int" (ecbTempi) structure
+ *              in Global memory and print the contents.
+ *        
+ */
+static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
+
+/*****************************************************************************/
+/*	ecbShowTempi()                                                       */
+/*		Read ECB "Temperature Status, scaled int" (ecbTempi)         */
+/*              structure in Global memory and print the contents.           */
+/*      Return values:                                                       */
+/*              void function, no value returned.                            */
+/*****************************************************************************/
+
+#include <vxWorks.h>
+#include <stdioLib.h>
+
+#include "ecbFunc.h"     /* function prototypes for ecb______ */
+#include "ecbStat.h"     /* global structures for ecb status */
+#include "ecbAdr.h"      /* Slave addresses on ECB bus */
+
+void ecbShowTempi()
+{
+    printf("ecbShowTempi: values in ecbTempi global status structure:\n");
+    printf("ecbShowTempi: ecbadr   ==> 0x%x\n",ecbTempi.ecbadr);
+    printf("ecbShowTempi: newdata  ==> %u\n",ecbTempi.newdata);
+    printf("ecbShowTempi: onbtemp  ==> %d\n",ecbTempi.onbtemp);
+    printf("ecbShowTempi: offbtemp ==> %d\n",ecbTempi.offbtemp);
+    printf("ecbShowTempi: the address corresponds to:");
+    
+    switch(ecbLastCmd.ecbadr)
+      {
+	case ECBRFFOR:
+ 	  printf(" FORWARD RCVR/XCTR SLAVE.\n");
+	  break;
+	case ECBRFAFT:
+ 	  printf(" AFT RCVR/XCTR SLAVE.\n");
+	  break;
+	case ECBIFFOR:
+ 	  printf(" FORWARD IF PROCESSOR SLAVE.\n");
+	  break;
+	case ECBIFAFT:
+ 	  printf(" AFT IF PROCESSOR SLAVE.\n");
+	  break;
+	case ECBATTEN:
+ 	  printf(" ATTENUATOR CHASSIS SLAVE.\n");
+	  break;
+	default:
+	  printf(" NO KNOWN ADDRESS!!!!\n");
+	  break;
+      }
+}
---
+++ vx-eldora/vx/hskp/src/ecb/ecbShowTempr.c
@@ -0,0 +1,65 @@
+/*
+ *	$Id$
+ *
+ *	Module:		 ecbShowTempr
+ *	Original Author: Shawn B. Copeland
+ *      Copyright National Center for Atmospheric Research
+ *	Date:		 $Date$
+ *
+ * revision history
+ * ----------------
+ * $Log$
+ *
+ *
+ * description: Read ECB "Temperature Status, raw counts" (ecbTempr) structure
+ *              in Global memory and print the contents.
+ *        
+ */
+static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
+
+/*****************************************************************************/
+/*	ecbShowTempr()                                                       */
+/*		Read ECB "Temperature Status, raw counts" (ecbTempr)         */
+/*              structure in Global memory and print the contents.           */
+/*      Return values:                                                       */
+/*              void function, no value returned.                            */
+/*****************************************************************************/
+
+#include <vxWorks.h>
+#include <stdioLib.h>
+
+#include "ecbFunc.h"     /* function prototypes for ecb______ */
+#include "ecbStat.h"     /* global structures for ecb status */
+#include "ecbAdr.h"      /* Slave addresses on ECB bus */
+
+void ecbShowTempr()
+{
+    printf("ecbShowTempr: values in ecbTempr global status structure:\n");
+    printf("ecbShowTempr: ecbadr   ==> 0x%x\n",ecbTempr.ecbadr);
+    printf("ecbShowTempr: newdata  ==> %u\n",ecbTempr.newdata);
+    printf("ecbShowTempr: onbtemp  ==> %u\n",ecbTempr.onbtemp);
+    printf("ecbShowTempr: offbtemp ==> %u\n",ecbTempr.offbtemp);
+    printf("ecbShowTempr: the address corresponds to:");
+    
+    switch(ecbLastCmd.ecbadr)
+      {
+	case ECBRFFOR:
+ 	  printf(" FORWARD RCVR/XCTR SLAVE.\n");
+	  break;
+	case ECBRFAFT:
+ 	  printf(" AFT RCVR/XCTR SLAVE.\n");
+	  break;
+	case ECBIFFOR:
+ 	  printf(" FORWARD IF PROCESSOR SLAVE.\n");
+	  break;
+	case ECBIFAFT:
+ 	  printf(" AFT IF PROCESSOR SLAVE.\n");
+	  break;
+	case ECBATTEN:
+ 	  printf(" ATTENUATOR CHASSIS SLAVE.\n");
+	  break;
+	default:
+	  printf(" NO KNOWN ADDRESS!!!!\n");
+	  break;
+      }
+}
