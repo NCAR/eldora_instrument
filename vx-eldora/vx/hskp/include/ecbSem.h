2ca250dd7894356c1593e8c9ed172bba8c535c28
Shawn Copeland <unknown@ucar.edu>
1992-06-24 20:46:33 +0000

    a version for safety's sake.  Current as of 6/24/92
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@890 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/vx/hskp/include/ecbSem.h
+++ vx-eldora/vx/hskp/include/ecbSem.h
@@ -9,17 +9,18 @@
  * revision history
  * ----------------
  * $Log$
- *
+ * Revision 1.1  1992/05/29  17:52:55  shawn
+ * Initial revision
  *
  * description: Contains semaphore definitions for the ecb master.
  *        
  */
 
+#ifdef MAINPROG
 SEM_ID ecb_cmd_not_pending;
-
-
-
-
+#else
+extern SEM_ID ecb_cmd_not_pending;
+#endif
 
 
 
