ebd162650eed9a258beb10c8b85e8c1336539035
Shawn Copeland <unknown@ucar.edu>
1992-06-30 20:44:35 +0000

    Added attenuator and Utility menu code.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@914 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/vx/hskp/src/ecb/ecbMenu.c
+++ vx-eldora/vx/hskp/src/ecb/ecbMenu.c
@@ -9,12 +9,14 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.3  1992/09/24  18:20:32  shawn
+ * Added "get temperature" options to if, rf, and atten menus.  Added
  * "show rpc global status" option to utility menu.
  *
  * Revision 1.2  1992/06/30  20:44:35  shawn
  * Added attenuator and Utility menu code.
  *
-static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
+ * Revision 1.1  1992/06/29  19:52:47  shawn
  * Initial revision
  *
  * description: Upper level menu for manipulating ECB slaves from HSKP
@@ -33,7 +35,7 @@ static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
 
 #include <vxWorks.h>
 #include <stdioLib.h>
-#define BSTATTIMEOUT 9999999 /* get-bus-status timeout value */
+#include "semLib.h"
 
 #include "ecbFunc.h"     /* function prototypes for ecb______ */
 #include "ecbStat.h"     /* global structures for ecb status */
@@ -177,12 +179,200 @@ void ifchoices()
 	      if(!ecbSlvStat.newdata)
 		printf("GET-BUS-STATUS FROM IF PROCESSOR HAS TIMED OUT.\n");
 	      else
-printf("rfchoices: stub entered.\n");
+		ecbShowBusStat();
+	      break;
+	    case 4:
+	      printf("Get IF Processor Temperature (scaled integer)\n");
+	      printf("   Enter 1 for forward, 0 for aft ==>");
+	      scanf("%d",&foraft);
+	      if(foraft && foraft!=1)
+		{
+		    printf("%d is an invalid forward/aft ",foraft);
+		    printf("value (0 or 1)\n");
+		    break;
+		}
+	      if(foraft)
+		ecbGetTsami((unsigned char) ECBIFFOR);
+	      else
+		ecbGetTsami((unsigned char) ECBIFAFT);
+	      for(icount=0; icount<TEMPITIMEOUT; icount++)
+		if(ecbTempi.newdata) break;
+	      if(!ecbTempi.newdata)
+		printf("GET-TEMP-INTEGER FROM IF PROCESSOR HAS TIMED OUT.\n");
+	      else
+		ecbShowTempi();
+	      break;
+	    case 5:
+	      printf("Get IF Processor Temperature (raw counts)\n");
+	      printf("   Enter 1 for forward, 0 for aft ==>");
+	      scanf("%d",&foraft);
+	      if(foraft && foraft!=1)
+		{
+		    printf("%d is an invalid forward/aft ",foraft);
+		    printf("value (0 or 1)\n");
+		    break;
+		}
+	      if(foraft)
+		ecbGetTsamr((unsigned char) ECBIFFOR);
+	      else
+		ecbGetTsamr((unsigned char) ECBIFAFT);
+	      for(icount=0; icount<TEMPRTIMEOUT; icount++)
+		if(ecbTempr.newdata) break;
+	      if(!ecbTempr.newdata)
+		printf("GET-TEMP-RAW FROM IF PROCESSOR HAS TIMED OUT.\n");
+	      else
+		ecbShowTempr();
+	      break;
+	    default:
+	      printf("ifchoices:  %d IS NOT A VALID MENU ",menu_choice);
+	      printf("OPTION.   TRY AGAIN...\n");
+	      break;
+	  }
+    }
+}
+
+void rfchoices()
+{
+    int menu_choice=0,icount;
+    int foraft=0,unitnum=0,nrval=0;
+    double freq=0.0;
+    
+    while(1)
+      {
+	printf("\nrfchoices - Menu for manipulating ECB Slaves in ");
+	printf("RCVR/XCTRs\n\n");
+	printf("1.)  LEAVE THIS MENU (return to main menu).\n");
+	printf("2.)  Set Frequency in a DDS unit.\n");
+	printf("3.)  Get Bus Status.\n");
+	printf("4.)  Get Temperature (scaled integer).\n");
+	printf("5.)  Get Temperature (raw counts).\n");
+	printf("6.)  Get Lock Status.\n");
+	printf("\nEnter a choice 1-6 ==>");
+	scanf("%d",&menu_choice);
+	if (menu_choice == 1) break;
+	switch(menu_choice)
+	  {
 	    case 2:
 	      printf("Set DDS Frequency\n");
 	      printf("   Enter 1 for forward, 0 for aft ==>");
 	      scanf("%d",&foraft);
-printf("atchoices: stub entered.\n");
+	      if(foraft && foraft!=1)
+		{
+		    printf("%d is an invalid forward/aft ",foraft);
+		    printf("value (0 or 1)\n");
+		    break;
+		}
+	      printf("   Enter DDS unit number (1-6) ==>");
+	      scanf("%d",&unitnum);
+	      if(unitnum < ECBDDSLOWUNIT || unitnum > ECBDDSHIGHUNIT)
+		{
+		    printf("%d is an invalid DDS unit number.\n",unitnum);
+		    break;
+		}
+	      printf("   Enter frequency in GHz (9.3-9.8) ==>");
+	      scanf("%lf",&freq);
+	      freq *= 1000000000.0;
+	      if(freq < ECBDDSLOWFREQ || freq > ECBDDSHIGHFREQ)
+		{
+		    printf("%lf is an invalid frequency\n",freq);
+		    break;
+		}
+	      printf("   Enter 1 for noise reduction, 0 for no NR ==>");
+	      scanf("%d",&nrval);
+	      if(nrval && nrval!=1)
+		{
+		    printf("%d is an invalid NR value (0 or 1)\n",nrval);
+		    break;
+		}
+	      if(foraft)
+		ecbSetDDSfloat((unsigned char) ECBRFFOR,(unsigned char) unitnum, (double) freq,(unsigned char) nrval);
+	      else
+		ecbSetDDSfloat((unsigned char) ECBRFAFT,(unsigned char) unitnum, (double) freq,(unsigned char) nrval);
+	      break;
+	    case 3:
+	      printf("Get RCVR/XCTR slave Bus Status\n");
+	      printf("   Enter 1 for forward, 0 for aft ==>");
+	      scanf("%d",&foraft);
+	      if(foraft && foraft!=1)
+		{
+		    printf("%d is an invalid forward/aft ",foraft);
+		    printf("value (0 or 1)\n");
+		    break;
+		}
+	      if(foraft)
+		ecbGetBusStat((unsigned char) ECBRFFOR);
+	      else
+		ecbGetBusStat((unsigned char) ECBRFAFT);
+	      for(icount=0; icount<BSTATTIMEOUT; icount++)
+		if(ecbSlvStat.newdata) break;
+	      if(!ecbSlvStat.newdata)
+		printf("GET-BUS-STATUS FROM RCVR/XCTR HAS TIMED OUT.\n");
+	      else
+		ecbShowBusStat();
+	      break;
+	    case 4:
+	      printf("Get RCVR/XCTR Temperature (scaled integer)\n");
+	      printf("   Enter 1 for forward, 0 for aft ==>");
+	      scanf("%d",&foraft);
+	      if(foraft && foraft!=1)
+		{
+		    printf("%d is an invalid forward/aft ",foraft);
+		    printf("value (0 or 1)\n");
+		    break;
+		}
+	      if(foraft)
+		ecbGetTsami((unsigned char) ECBRFFOR);
+	      else
+		ecbGetTsami((unsigned char) ECBRFAFT);
+	      for(icount=0; icount<TEMPITIMEOUT; icount++)
+		if(ecbTempi.newdata) break;
+	      if(!ecbTempi.newdata)
+		printf("GET-TEMP-INTEGER FROM RCVR/XCTR HAS TIMED OUT.\n");
+	      else
+		ecbShowTempi();
+	      break;
+	    case 5:
+	      printf("Get RCVR/XCTR Temperature (raw counts)\n");
+	      printf("   Enter 1 for forward, 0 for aft ==>");
+	      scanf("%d",&foraft);
+	      if(foraft && foraft!=1)
+		{
+		    printf("%d is an invalid forward/aft ",foraft);
+		    printf("value (0 or 1)\n");
+		    break;
+		}
+	      if(foraft)
+		ecbGetTsamr((unsigned char) ECBRFFOR);
+	      else
+		ecbGetTsamr((unsigned char) ECBRFAFT);
+	      for(icount=0; icount<TEMPRTIMEOUT; icount++)
+		if(ecbTempr.newdata) break;
+	      if(!ecbTempr.newdata)
+		printf("GET-TEMP-RAW FROM RCVR/XCTR HAS TIMED OUT.\n");
+	      else
+		ecbShowTempr();
+	      break;
+	    case 6:
+	      printf("Get RCVR/XCTR Lock status\n");
+	      printf("   Enter 1 for forward, 0 for aft ==>");
+	      scanf("%d",&foraft);
+	      if(foraft && foraft!=1)
+		{
+		    printf("%d is an invalid forward/aft ",foraft);
+		    printf("value (0 or 1)\n");
+		    break;
+		}
+	      if(foraft)
+		ecbGetDDSool((unsigned char) ECBRFFOR);
+	      else
+		ecbGetDDSool((unsigned char) ECBRFAFT);
+	      for(icount=0; icount<DDSOOLTIMEOUT; icount++)
+		if(ecbOol.newdata) break;
+	      if(!ecbOol.newdata)
+		printf("GET-DDS-OOL FROM RCVR/XCTR HAS TIMED OUT.\n");
+	      else
+		ecbShowDDSool();
+	      break;
 	    default:
 	      printf("rfchoices:  %d IS NOT A VALID MENU ",menu_choice);
 	      printf("OPTION.   TRY AGAIN...\n");
@@ -193,7 +383,42 @@ printf("atchoices: stub entered.\n");
 
 void atchoices()
 {
-printf("utilchoices: stub entered.\n");
+    int menu_choice=0,icount;
+    int foraft=0,xmittp=0,N=0,muxval=0,attenval=121;
+    
+    while(1)
+      {
+	printf("\natchoices - Menu for manipulating ECB Slave in ");
+	printf("ATTENUATOR Chassis\n\n");
+	printf("1.)  LEAVE THIS MENU (return to main menu).\n");
+	printf("2.)  Set Attenuator Value.\n");
+	printf("3.)  Get Bus Status.\n");
+	printf("4.)  Get Temperature (scaled integer).\n");
+	printf("5.)  Get Temperature (raw counts).\n");
+	printf("6.)  Set Trigger Mux Value.\n");
+	printf("7.)  Set Trigger Divide-by-N Value.\n");
+	printf("\nEnter a choice 1-5 ==>");
+	scanf("%d",&menu_choice);
+	if (menu_choice == 1) break;
+	switch(menu_choice)
+	  {
+	    case 2:
+	      printf("Set Attenuator Value\n");
+	      printf("   Enter 1 for forward, 0 for aft ==>");
+	      scanf("%d",&foraft);
+	      if(!foraft)
+		printf("\nAFT ATTENUATOR SELECTED\n\n");
+	      else if (foraft==1)
+		printf("\nFORWARD ATTENUATOR SELECTED\n\n");
+	      else
+		{
+		    printf("%d is an invalid forward/aft ",foraft);
+		    printf("value (0 or 1)\n");
+		    break;
+		}
+	      printf("   Enter Attenuator Value in dB (0-121) ==>");
+	      scanf("%d",&attenval);
+	      if(attenval < ECBATLOWATTEN || attenval > ECBATHIGHATTEN)
 		{
 		    printf("%d is an invalid attenuator value\n",attenval);
 		    break;
