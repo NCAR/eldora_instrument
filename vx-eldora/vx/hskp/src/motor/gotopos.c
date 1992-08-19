beab348d64f6219920edd82a9c2eb48c0c15957c
Craig Walther <unknown@ucar.edu>
1992-08-19 17:41:01 +0000

    Initial revision
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@945 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

---
+++ vx-eldora/vx/hskp/src/motor/gotopos.c
@@ -0,0 +1,31 @@
+/*
+ *
+ *	$Id$
+ *
+ *	Module:	gotopos.c	 
+ *	Original Author: Reif Heck 
+ *      Copywrited by the National Center for Atmospheric Research
+ *	Date:		 $Date$
+ *
+ * revision history
+ * ----------------
+ * $Log$
+ *
+ * description:
+ *             
+ *             
+ *              
+ *              
+ *             
+ *             
+ *             
+ */
+
+static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
+
+#include <cntrlIncl.h>
+
+void goto_pos(float position)
+{
+
+}
