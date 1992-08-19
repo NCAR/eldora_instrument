260c49e3b0101f79544111f1a77d815535b1e314
Craig Walther <unknown@ucar.edu>
1992-08-19 17:27:19 +0000

    Initial revision
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@944 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

---
+++ vx-eldora/vx/hskp/src/gps/init_gps.c
@@ -0,0 +1,24 @@
+/*
+ *	$Id$
+ *
+ *	Module: gpsInit.c		 
+ *	Original Author: Craig Walther 
+ *      Copywrited by the National Center for Atmospheric Research
+ *	Date:		 $Date$
+ *
+ * revision history
+ * ----------------
+ * $Log$
+ *
+ * description:
+ *        
+ */
+
+static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
+
+#include <gpsIncl.h>
+
+void gps_init()
+{
+return;
+}
