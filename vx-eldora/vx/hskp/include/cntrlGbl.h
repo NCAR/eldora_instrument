74cf560f23a01779f8a84e52c212cdb5ae3bdc07
Craig Walther <unknown@ucar.edu>
1992-09-01 17:30:06 +0000

    Initial revision
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@969 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

---
+++ vx-eldora/vx/hskp/include/cntrlGbl.h
@@ -0,0 +1,25 @@
+/*
+ *
+ *	$Id$
+ *
+ *	Module:	cntrlGbl.h	 
+ *	Original Author: Reif Heck 
+ *      Copywrited by the National Center for Atmospheric Research
+ *	Date:		 $Date$
+ *
+ * revision history
+ * ----------------
+ * $Log$
+ * Revision 1.1  1992/08/14  21:36:36  reif
+ * Initial revision
+ *
+ *
+ * description: These are the global variables associated withthe motor controller.
+ *             
+ *             
+ *              
+ *              
+ *             
+ *             
+ *             
+ */
---
+++ vx-eldora/vx/hskp/include/gpsGbl.h
@@ -0,0 +1,37 @@
+/*
+ *	$Id$
+ *
+ *	Module:	gpsGbl.h	 
+ *	Original Author: Craig Walther 
+ *      Copywrited by the National Center for Atmospheric Research
+ *	Date:		 $Date$
+ *
+ * revision history
+ * ----------------
+ * $Log$
+ * Revision 1.1  1992/08/19  19:33:44  craig
+ * Initial revision
+ *
+ *
+ * description: These are the global variables used to handle the
+ *              Global Positioning System (GPS) interface.
+ *              This interface consists of a Matrix
+ *              CPU-100 Board running software in an EPROM that can handle the  *              Trimble TANS II GPS receiver.
+ */
+
+#ifndef INCgpsGblh
+#define INCgpsGblh
+
+/*****************  SET ADDRESSES IN THE CPU100 Dual Port RAM  ************/
+
+scope char *gps_status;    /* Pointer to the status word */
+scope long *gps_data_loc;  /* Pointer to the data location long word */
+scope short *gps_command;   /* Pointer to the GPS command Word */
+scope short *gps_mbox;      /* Pointer to the GPS mailbox Command */
+scope short *gps_hndshk;    /* Pointer to the GPS handshake area */
+scope char *tp41_mbox_0;    /* Pointer to the zeroth mailbox */
+
+#endif /* INC */
+
+
+
