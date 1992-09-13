b6430fa3b6d271de145b1112997887bb7afca6b9
Shawn Copeland <unknown@ucar.edu>
1992-09-13 23:43:20 +0000

    Initial revision
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1003 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

---
+++ vx-eldora/vx/mhen/src/timmod/testpulseb.c
@@ -0,0 +1,108 @@
+/*
+ *	$Id$
+ *
+ *	Module:	TESTPULSEB	 
+ *	Original Author: Shawn B. Copeland 
+ *      Copywrited by the National Center for Atmospheric Research
+ *	Date:		 $Date$
+ *
+ * revision history
+ * ----------------
+ * $Log$
+ * Revision 1.1  1992/09/11  20:02:04  shawn
+ * Initial revision
+ *
+ *
+ * description:
+ *
+  Purpose:       Program ELDORA timing module rev B test pulse parameters.
+  Arguments:     foraft     int, 1 for forward, 0 for aft.
+                 mux        int, mux value to trigger test pulse:
+		            0 - CHIP(0)
+			    1 - CHIP(1)
+			    2 - CHIP(2)
+			    3 - CHIP(3)
+			    4 - CHIP(4)
+			    5 - PK(0)
+		            8 - CHIP(5)
+			    9 - CHIP(6)
+			    10 - CHIP(7)
+			    11 - CHIP(8)
+			    12 - CHIP(9)
+			    13 - PK(1)
+		 dist       int, meters to beginning of testpulse.
+		 width      int, test pulse width in meters.
+  Date Written:  09/11/92
+ *        
+ */
+static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
+
+
+#include "ELDRP7.h"
+
+void testpulseb(foraft,mux,dist,width)
+     
+     int foraft,mux,dist,width;
+     
+{
+  unsigned int realdist,realwidth;
+  unsigned short *timmux,*timdist,*timwidth;
+  unsigned short distcounts,widthcounts;
+
+/* Pointers to Timing Module locations  */
+
+if (foraft == 0)
+  {   /* AFT */
+      timmux   = (unsigned short *)(TIMBASE + TIMATPMUX);  /* Mux */
+      timdist  = (unsigned short *)(TIMBASE + TIMATPDIST); /* Distance Countr*/
+      timwidth = (unsigned short *)(TIMBASE + TIMATPWIDT); /* Width Counter*/
+  }
+else if (foraft == 1)
+  {   /* FOR */
+      timmux   = (unsigned short *)(TIMBASE + TIMFTPMUX);  /* Mux */
+      timdist  = (unsigned short *)(TIMBASE + TIMFTPDIST); /* Distance Countr*/
+      timwidth = (unsigned short *)(TIMBASE + TIMFTPWIDT); /* Width Counter*/
+  }
+else
+  {   /* ERROR */
+      printf("testpulseb: incorrect value for foraft (%d) passed.\n",foraft);
+      printf("testpulseb: aborting without programming test pulse values.\n");
+      return;
+  }
+
+  /* print out usage info */
+  printf("Hello, this is testpulseb.  Usage:\n");
+  printf("testpulseb foraft[1,0],mux,dist[m],width[m]\n\n");
+  printf("values actually passed to testpulseb==> %d,%d,%d,%d\n",foraft,mux,dist,width);
+
+  /* do minor error checking */
+  if ((mux<0) || (mux==6) || (mux==7) || (mux > 13))
+    {
+      printf("testpulseb: invalid mux value (%d) passed.\n",mux);
+      printf("testpulseb: aborting without programming test pulse values.\n");
+      return;
+    }
+
+  /* compute derived parameters */
+  distcounts = dist/10;   /* 10 meters per 15MHz count */
+  widthcounts = width/10; /* 10 meters per 15MHz count */
+  realdist = distcounts * 10;
+  realwidth = widthcounts * 10;
+
+  /* write actual values out */
+  *timmux   = mux;
+  *timdist  = distcounts;
+  *timwidth = widthcounts;
+
+  /* ===========SHOW ACTUAL PROGRAMMED VALUES============== */
+  printf("values actually setup by testpulseb:\n");
+  printf("  mux       = %d\n  dist      = %d\n",mux,realdist);
+  printf("  width     = %d\n",realwidth);
+	    
+printf("testpulseb: finished.\n");
+}
+
+
+
+
+
