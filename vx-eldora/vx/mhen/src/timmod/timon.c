9352872b87977fe5db451f06204ee168e5b9b120
Eric Loew <ericloew@ucar.edu>
1993-05-12 15:59:24 +0000

    modified code to map OLD TIMING MODULE into VME Extended Adress Space.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1088 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/vx/mhen/src/timmod/timon.c
+++ vx-eldora/vx/mhen/src/timmod/timon.c
@@ -9,6 +9,22 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.3  1992/09/10  20:54:32  shawn
+ * Changed value written to 0x01, from 0x00, to accomodate timmod rev. b
+ * too.  Rev. A had two addresses, an on and an off, Rev. B has one, and
+ * you write a 1 to turn it on and a 0 to turn it off.  Thus, timon will
+ * now work with both cards.  timoff will be left alone, since for rev.
+ * A there will be a seperate address to write to, and for rev. B it will
+ * be the same one as to turn it on.  There will also be created two new
+ * modules, timonb and timoffb, to be used exclusively with the rev. B
+ * board.  This change accompanies a change to ELDRP7.h in which a line
+ * was added to define the offset to the rev. B ON/OFF address, namely,
+ * a #define TIMONOFFB.
+ *
+ * The weather was quite nice while I was making these changes, unlike
+ * the other staff member who had to be threatened with root access just
+ * to do a simple thing like check in a piece of my code.
+ *
  * Revision 1.2  1992/09/10  20:40:43  eric
  * *** empty log message ***
  *
@@ -29,7 +45,7 @@ static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
 timon()
 {
    unsigned short *timon;
- 
-   timon = (unsigned short *)(TIMBASE + TIMON);
+ /* map Old Timing Module in VME Extended Space */ 
+   timon = (unsigned short *)(TIMBASE - STD_BASE + EXTD_BASE + TIMON);
    *timon = 0x0001;  /* changed value to 0x01 to accomodate rev. B board */
 }
