8f5883db99389c974a1f332be0ccaf3cc15958b4
Eric Loew <ericloew@ucar.edu>
1993-05-12 16:18:52 +0000

    modified code to map OLD Timing Module to VME Extended Address Space.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1091 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/vx/mhen/src/timmod/timoff.c
+++ vx-eldora/vx/mhen/src/timmod/timoff.c
@@ -9,6 +9,9 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.2  1992/09/10  20:40:58  eric
+ * *** empty log message ***
+ *
  * Revision 1.1  1991/12/04  19:06:51  eric
  * Initial revision
  *
@@ -26,7 +29,7 @@ static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
 timoff()
 {
    unsigned short *timoff;
- 
-   timoff = (unsigned short *)(TIMBASE + TIMOFF);
+ /* map Old Timing Module in VME Extended Space */ 
+   timoff = (unsigned short *)(TIMBASE - STD_BASE + EXTD_BASE + TIMOFF);
    *timoff = 0x0000;
 }
