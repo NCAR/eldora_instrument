898728d416d57114e9c8a690e4d5bb72d7998271
Rich Neitzel <rich@ucar.edu>
1993-07-29 17:55:15 +0000

    Upgraded to VxWorks 5.1 & removed BSDisms.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1118 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/display/wdw/Mouse.cc
+++ vx-eldora/display/wdw/Mouse.cc
@@ -9,6 +9,9 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 2.1  1993/07/01  16:20:03  thor
+ * Brought code up to latest ANSI draft spec.
+ *
  * Revision 2.0  1992/11/03  12:51:14  thor
  * First offical ELDORA release!
  *
@@ -56,34 +59,34 @@ Mouse::Mouse(FAST void *addr, Point firstXY, FAST long *cursorBits, int vector)
     cursorBase = addr + CURSOR_OFFSET;
     crosshairBase = addr + CROSSHAIR_OFFSET;
     kbdBase = addr + KBD_OFFSET;
-
+    
     cursorImage = (long *)(addr + CURSOR_IMAGE_OFFSET);
-
+    
     interruptMCU = (unsigned char *)(addr + MCU_INT_OFFSET);
     mcuCmd = (unsigned char *)(addr + MCU_CMD_OFFSET);
     buttonState = (unsigned char *)(addr + BUTTON_OFFSET);
     kbdHead = (unsigned char *)(addr + KBD_HEAD_OFFSET);
     kbdTail = kbdHead + 1;
-
+    
     kbdQueue = (keyEvent *)(addr + KBD_QUEUE_OFFSET);
-
+    
     curX = (unsigned short *)(addr + CURX_OFFSET);
-
+    
     isrData.clearMe = (unsigned char *)(addr + WHY_OFFSET);
-
-// Download the cursor pattern.
-
-    bcopy((char *)cursorBits,(char *)cursorImage,CURSOR_SIZE);
-
+    
+// Download the cu1rsor pattern.
+    
+    memcpy((char *)cursorImage,(char *)cursorBits,CURSOR_SIZE);
+    
 // Now create the needed semaphores.
     accessSem = semBCreate(SEM_Q_FIFO,SEM_FULL);
-
+    
     isrData.sem = semBCreate(SEM_Q_FIFO,SEM_EMPTY);
-
+    
 // Now link ISR to vector.
     vector &= 0xf0;		// First strip off AGC controlled bits.
     vector += 8;		// Now add offset for MCU interrupt
-
+    
     if (intConnect((VOIDFUNCPTR *)(vector*4),(VOIDFUNCPTR)mouseISR,
 		   (int)&isrData) == ERROR)
       {
@@ -91,78 +94,78 @@ Mouse::Mouse(FAST void *addr, Point firstXY, FAST long *cursorBits, int vector)
 		  "Failed to connect to interrupt vector %d. Exiting.\n");
 	  exit(1);
       }
-
+    
 // Now initialize the MCU.
     FAST unsigned char *cinit = (unsigned char *)(mouseBase + MOUSE_INT_OFF);
-
+    
     *cinit++ = BUTTON_DOWN;	// Set up mouse interrupt conditions.
-
+    
     FAST unsigned short *sinit = (unsigned short *)(cinit);
-
+    
     *sinit++ = 0;		// Set min/max positions.
     *sinit++ = 0;
     *sinit++ = 0x4ff;		// Stop moving at 1279,1023.
     *sinit++ = 0x3ff;
-
+    
     cinit = (unsigned char *)sinit;
-
+    
     *cinit++ = XGEAR;		// Set gearing (i.e. mouse speed).
     *cinit++ = YGEAR;
     *cinit++ = X_HI_GEAR;
     *cinit = Y_HI_GEAR;
-
+    
     cinit = (unsigned char *)cursorBase;
     
     *cinit++ = CURSOR_ENABLE;	// Turn on cursor.
-
+    
     sinit = (unsigned short *)(++cinit);
-
+    
     *sinit++ = X_SLEW;		// Needed by Bt431 cursor chip.
     *sinit++ = (unsigned short)Y_SLEW;
-
+    
     *sinit++ = firstXY.x;	// This is where the cursor will appear.
     *sinit = firstXY.y;
-
+    
     cinit = (unsigned char *)crosshairBase;
-
+    
     *cinit++ = 0;		// No crosshair yet.
-
+    
     sinit = (unsigned short *)(++cinit);
-
+    
     *sinit++ = X_SLEW;
     *sinit = (unsigned short)Y_SLEW;
-
+    
     cinit = (unsigned char *)(crosshairBase + CROSS_WIDTH_OFF);
-
+    
     *cinit = CROSS_WIDTH;
-
+    
 //  No keyboard for now!
 //     cinit  = (unsigned char *)(kbdBase + KBD_CMD_OFF);
 // 
 //     *cinit++ = KBD_RESET;	// Get keyboard ready.
 // 
 //     *cinit = KBD_INT_MASK;	// Interrupt on key press.
-
+    
     volatile FAST unsigned char *mcuInt = interruptMCU;
     FAST unsigned char *cmdReg = mcuCmd;
-
+    
     // To send the AGC MCU a command, first write command type to
     // mcuCmd and then interrupt the MCU by writing any value to
     // interruptMCU.
-
+    
     *cmdReg = CURSOR_PARAM_CHANGE;
     *mcuInt = 1;
     taskDelay(10);
-
+    
     *cmdReg = CURSOR_XY_CHANGE;
     *mcuInt = 2;
     taskDelay(10);
-
+    
     *cmdReg = CROSSHAIR_PARAM_CHANGE;
     *mcuInt = 3;
     taskDelay(10);
-
-
+    
+    
     *isrData.clearMe = 0;	// Must be set to 0 for MCU to interrupt host.
 }
 
@@ -213,7 +216,7 @@ void Mouse::setCursorImage(FAST long *cursorBits)
 
     semTake(sem,WAIT_FOREVER);
 
-    bcopy((char *)cursorBits,(char *)target,CURSOR_SIZE);
+    memcpy((char *)target,(char *)cursorBits,CURSOR_SIZE);
 
     *cmd = CURSOR_PARAM_CHANGE;
     *mcuInt = 1;
