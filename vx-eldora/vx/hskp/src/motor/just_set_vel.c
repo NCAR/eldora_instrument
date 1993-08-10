1f351b206b1bc0e2d40dea05cb8fc22c34f1e16b
Craig Walther <unknown@ucar.edu>
1993-08-10 19:58:32 +0000

    Initial revision
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1123 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

---
+++ vx-eldora/vx/hskp/src/motor/just_set_vel.c
@@ -0,0 +1,66 @@
+
+/*
+
+ *	$Id$
+ *
+ *	Module:	just_set_vel.c	 
+ *	Original Author: Reif Heck 
+ *      Copywrited by the National Center for Atmospheric Research
+ *	Date:		 $Date$
+ *
+ * revision history
+ * ----------------
+ * $Log$
+ * Revision 1.1  1992/09/01  20:47:20  craig
+ * Initial revision
+ *
+ * description:  This module sets the velocity in the motor control card
+ *               as given by the calling routine.  This routine does not
+ *               stop or start the motor.  This routine should be used
+ *               only if the motor has been started and is running an 
+ *               already requested velocity.
+ *             
+ */
+
+static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
+
+#define scope extern
+
+/* Include fifty million vx-works .h files */
+
+#include "vxWorks.h"
+#include "math.h"
+#include "stdioLib.h"
+#include "intLib.h"
+#include "memLib.h"
+#include "semLib.h"
+#include "taskLib.h"
+#include "tyLib.h"
+#include "ioLib.h"
+#include "in.h"
+#include "systime.h"
+#include "sysLib.h"
+
+/* include the .h files that are housekeeper code specific */
+
+#include "tp41vAdr.h"
+#include "cntrlDef.h"
+#include "cntrlGbl.h"
+#include "cntrlFunc.h"
+
+void just_set_vel(float cmrpm)
+{
+union
+  {
+      unsigned short velocity;
+      unsigned char vel[2];
+  }cmnd;
+
+cmnd.velocity = (COUNTS*cmrpm*PERIOD*0.01667)*16;
+
+*vel_msb=cmnd.vel[0];
+
+*vel_lsb=cmnd.vel[1];
+
+return;
+}
