33ab37b30a0be690d500ee3ea4e87ddad0859f6c
Craig Walther <unknown@ucar.edu>
1993-09-20 18:01:36 +0000

    Initial revision
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1156 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

---
+++ vx-eldora/vx/hskp/src/main/hostStartup.c
@@ -0,0 +1,64 @@
+/*
+ *	$Id$
+ *
+ *	Module:		 hostStartup.c
+ *	Original Author: Craig Walther
+ *      Copywrited by the National Center for Atmospheric Research
+ *	Date:		 $Date$
+ *
+ * revision history
+ * ----------------
+ *
+ *
+ *
+ * description:
+ *       This routine runs first in the housekeeping processor and
+ *           starts up all of the other routines (tasks) that run in it.
+ *
+ */
+static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
+
+#define scope extern
+#include "vxWorks.h"
+#include "stdioLib.h"
+#include "string.h"
+#include "hostLib.h"
+#include "taskLib.h"
+#include "nfsLib.h"
+#include "iosLib.h"
+#include "memLib.h"
+#include "vmevmeGbl.h"
+#include "hskpFunc.h"
+
+
+void hostStartup(void)
+{
+    FILE *fp1;
+    char fname[40];
+
+     strcpy(fname,"odin:/vxbin/hskp/scripts/realtime.cmds");
+    
+    /* Open the file and read all global control flags */
+    if(!(fp1=fopen(fname,"r")))
+      {
+	  printf("Could not find file of global control flags\n");
+	  fake_angles = 0;
+      }
+    else
+      {
+	  fscanf(fp1,"%1d\n",&fake_angles);
+      }
+    fclose(fp1);
+    printf("The value of fake_angles is: %d\n",fake_angles);
+    
+    /* Add any other hosts */
+    hostAdd("loki","128.117.85.7");
+    hostAdd("thor","128.117.85.51");
+        
+    taskSpawn("startup",100,0,5000,(FUNCPTR)startup,0,0,
+	      0,0,0,0,0,0,0,0);
+    
+    taskSpawn("hskp",100,0,5000,(FUNCPTR)hskp,0,0,
+	      0,0,0,0,0,0,0,0);
+}
+
