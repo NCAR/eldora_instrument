af9321688d41e4a69c668330962a694b493e83fa
Craig Walther <unknown@ucar.edu>
1994-06-30 14:10:43 +0000

    Changed defines to static floats. Time variable name changes. Moved index out of bounds tests to protect against bad initial values.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1218 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/vx/hskp/src/arinc429/convert_iru.c
+++ vx-eldora/vx/hskp/src/arinc429/convert_iru.c
@@ -9,6 +9,9 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.4  1994/05/20  20:36:33  craig
+ * *** empty log message ***
+ *
  * Revision 1.1  1992/09/28  23:03:55  craig
  * Initial revision
  *
@@ -24,6 +27,27 @@ static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
 
 #include "hskpAll.h"
 
+/* Define conversion factors from IRU units to meteorological units, note
+   that these factors will also include the scale factors given by
+   Honeywell to convert the binary words to standard IRU units. They
+   are precomputed to increase runtine speed. */ 
+
+static float PIRAD_TO_DEG =        9.54e-7*180.0; /* Pirads to degrees */
+static float WKNOTS_TO_MPERSEC =   2.44e-4*0.51449; /* knots to m/s (winds) */
+static float VKNOTS_TO_MPERSEC =   0.0039*0.51449; /* knots to m/s
+                                                      (velocities) */
+static float FEET_TO_KM =          0.125*0.0003048; /* feet to kilometers */
+static float GS_TO_MPERSEC2 =      1.22e-4*9.7959; /* G's to meter/sec2 */
+static float FTPERSEC_TO_MPERSEC = 2.44e-4*0.3048; /* feet/sec to meter/sec */
+static float FTPERMIN_TO_MPERSEC = 0.03125*0.3048/60.0; /* feet/min to
+                                                          meter/sec */
+static float TRATE_SCALE =         3.05e-5; /* Honeywell scale for track
+                                               rate */ 
+static float PRATE_SCALE =         0.0039; /* Honeywell scale for pitch
+                                              rate */ 
+static float HEAD_CORR =           0.0; /* Correction to heading for box
+                                                  alignment */
+
 int convert_iru(long num_words, long *where_data_is, struct ins_data *goes)
 {
 /* Define the iru latency in milliseconds, remember that the latency (called
@@ -34,36 +58,20 @@ int convert_iru(long num_words, long *where_data_is, struct ins_data *goes)
 */
 #define IRU_LATENCY 50
 
-/* Define conversion factors from IRU units to meteorological units, note
-   that these factors will also include the scale factors given by
-   Honeywell to convert the binary words to standard IRU units */ 
-
-#define PIRAD_TO_DEG          9.54e-7*180.0    /* Pirads to degrees */
-#define WKNOTS_TO_MPERSEC     2.44e-4*0.51449  /* knots to m/s (winds) */
-#define VKNOTS_TO_MPERSEC     0.0039*0.51449   /* knots to m/s (velocities) */
-#define FEET_TO_KM            0.125*0.0003048  /* feet to kilometers */
-#define GS_TO_MPERSEC2        1.22e-4*9.7959   /* G's to meter/sec2 */
-#define FTPERSEC_TO_MPERSEC   2.44e-4*0.3048   /* feet/sec to meter/sec */
-#define FTPERMIN_TO_MPERSEC   0.03125*0.3048/60.   /* feet/min to meter/sec */
-#define TRATE_SCALE           3.05e-5     /* Honeywell scale for track rate */ 
-#define PRATE_SCALE           0.0039      /* Honeywell scale for pitch rate */ 
-#define HEAD_CORR             0.0              /* Correction to heading for box
-                                                  alignment */
-
 long i, data, label, msecs_today, msecs_this_data;
-char hr,min,sec,today,mon,yr;
-short msec,jday,test;
+char hour,minute,second,today,month,year;
+short millisec,julday,test;
 
 /* Read the time from the time of day clock */
 
-get_time(&hr,&min,&sec,&msec,&jday,&mon,&today,&yr);
-goes->msec_longitude[msec_longitude_indx] = msec;
-goes->sec_longitude[msec_longitude_indx] = sec;
+get_time(&hour,&minute,&second,&millisec,&julday,&month,&today,&year);
+goes->msec_longitude[msec_longitude_indx] = millisec;
+goes->sec_longitude[msec_longitude_indx] = second;
 msec_longitude_indx++;
 if(msec_longitude_indx >= 5) msec_longitude_indx = 0;
-last_iru_data.seconds = sec;
-last_iru_data.msec_longitude = msec;
-msecs_today = msec + 1000 * (sec + 60 * (min + 60 * hr));
+last_iru_data.seconds = second;
+last_iru_data.msec_longitude = millisec;
+msecs_today = millisec + 1000 * (second + 60 * (minute + 60 * hour));
 
 
 /* Clear the ARINC errors bit from the status word */
@@ -83,128 +91,130 @@ for(i=0; i<num_words; i++)
    integer, the most significant bit of these words is the sign by virtue of
    how the word is passed by the ARINC 429 interface. */
 
-      if(data & (long)0x600 != 0x600) currStatus->iru |= ARINC_ERRORS;
+      if((data & (long)0x600) != 0x600) currStatus->iru |= ARINC_ERRORS;
 
 /*      printf("converting word %d data= %x, label = %x\n",i,data,label); */
 
       switch(label)
 	{
+
 	    case LATITUDE_LAB:
 	    last_iru_data.latitude =
 	      (float)(data>>11)*(float)PIRAD_TO_DEG;
+            if(latitude_indx >= 5) latitude_indx = 0;
 	    goes->latitude[latitude_indx] = last_iru_data.latitude;
 	    latitude_indx++;
-	    if(latitude_indx >= 5) latitude_indx = 0;
 	    break;
 
 	    case LONGITUDE_LAB:
 	    last_iru_data.longitude =
 	      (float)(data>>11)*(float)PIRAD_TO_DEG;
+            if(longitude_indx >= 5) longitude_indx = 0;
 	    goes->longitude[longitude_indx] = last_iru_data.longitude;
 	    longitude_indx++;
-	    if(longitude_indx >= 5) longitude_indx = 0;
 	    break;
 
 	    case WIND_SPEED_LAB:
 	    last_iru_data.wind_speed =
 	      (float)(data>>11)*(float)WKNOTS_TO_MPERSEC;
+            if(wind_speed_indx >= 10) wind_speed_indx = 0;
 	    goes->wind_speed[wind_speed_indx] = last_iru_data.wind_speed;
 	    wind_speed_indx++;
-	    if(wind_speed_indx >= 10) wind_speed_indx = 0;
 	    break;
 
 	    case WIND_DIRECTION_LAB:
 	    last_iru_data.wind_direction =
 	      (float)(data>>11)*(float)PIRAD_TO_DEG;
-	    if(last_iru_data.wind_direction < 0)
+	    if(last_iru_data.wind_direction < 0.0)
 	      last_iru_data.wind_direction += 360.0;
+            if(wind_direction_indx >= 10) wind_direction_indx = 0;
 	    goes->wind_direction[wind_direction_indx] = 
 	      last_iru_data.wind_direction;
 	    wind_direction_indx++;
-	    if(wind_direction_indx >= 10) wind_direction_indx = 0;
 	    break;
 
 	    case NS_VELOCITY_LAB:
 	    last_iru_data.ns_velocity =
 	      (float)(data>>11)*(float)VKNOTS_TO_MPERSEC;
+            if(ns_velocity_indx >= 10) ns_velocity_indx = 0;
 	    goes->ns_velocity[ns_velocity_indx] = last_iru_data.ns_velocity;
 	    ns_velocity_indx++;
-	    if(ns_velocity_indx >= 10) ns_velocity_indx = 0;
 	    break;
 
 	    case EW_VELOCITY_LAB:
 	    last_iru_data.ew_velocity =
 	      (float)(data>>11)*(float)VKNOTS_TO_MPERSEC;
+            if(ew_velocity_indx >= 10) ew_velocity_indx = 0;
 	    goes->ew_velocity[ew_velocity_indx] = last_iru_data.ew_velocity;
 	    ew_velocity_indx++;
-	    if(ew_velocity_indx >= 10) ew_velocity_indx = 0;
 	    break;
 
 	    case HEADING_LAB:
 	    last_iru_data.heading =
 	      (float)(data>>11)*(float)PIRAD_TO_DEG + HEAD_CORR;
-	    if(last_iru_data.heading < 0)
+	    if(last_iru_data.heading < 0.0)
 	      last_iru_data.heading += 360.0;
 	    if(last_iru_data.heading > 360.0)
 	      last_iru_data.heading -= 360.0;
+            if(heading_indx >= 25) heading_indx = 0;
 	    goes->heading[heading_indx] = last_iru_data.heading;
 	    heading_indx++;
-	    if(heading_indx >= 25) heading_indx = 0;
 	    break;
 
 	    case DRIFT_LAB:
 	    last_iru_data.drift =
 	      (float)(data>>11)*(float)PIRAD_TO_DEG;
+            if(drift_indx >= 25) drift_indx = 0;
 	    goes->drift[drift_indx] = last_iru_data.drift;
 	    drift_indx++;
-	    if(drift_indx >= 25) drift_indx = 0;
 	    break;
 
 	    case ALTITUDE_LAB:
 	    last_iru_data.altitude =
 	      (float)(data>>11)*(float)FEET_TO_KM;
+            if(altitude_indx >= 25) altitude_indx = 0;
 	    goes->altitude[altitude_indx] = last_iru_data.altitude;
 	    altitude_indx++;
-	    if(altitude_indx >= 25) altitude_indx = 0;
 	    break;
 
 	    case INERTIAL_VSPEED_LAB:
 	    last_iru_data.inertial_vspeed =
 	      (float)(data>>11)*(float)FTPERMIN_TO_MPERSEC;
+            if(inertial_vspeed_indx >= 25) inertial_vspeed_indx = 0;
 	    goes->inertial_vspeed[inertial_vspeed_indx]
 	      = last_iru_data.inertial_vspeed;
 	    inertial_vspeed_indx++;
-	    if(inertial_vspeed_indx >= 25) inertial_vspeed_indx = 0;
 	    break;
 
 	    case PITCH_LAB:
 	    last_iru_data.pitch =
 	      (float)(data>>11)*(float)PIRAD_TO_DEG;
+            if(pitch_indx >= 50) pitch_indx = 0;
 	    goes->pitch[pitch_indx] = last_iru_data.pitch;
 	    pitch_indx++;
-	    if(pitch_indx >= 50) pitch_indx = 0;
 	    break;
 
 	    case ROLL_LAB:
 	    last_iru_data.roll =
 	      (float)(data>>11)*(float)PIRAD_TO_DEG;
+            if(roll_indx >= 50) roll_indx = 0;
 	    goes->roll[roll_indx] = last_iru_data.roll;
 	    roll_indx++;
-	    if(roll_indx >= 50) roll_indx = 0;
 	    break;
 
 	    case INTEG_VERT_ACC_LAB:
 	    last_iru_data.integ_vert_acc =
 	      (float)(data>>11)*(float)FTPERSEC_TO_MPERSEC;
+            if(integ_vert_acc_indx >= 50) integ_vert_acc_indx = 0;
 	    goes->integ_vert_acc[integ_vert_acc_indx]
 	      = last_iru_data.integ_vert_acc;
 	    integ_vert_acc_indx++;
-	    if(integ_vert_acc_indx >= 50) integ_vert_acc_indx = 0;
 	    break;
 
 	    case VERT_ACC_LAB:
 	    last_iru_data.vert_acc =
 	      (float)(data>>11)*(float)GS_TO_MPERSEC2;
+            if(vert_acc_indx >= 50) vert_acc_indx = 0;
 	    goes->vert_acc[vert_acc_indx] = last_iru_data.vert_acc;
 
 	    /* Vertical acceleration is the last 50 Hertz data value, that is
@@ -225,23 +235,22 @@ for(i=0; i<num_words; i++)
 	    fill_platform(msecs_this_data);
 
 	    vert_acc_indx++;
-	    if(vert_acc_indx >= 50) vert_acc_indx = 0;
 	    break;
 
 	    case TRACK_RATE_LAB:
 	    last_iru_data.track_rate =
 	      (float)(data>>11)*(float)TRATE_SCALE;
+            if(track_rate_indx >= 50) track_rate_indx = 0;
 	    goes->track_rate[track_rate_indx] = last_iru_data.track_rate;
 	    track_rate_indx++;
-	    if(track_rate_indx >= 50) track_rate_indx = 0;
 	    break;
 
 	    case PITCH_RATE_LAB:
 	    last_iru_data.pitch_rate =
 	      (float)(data>>11)*(float)PRATE_SCALE;
+            if(pitch_rate_indx >= 50) pitch_rate_indx = 0;
 	    goes->pitch_rate[pitch_rate_indx] = last_iru_data.pitch_rate;
 	    pitch_rate_indx++;
-	    if(pitch_rate_indx >= 50) pitch_rate_indx = 0;
 	    break;
 
 	  default:
