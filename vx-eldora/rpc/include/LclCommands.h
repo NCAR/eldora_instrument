52c97e1c4d907bb2ee34315c89a40e06d01bbfc1
Rich Neitzel <rich@ucar.edu>
1994-09-06 16:14:10 +0000

    Many changes.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1256 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/rpc/include/LclCommands.h
+++ vx-eldora/rpc/include/LclCommands.h
@@ -9,6 +9,9 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.1  1992/06/25  17:39:01  thor
+ * Initial revision
+ *
  *
  *
  * description:
@@ -21,12 +24,46 @@
 #define DC_1       0x20
 #define DC_2       0x40
 #define DC_3       0x80
-#define FFT        0x100
-#define FFT_1      0x200
-#define FFT_2      0x400
-#define FFT_3      0x800
-#define SLOW_CAL   0x1000
+#define AD_CHAN_LO 0x100
+#define AD_CHAN_HI 0x200
+#define FFT        0x400
+#define FFT_1      0x800
+#define FFT_2      0x1000
+#define FFT_3      0x2000
+#define SLOW_CAL   0x4000
+#define RESYNC     0x8000
 #define FAST_CAL   0x10000
+#define LOAD       0x20000
+#define DC_NULL    0xFFFFFC0F
+#define FFT_NULL   0xFFFFC7FF
 
+/* TASS defines */
+
+#define TS_ON           0x40000     /* Collect time series data */
+
+#define TS_OFF          0x80000     /* Turn off time series data collection */
+
+/* Status Block Defines */
+
+#define DSP_LOAD     0x1
+#define END_OF_BEAM  0x2
+#define MID_BEAM     0x4
+#define DC_BUSY      0x8
+#define DC_STAT      0x10
+#define DP_SYNC      0x20
+#define COLL_SYNC    0x40
+#define DC_BUSY_MASK 0xF7
+#define DC_STAT_MASK 0xEF
+#define INIT_MCPL    0x1
+#define MAD_XFER     0x2
+#define INIT_TPB     0x4
+#define BROADCAST    0x8
+#define DP1          0x10
+#define DP2          0x20
+#define RP           0x40
+#define GP           0x80
+ 
 #endif /* INCLclCommandsh */
+
+
 
--- vx-eldora/rpc/include/RadarGbls.h
+++ vx-eldora/rpc/include/RadarGbls.h
@@ -9,6 +9,9 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.2  1993/07/23  15:00:22  eric
+ * Added TASS stuff.
+ *
  * Revision 1.1  1992/06/25  17:39:03  thor
  * Initial revision
  *
@@ -33,7 +36,6 @@
 
 struct RadarCommand {
     u_long cmd;
-    u_long count;
     int dc_points;
 #ifdef TASS
     TSACQ tass_info;
@@ -41,7 +43,6 @@ struct RadarCommand {
 };
 
 struct RadarStatus {
-    u_long count;
     char rp7;
     char mcpl;
     short status;
@@ -51,19 +52,18 @@ struct RadarStatus {
 #ifdef OK_RPC
 typedef struct RadarCommand RadarCommand;
 typedef struct RadarStatus RadarStatus;
-#endif /* OK_RPC */
 
 #ifndef UNIX
 #include "semLib.h"
 
 SCOPE RadarStatus *currStatus;
-RP7_SCOPE  int fill_busy, fill_ts_array, pts, ts_flag, ts_samps; /* array fill globals */
-RP7_SCOPE  int fft_flag, radar_fore_aft, send_ray[3][2], send_nav, send_ads;
+RP7_SCOPE  volatile int fill_busy, fill_ts_array, pts, ts_flag, ts_samps; /* array fill globals */
+RP7_SCOPE  volatile int fft_flag, radar_fore_aft, send_ray[3][2], send_nav, send_ads;
 RP7_SCOPE  unsigned long ad_freq;
-RP7_SCOPE  long  curr_ray_add[3][2], curr_mailbox_add[3][2], curr_nav_add; 
-RP7_SCOPE  long  curr_nav_mailbox_add, curr_ads_add, curr_ads_mailbox_add;
-RP7_SCOPE  float *a_pntr;
-RP7_SCOPE  int  load, stop, reboot, resync, dc_removal, ts_freq; /* rpc globals */
+RP7_SCOPE  volatile long  curr_ray_add[3][2], curr_mailbox_add[3][2], curr_nav_add; 
+RP7_SCOPE  volatile long  curr_nav_mailbox_add, curr_ads_add, curr_ads_mailbox_add;
+RP7_SCOPE  volatile float *a_pntr;
+RP7_SCOPE  volatile int  load, stop, reboot, resync, dc_removal, ts_freq; /* rpc globals */
 RP7_SCOPE SEM_ID bim_int0_sem;
 RP7_SCOPE SEM_ID bim_int1_sem;
 RP7_SCOPE SEM_ID bim_int2_sem;
@@ -77,9 +77,10 @@ RP7_SCOPE  unsigned char  *bim_cr0, *bim_cr1, *bim_cr2, *bim_cr3;
 RP7_SCOPE  char ad_chan;
 #ifdef TASS
 RP7_SCOPE TSACQ *tass_ptr;
+RP7_SCOPE int ts_acq_on;
 #endif /* TASS */
 #endif /* UNIX */
-
+#endif /* OK_RPC */
 #endif /* INCRadarGblsh */
 
 
