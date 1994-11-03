eda661ca891c8e3b1d3078d9cb6eb829a8808f68
Rich Neitzel <rich@ucar.edu>
1994-11-03 13:36:02 +0000

    Added code to properly handle long/short PRTs.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1318 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/display/Vert/Vert.cc
+++ vx-eldora/display/Vert/Vert.cc
@@ -9,6 +9,9 @@
 // revision history
 // ----------------
 // $Log$
+// Revision 1.4  1994/11/01  17:27:06  thor
+// Whoops, forgot VS & VL.
+//
 // Revision 1.3  1994/11/01  15:31:35  thor
 // Added dual PRT support.
 //
@@ -120,24 +123,29 @@ void Vert::reset(FAST Header *hdr, FAST DispCommand *cmd)
                           (param == ParamNames::VEL_LONG) ||
                           (param == ParamNames::VEL_SHORT))
                         {
-                            if (max[0] == 0.0 && min[0] == 0.0)
-                              {
-                                  if (rd->num_ipps_trans == 2)
-                                    {
-                                        float range = 75.0 /
-                                            (rd->freq1 *
-                                             (rd->interpulse_per2
-                                              - rd->interpulse_per1));
-
-                                        max[0] = range;
-                                        min[0] = -range;
-                                    }
+                          if (max[0] == 0.0 && min[0] == 0.0)
+                            {
+                              float range;
+                        
+                              if (rd->num_ipps_trans == 2)
+                                {
+                                  if (param == ParamNames::VELOCITY)
+                                    range = 75.0 /
+                                        (rd->freq1 * (rd->interpulse_per2
+                                                      - rd->interpulse_per1));
+                                  else if (param == ParamNames::VEL_LONG)
+                                    range = 75.0 /
+                                        (rd->freq1 * rd->interpulse_per2);
                                   else
-                                    {
-                                        max[0] = rd->eff_unamb_vel;
-                                        min[0] = -(rd->eff_unamb_vel);
-                                    }
-                              }
+                                    range = 75.0 /
+                                        (rd->freq1 * rd->interpulse_per1);
+                                }
+                              else
+                                range = rd->eff_unamb_vel;
+
+                              max[0] = range;
+                              min[0] = -range;
+                            }
                         }
                       break;
                   }
@@ -166,24 +174,29 @@ void Vert::reset(FAST Header *hdr, FAST DispCommand *cmd)
                           (param == ParamNames::VEL_LONG) ||
                           (param == ParamNames::VEL_SHORT))
                         {
-                            if (max[1] == 0.0 && min[1] == 0.0)
-                              {
-                                  if (rd->num_ipps_trans == 2)
-                                    {
-                                        float range = 75.0 /
-                                            (rd->freq1 *
-                                             (rd->interpulse_per2
-                                              - rd->interpulse_per1));
-
-                                        max[1] = range;
-                                        min[1] = -range;
-                                    }
+                          if (max[1] == 0.0 && min[1] == 0.0)
+                            {
+                              float range;
+                        
+                              if (rd->num_ipps_trans == 2)
+                                {
+                                  if (param == ParamNames::VELOCITY)
+                                    range = 75.0 /
+                                        (rd->freq1 * (rd->interpulse_per2
+                                                      - rd->interpulse_per1));
+                                  else if (param == ParamNames::VEL_LONG)
+                                    range = 75.0 /
+                                        (rd->freq1 * rd->interpulse_per2);
                                   else
-                                    {
-                                        max[1] = rd->eff_unamb_vel;
-                                        min[1] = -(rd->eff_unamb_vel);
-                                    }
-                              }
+                                    range = 75.0 /
+                                        (rd->freq1 * rd->interpulse_per1);
+                                }
+                              else
+                                range = rd->eff_unamb_vel;
+
+                              max[1] = range;
+                              min[1] = -range;
+                            }
                         }
                       break;
                   }
@@ -212,24 +225,29 @@ void Vert::reset(FAST Header *hdr, FAST DispCommand *cmd)
                           (param == ParamNames::VEL_LONG) ||
                           (param == ParamNames::VEL_SHORT))
                         {
-                            if (max[2] == 0.0 && min[2] == 0.0)
-                              {
-                                  if (rd->num_ipps_trans == 2)
-                                    {
-                                        float range = 75.0 /
-                                            (rd->freq1 *
-                                             (rd->interpulse_per2
-                                              - rd->interpulse_per1));
-
-                                        max[2] = range;
-                                        min[2] = -range;
-                                    }
+                          if (max[2] == 0.0 && min[2] == 0.0)
+                            {
+                              float range;
+                        
+                              if (rd->num_ipps_trans == 2)
+                                {
+                                  if (param == ParamNames::VELOCITY)
+                                    range = 75.0 /
+                                        (rd->freq1 * (rd->interpulse_per2
+                                                      - rd->interpulse_per1));
+                                  else if (param == ParamNames::VEL_LONG)
+                                    range = 75.0 /
+                                        (rd->freq1 * rd->interpulse_per2);
                                   else
-                                    {
-                                        max[2] = rd->eff_unamb_vel;
-                                        min[2] = -(rd->eff_unamb_vel);
-                                    }
-                              }
+                                    range = 75.0 /
+                                        (rd->freq1 * rd->interpulse_per1);
+                                }
+                              else
+                                range = rd->eff_unamb_vel;
+
+                              max[2] = range;
+                              min[2] = -range;
+                            }
                         }
                       break;
                   }
