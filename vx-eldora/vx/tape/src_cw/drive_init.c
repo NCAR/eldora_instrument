998b5e18312a2879ab914748df10f2044b08274b
Craig Walther <unknown@ucar.edu>
1994-11-14 17:52:23 +0000

    Modified timeout
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1333 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/vx/tape/src_cw/drive_init.c
+++ vx-eldora/vx/tape/src_cw/drive_init.c
@@ -2,41 +2,44 @@
  *	$Id$
  *
  *	Module:drive_init.c		 
- *	Original Author: 
+ *	Original Author: Reif Heck 
  *      Copywrited by the National Center for Atmospheric Research
  *	Date:		 $Date$
  *
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.1  1994/01/06  21:31:55  craig
+ * Initial revision
+ *
  * Revision 1.1  1993/09/22  17:03:38  reif
  * Initial revision
  *
- * description:
+ * description:  Waits for drive to come ready, then sets it mode select
+ *               parameters.
  *        
  */
 static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
 #include <cipincl.h>
 
-int drive_init(char drv_num)
+void drive_init(unsigned char drv_num)
 {
-int starting_amnt;
+
 unsigned char drv_stat=0;
 int error[4];
 int number_ints=0;
-int i;
 
 drv_stat=tst_unt_rdy(drv_num);  /* SEE IF EXABYTE IS READY */
+
 while(drv_stat!=0x80)
   {
       drv_stat=tst_unt_rdy(drv_num);  /* SEE IF EXABYTE IS READY */
-      loggerEvent("ERROR= %X ON ",&drv_stat,1);
-      loggerEvent("DRV%d\n",&drv_num,1);
-      taskDelay(1000);
+      printf("Waiting for SCSI Drive %2d to come ready\n",drv_num);
+      taskDelay(100);
   }
 exb_cmds(MODE_SELECT,MD_SEL,drv_num);  /* CONFIGURE EXABYTE DRIVEs */
-starting_amnt=start_amnt(drv_num);
-return(starting_amnt);
+
+return;
 }
 
 
