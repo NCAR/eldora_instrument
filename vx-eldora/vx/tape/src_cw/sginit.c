dc65599c0f661617de5f159d9d3d01aac7ed6b29
Craig Walther <unknown@ucar.edu>
1995-03-31 22:50:11 +0000

    Don't know
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1371 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/vx/tape/src_cw/sginit.c
+++ vx-eldora/vx/tape/src_cw/sginit.c
@@ -9,6 +9,9 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.1  1994/01/06  21:31:36  craig
+ * Initial revision
+ *
  * Revision 1.2  1993/09/24  22:14:00  reif
  * *** empty log message ***
  *
@@ -41,7 +44,7 @@ void sg_init()
 register int i;
  for(i=0; i<30; i++)
    {
-       rad_sg[i].next_rad_sg_addr=0x00000000;
+       rad_sg[i].next_sg_addr=0x00000000;
        rad_sg[i].am_and_dat_len1=0x00000000;
        rad_sg[i].dat_addr1=0x00000000;
        rad_sg[i].am_and_dat_len2=0x00000000;
@@ -61,7 +64,7 @@ register int i;
    }
 for(i=0; i<5; i++)
    {
-       nav_sg[i].next_nav_sg_addr=0x00000000;
+       nav_sg[i].next_sg_addr=0x00000000;
        nav_sg[i].am_and_dat_len1=0x00000000;
        nav_sg[i].dat_addr1=0x00000000;
        nav_sg[i].am_and_dat_len2=0x00000000;
@@ -81,7 +84,7 @@ for(i=0; i<5; i++)
    }
 for(i=0; i<5; i++)
    {
-       ads_sg[i].next_ads_sg_addr=0x00000000;
+       ads_sg[i].next_sg_addr=0x00000000;
        ads_sg[i].am_and_dat_len1=0x00000000;
        ads_sg[i].dat_addr1=0x00000000;
        ads_sg[i].am_and_dat_len2=0x00000000;
@@ -100,3 +103,6 @@ for(i=0; i<5; i++)
        ads_sg[i].dat_addr8=0x00000000;
    }
 }
+
+
+
