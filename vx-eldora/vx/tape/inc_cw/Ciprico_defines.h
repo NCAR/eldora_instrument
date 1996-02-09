c8291093f5102eeb0d434cc46e377febdbf71418
Craig Walther <unknown@ucar.edu>
1996-02-09 17:44:40 +0000

    made maximum buffer size 32767 instead of 32768
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1384 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/vx/tape/inc_cw/Ciprico_defines.h
+++ vx-eldora/vx/tape/inc_cw/Ciprico_defines.h
@@ -9,14 +9,8 @@
  * revision history
  * ----------------
  * $Log$
- *
- * Revision 1.2  1992/10/17  21:38:07  reif
- * changed for global structures
- *
- * Revision 1.1  1992/08/14  18:08:58  reif
- * Initial revision
- *
- *
+ * Revision 1.3  1995/01/26  16:22:24  craig
+ * *** empty log message ***
  *
  * description:
  *        
@@ -24,68 +18,81 @@
 #ifndef INCCiprico_definesh
 #define INCCiprico_definesh
 
-/* Exabyte command identifiers: */
+/* Ciprico command identifiers: (array indecies for parameter blocks)*/
 
-#define TEST_UNIT_READY 0 
-#define REWND           1 
-#define REQUEST_SENSE   2   
-#define READ_BLK_LIMS   3
-#define TAPE_READ       4  
-#define TAPE_WRITE      5 
-#define WRITE_FILEMARK  6 
-#define SPACE           7 
-#define INQUIRY         8 
-#define MODE_SELECT     9 
-#define RESERVE_UNIT    10  
-#define RELEASE_UNIT    11  
-#define ERASE           12 
-#define MODE_SENSE      13 
-#define UNLOAD          14 
-#define MEDIA_ACCESS    15
-#define LOCATE          16 
-#define VERIFY          17
-#define READ_POSIT      18
+#define TEST_UNIT_READY  0    /* Test Unit ready */ 
+#define REWND            1    /* rewind */
+#define REQUEST_SENSE    2    /* request Sense */
+#define READ_BLK_LIMS    3    /* Read Block Limits */
+#define TAPE_READ        4    /* Read tape */
+#define TAPE_WRITE       5    /* Write tape */
+#define WRITE_FILEMARK   6    /* Write filemark */ 
+#define SPACE            7 
+#define INQUIRY          8 
+#define MODE_SELECT      9 
+#define LOAD             10  
+#define RELEASE_UNIT     11  
+#define ERASE            12 
+#define MODE_SENSE       13 
+#define UNLOAD           14 
+#define MEDIA_ACCESS     15
+#define LOCATE           16 
+#define VERIFY           17
+#define READ_POSIT       18
 #define SEND_DIAGNOSTICS 19
-#define RCV_DIAGNOSTICS 20
+#define RCV_DIAGNOSTICS  20
+#define LOG_SELECT       21
+#define LOG_SENSE        22
+#define LOG_SENSE_SPGS   23
+#define LAST_CMD_ID      23
 
 /* Rimfire command identifiers: */
 
-#define UNIT_OPS    21 
-#define GEN_OPS     22 
+#define UNIT_OPS    24 
+#define GEN_OPS     25
+#define LAST_CIP_CMD 25
 
-/* EXABYTE COMMAND OPCODES */
-#define TPUR 0x00
-#define RWND 0x01
-#define RQST_SNS 0x03
-#define RD_BLK_LMTS 0x05
-#define TP_RD 0x08
-#define TP_WRT 0x0A
-#define WRT_FLMK 0x10
-#define SP 0x11
-#define INQ 0x12
-#define VFY 0x13
-#define MD_SEL 0x15
-#define RSV_UT 0x16
-#define REL_UT 0x17
-#define ERS 0x19
-#define MD_SNS 0x1A
-#define ULD 0x1B
-#define MED_RMV 0x1E
-#define LOC 0x2B
-#define RD_POS 0x34
-#define UOPS 0x08
-#define GOPS 0x07
-#define RCV_DIAGS 0x1C
-#define SND_DIAGS 0x1D
+/* SCSI COMMAND OPCODES */
+
+#define TPUR 0x00        /* Test Unit Ready */
+#define RWND 0x01        /* Rewind */
+#define RQST_SNS 0x03    /* Request sense */
+#define RD_BLK_LMTS 0x05 /* Read Bloc k Limits */
+#define TP_RD 0x08       /* Tape read */
+#define TP_WRT 0x0A      /* Tape write */
+#define WRT_FLMK 0x10    /* Write a Filemark */
+#define SP 0x11          /* Space */
+#define INQ 0x12         /* Inquiry */
+#define VFY 0x13         /* Verify */
+#define MD_SEL 0x15      /* Mode Select */
+#define RSV_UT 0x16      /* Reserve Unit */
+#define REL_UT 0x17      /* release Unit */
+#define ERS 0x19         /* Erase */
+#define MD_SNS 0x1A      /* Mode Sense */
+#define ULD 0x1B         /* Load - Unload */
+#define LD_ULD 0x1B      /* Load - Unload */
+#define MED_RMV 0x1E     /* Prevent/allow medium removal */
+#define LOC 0x2B         /* Locate */
+#define RD_POS 0x34      /* Read position */
+#define RCV_DIAGS 0x1C   /* Receive Diagnostics */
+#define SND_DIAGS 0x1D   /* Send diagnostics */
+#define LOG_SEL   0x4C   /* Log Select */
+#define LOG_SEN   0x4D   /* Log Sence */
+
+/* Ciprico command opcodes */
+
+#define UOPS 0x08        /* Unit ops */
+#define GOPS 0x07        /* General ops */
 
 /******************* MISC DEFINES ***************************/
-#define CIP_BASE 0xFFFFEF00
-#define CIP_ADDR_BUFF 0x00
-#define CIP_CHAN_ATTN 0x08
-#define CIP_BD_STAT 0x10
-#define CIP_BD_RST 0x18
+#define CIP_BASE 0xFFFFEF00     /* base address ciprico card */
+#define CIP_ADDR_BUFF 0x00      /* Offset to the address buffer */
+#define CIP_CHAN_ATTN 0x08      /* Offset to the channel attention location */
+#define CIP_BD_STAT 0x10        /* Offset to the Ciprico status register */
+#define CIP_BD_RST 0x18         /* Offset to the Ciprico reset register */
+
 #define RSVD 0x00
-#define LEOT 0x40
+#define LEOT 0x0D
 #define LBOT 0x01 
 #define BLOCKS 0x00
 #define EOFS 0x01
@@ -101,12 +108,16 @@
 #define DATA_RECS 0x2001800
 #define MAX_TRIES 70
 #define MAX_COUNT 1000
-#define MAX_BLK_SZ 32768
-#define GOP_THROTTLE 0xA0
-#define GOP_OP_FLAGS 0x01
-#define GOP_OWN_ID 0x07
-#define GOP_TID 0xFF
-#define GOP_RESV 0x00000000
+#define MAX_BLK_SZ 32767
+
+/* General options commands */
+
+#define GOP_THROTTLE 0xA0    /* 32 bytes transfered before releasing VMEBus */ 
+#define GOP_OP_FLAGS 0x01    /* Allow for Disconnect/reselect,
+				no parity, no block mode */
+#define GOP_OWN_ID 0x07      /* This is SCSI ID of Ciprico board */
+#define GOP_TID 0xFF         /* Target ID, 0xFF indicates Ciprico board only */
+#define GOP_RESV 0x00000000 
 #define UOP_DTO1 0xFD
 #define UOP_DTO2 0xE8
 #define UOP_UNIT_ID0 0x00 
@@ -114,9 +125,11 @@
 #define UOP_TID 0xFF
 #define UOP_VME 0x07F80000
 #define UOP_CNT 0x00000002
+#define TOTAL_TAPE_SIZE 10000.0   /* In mega bytes */
 #define MAX_MEM 4096
 #define SCATTER_GATHER 0x69
 #define HEADER 0x68
+#define DATA 0x68
 #define DEG_TO_RAD 0.01745
 #define MAX_FILE_SIZE 95000000
 #define ENDING_PERCENT 5.0
@@ -156,10 +169,10 @@ struct param_block {
     };
 typedef struct param_block PARMBLK;
 
-struct mode_select{ /* mode_select parameter block */
+struct mode_struct{ /* mode_select parameter block */
     unsigned char res1;
     unsigned char res2;
-    unsigned char buf_mode;  /* buffer mode,bits 4-6,0=no buf,1=buf*/
+    unsigned char buf_mode_spd;  /* buffer mode and speed */
     unsigned char blk_desc_len; /* block descriptor length */
     unsigned char density;
     unsigned char num_blks_msb;
@@ -169,31 +182,22 @@ struct mode_select{ /* mode_select parameter block */
     unsigned char blk_len_msb;
     unsigned char blk_len_mid;
     unsigned char blk_len_lsb;
-    unsigned char vend_parms;
+    unsigned char page_code;   /* Only page is device configuration */
+    unsigned char page_len;
+    unsigned char CAP_CAF_fmt;
+    unsigned char active_par;
+    unsigned char write_full_ratio;
+    unsigned char read_full_ratio;
+    unsigned char write_dly_msb;
+    unsigned char write_dly_lsb;
+    unsigned char DBR_other_stuff;
+    unsigned char gap_size;
+    unsigned char EOD_EGG_SEW;
+    unsigned char buff_size_msb;
+    unsigned char buff_size_mid;
+    unsigned char buff_size_lsb;
+    unsigned char data_compress;
     unsigned char res4;
-    unsigned char motion_thr;
-    unsigned char recon_thr;
-    unsigned char gap_thr;
-    }; 
-
-struct mode_sense{ /* mode sense parameter block */
-    unsigned char sense_data_len;
-    unsigned char medium;
-    unsigned char WP_buf_mode;  /* buffer mode,bits 4-6,0=no buf,1=buf*/
-    unsigned char blk_desc_len; /* block descriptor length */
-    unsigned char density;
-    unsigned char num_blks_msb;
-    unsigned char num_blks_mid;
-    unsigned char num_blks_lsb;
-    unsigned char res3;
-    unsigned char blk_len_msb;
-    unsigned char blk_len_mid;
-    unsigned char blk_len_lsb;
-    unsigned char vend_parms;
-    unsigned char res4;
-    unsigned char motion_thr;
-    unsigned char recon_thr;
-    unsigned char gap_thr;
     };
 
 struct request_sense{ /* request sense parameter structure */
@@ -206,30 +210,22 @@ struct request_sense{ /* request sense parameter structure */
     unsigned char info_mid_low;
     unsigned char info_lsb;
     unsigned char addit_sense_len;
-    unsigned char res1;
-    unsigned char res2;
-    unsigned char res3;
-    unsigned char undr_ovr_cntr;
+    unsigned char cmnd_spcfc_msb;
+    unsigned char cmnd_spcfc_mid_high;
+    unsigned char cmnd_spcfc_mid_low;
+    unsigned char cmnd_spcfc_lsb;
     unsigned char addit_sense_code;
     unsigned char addit_sense_qual;
-    unsigned char res5;
-    unsigned char res6;
-    unsigned char err_ctr_msb;
-    unsigned char err_ctr_mid;
-    unsigned char err_ctr_lsb;
-    unsigned char stat1;
-    unsigned char stat2;
-    unsigned char stat3;
-    unsigned char res7;
-    unsigned char rem_tape_msb;
-    unsigned char rem_tape_mid;
-    unsigned char rem_tape_lsb;
-    unsigned char trkng_retry_cntr;
-    unsigned char rd_wrt_retry_cntr;
-    unsigned char fault_code;
+    unsigned char sub_assembly_code;
+    unsigned char SKSV_CD_BPV_bitpntr;
+    unsigned char fld_pntr_msb;
+    unsigned char fld_pntr_lsb;
+    unsigned char internal_stat;
 };
-struct rad_scatter_gather {
-    unsigned int next_rad_sg_addr;
+
+
+struct scatter_gather {
+    unsigned int next_sg_addr;
     unsigned int am_and_dat_len1;
     unsigned int dat_addr1;
     unsigned int am_and_dat_len2;
@@ -247,43 +243,76 @@ struct rad_scatter_gather {
     unsigned int am_and_dat_len8;
     unsigned int dat_addr8;
 };
-struct nav_scatter_gather {
-    unsigned int next_nav_sg_addr;
-    unsigned int am_and_dat_len1;
-    unsigned int dat_addr1;
-    unsigned int am_and_dat_len2;
-    unsigned int dat_addr2;
-    unsigned int am_and_dat_len3;
-    unsigned int dat_addr3;
-    unsigned int am_and_dat_len4;
-    unsigned int dat_addr4;
-    unsigned int am_and_dat_len5;
-    unsigned int dat_addr5;
-    unsigned int am_and_dat_len6;
-    unsigned int dat_addr6;
-    unsigned int am_and_dat_len7;
-    unsigned int dat_addr7;
-    unsigned int am_and_dat_len8;
-    unsigned int dat_addr8;
+
+struct receive_diags{
+    unsigned char page_code;
+    char resvd;
+    char page_length_msb;
+    char page_length_lsb;
+    char diag_params1;
+    char diag_params2;
+    char diag_params3;
+    char diag_params4;
+    char diag_params5;
+    char diag_params6;
+    char diag_params7;
+    char diag_params8;
 };
-struct ads_scatter_gather {
-    unsigned int next_ads_sg_addr;
-    unsigned int am_and_dat_len1;
-    unsigned int dat_addr1;
-    unsigned int am_and_dat_len2;
-    unsigned int dat_addr2;
-    unsigned int am_and_dat_len3;
-    unsigned int dat_addr3;
-    unsigned int am_and_dat_len4;
-    unsigned int dat_addr4;
-    unsigned int am_and_dat_len5;
-    unsigned int dat_addr5;
-    unsigned int am_and_dat_len6;
-    unsigned int dat_addr6;
-    unsigned int am_and_dat_len7;
-    unsigned int dat_addr7;
-    unsigned int am_and_dat_len8;
-    unsigned int dat_addr8;
+
+struct log_page_head{
+    unsigned char page_code;
+    char resvd1;
+    char page_length_msb;
+    char page_length_lsb;
+};
+
+struct log_param_short{
+    char code_msb;
+    char code_lsb;
+    char misc;
+    char length;
+    char pvalue_msb;
+    char pvalue_midh;
+    char pvalue_midl;
+    char pvalue_lsb;
+};
+
+struct log_param_long{
+    char code_msb;
+    char code_lsb;
+    char misc;
+    char length;
+    char pvalue0;
+    char pvalue1;
+    char pvalue2;
+    char pvalue3;
+    char pvalue4;
+    char pvalue5;
+    char pvalue6;
+    char pvalue7;
+};
+
+struct log_pg{
+    struct log_page_head head;
+    struct log_param_short sub_delay;
+    struct log_param_short pos_delay;
+    struct log_param_short total_rewrites;
+    struct log_param_short total_errors_cor;
+    struct log_param_short total_proc;
+    struct log_param_long  total_bytes;
+    struct log_param_short total_errors_uncor;
+    struct log_param_short vendor_unique;
+};
+
+struct supported_pgs{
+    char page_code;
+    char length_msb;
+    char length_lsb;
+    char page1;
+    char page2;
+    char page3;
+    char page4;
+    char page5;
 };
 
 #endif /* INC */
