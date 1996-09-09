ac383bafb72ce9dc3f6e8f931a4d8152c5ff4307
Eric Loew <ericloew@ucar.edu>
1996-09-09 22:52:19 +0000

    *** empty log message ***
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1421 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/rpc/include/RadarGbls.h
+++ vx-eldora/rpc/include/RadarGbls.h
@@ -9,6 +9,9 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.3  1994/09/06  16:14:10  thor
+ * Many changes.
+ *
  * Revision 1.2  1993/07/23  15:00:22  eric
  * Added TASS stuff.
  *
@@ -33,6 +36,7 @@
 #ifdef TASS
 #include "TSacq.h"
 #endif
+#include "Header.h"
 
 struct RadarCommand {
     u_long cmd;
@@ -64,6 +68,13 @@ RP7_SCOPE  volatile long  curr_ray_add[3][2], curr_mailbox_add[3][2], curr_nav_a
 RP7_SCOPE  volatile long  curr_nav_mailbox_add, curr_ads_add, curr_ads_mailbox_add;
 RP7_SCOPE  volatile float *a_pntr;
 RP7_SCOPE  volatile int  load, stop, reboot, resync, dc_removal, ts_freq; /* rpc globals */
+RP7_SCOPE volatile int Run_flag;
+RP7_SCOPE CELLSPACING *cs;
+RP7_SCOPE RADARDESC *rdsc;
+RP7_SCOPE WAVEFORM *wvfm;
+RP7_SCOPE PARAMETER *prm;
+RP7_SCOPE FIELDRADAR *fldrdr;
+
 RP7_SCOPE SEM_ID bim_int0_sem;
 RP7_SCOPE SEM_ID bim_int1_sem;
 RP7_SCOPE SEM_ID bim_int2_sem;
