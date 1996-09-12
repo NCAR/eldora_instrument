eb68ace52e309d224579cd58a0672af62bde5a21
Craig Walther <unknown@ucar.edu>
1996-09-12 17:22:18 +0000

    completed code
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1425 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/vx/hskp/src/main/update_testpulse.c
+++ vx-eldora/vx/hskp/src/main/update_testpulse.c
@@ -31,28 +31,59 @@ unsigned long T;
 double frequency, temp;
 unsigned char unitnum, ecbaddr,test,attenuation, foraft;
 int timeout;
+float dbz, log_power, received_lna_pwr, injected_lna_power, received_vel;
+float diff;
+
 
 /* Zero the testpulse dwell counter */
 tp_dwell_count = 0;
 
-/* Check to see if I am in control of the fore test pulse, or if the
-   fore radar processor is in control */
+/* Check to see if the previous testpulse was within the proper error band */
 
-if(fore_vmehndshk->tpulse_flg != 3)
-  {
-      /* I am in charge of the test pulse */
+dbz = ((fore_dbz_sum / tp_sum_count)) - 280.0 / 8.0;
+log_power = (dbz - fore_lna_radar_const - log_testpulse_range2) / 10.0;
+received_lna_pwr = pow((double)10.0,(double)log_power);
+injected_lna_pwr = fore_tp_level_sum / tp_sum_count + 
+                   ffrad->test_pulse_offset - fore_vmehndshk->tpulse_atten;
+diff = received_lna_pwr - injected_lna_pwr;
+if(diff < 0) diff *= -1;
+if(diff > MAX_TP_PWR_DIFF) printf("FP "); 
+received_vel = ((fore_vel_sum / tp_sum_count) - 8191.0) / fore_vscale;
+diff = received_vel - fore_tp_velocity[tp_freq_offset_count];
+if(diff < 0) diff *= -1;
+if(diff > MAX_TP_VEL_DIFF) printf("FV "); 
 
-     fore_vmehndshk->tpulse_freq_num = tp_freq_count + 1;
-     frequency = fore_half_nyquist[tp_freq_count][tp_freq_offset_count];
-     attenuation = tp_atten;
-     fore_vmehndshk->tpulse_flg = 1;
- }
-else   /* The fore radar processor is in charge of the test pulse */
-  {
-      frequency = fore_vmehndshk->tpulse_freq * 1.e9;
-      attenuation = fore_vmehndshk->tpulse_atten;
-      fore_vmehndshk->tpulse_flg = 4;
-  }
+dbz = ((aft_dbz_sum / tp_sum_count)) - 280.0 / 8.0;
+log_power = (dbz - aft_lna_radar_const - log_testpulse_range2) / 10.0;
+received_lna_pwr = pow((double)10.0,(double)log_power);
+injected_lna_pwr = aft_tp_level_sum / tp_sum_count + 
+                   afrad->test_pulse_offset - aft_vmehndshk->tpulse_atten;
+diff = received_lna_pwr - injected_lna_pwr;
+if(diff < 0) diff *= -1;
+if(diff > MAX_TP_PWR_DIFF) printf("AP "); 
+received_vel = ((aft_vel_sum / tp_sum_count) - 8191.0) / aft_vscale;
+diff = received_vel - aft_tp_velocity[tp_freq_offset_count];
+if(diff < 0) diff *= -1;
+if(diff > MAX_TP_VEL_DIFF) printf("AV "); 
+
+tp_sum_count = 0;
+fore_dbz_sum = 0;
+fore_tp_level_sum = 0;
+aft_dbz_sum = 0;
+aft_tp_level_sum = 0;
+fore_vel_sum = 0;
+aft_vel_sum = 0;
+
+/* Now re-program the fore testpulse with a new value */
+
+fore_vmehndshk->tpulse_freq_num = tp_freq_count + 1;
+frequency = fore_freqs[tp_freq_count] + 
+(double)fore_freq_offset[tp_freq_offset_count];
+printf("Fore frequency: %lf\n",frequency);
+attenuation = tp_atten;
+fore_vmehndshk->tpulse_freq = frequency;
+fore_vmehndshk->tpulse_atten = tp_atten;
+fore_vmehndshk->tpulse_flg = 1;
 
 /* Set the fore test pulse frequency */
 
@@ -66,63 +97,56 @@ nr = 1;
 timeout = 0;
 do
   {
+      if(timeout > 0)taskDelay(1);
       timeout++;
   }while((test = ecbSetDDS(ecbaddr,unitnum,B,T,nr) != 0) &&
-	 timeout < 3000);
+	 timeout < 30);
 
 /* Set the fore testpulse attenuation level */
+
 ecbaddr = ECBATTEN;
 foraft = 1;
 timeout=0;
 do
   {
+      if(timeout > 0)taskDelay(1);
       timeout++;
   }while((test = ecbSetAtten(ecbaddr, foraft, attenuation) != 0) &&
-	  timeout < 3000);  
+	  timeout < 30);  
 
 
 
-/* Check to see if I am in control of the aft test pulse, or if the
-   aft radar processor is in control */
+/* Now re-program the fore testpulse with a new value */
 
-if(aft_vmehndshk->tpulse_flg != 3)
+aft_vmehndshk->tpulse_freq_num = tp_freq_count + 1;
+frequency = aft_freqs[tp_freq_count] +
+(double)aft_freq_offset[tp_freq_offset_count];
+printf("Aft frequency: %lf\n",frequency);
+attenuation = tp_atten;
+aft_vmehndshk->tpulse_atten = tp_atten;
+aft_vmehndshk->tpulse_freq = frequency;
+aft_vmehndshk->tpulse_flg = 1;
+
+/* Update the counters for next time */
+
+tp_freq_count += 1;
+if((tp_freq_count + 1) > fraddes->num_freq_trans)
+{
+  tp_freq_count = 0;
+  tp_atten += TP_ATTEN_STEP;
+  if(tp_atten > TP_ATTEN_MAX)
   {
-      /* I am in charge of the test pulse */
+    tp_atten_start += 1;
+    if(tp_atten_start >= TP_ATTEN_STEP)
+    tp_atten_start = 0;
+    tp_atten = tp_atten_start;
 
-     aft_vmehndshk->tpulse_freq_num = tp_freq_count + 1;
-     frequency = aft_half_nyquist[tp_freq_count][tp_freq_offset_count];
-     attenuation = tp_atten;
-     aft_vmehndshk->tpulse_flg = 1;
+    tp_freq_offset_count += 1;
+    if(tp_freq_offset_count > 6)
+    tp_freq_offset_count = 0;
 
-     /* Update the counters for next time */
-
-     tp_freq_count++;
-     if((tp_freq_count + 1) >= fraddes->num_freq_trans)
-       {
-	   tp_freq_count = 0;
-	   tp_atten += TP_ATTEN_STEP;
-	   if(tp_atten > TP_ATTEN_MAX)
-	     {
-		 tp_atten_start++;
-		 if(tp_atten_start >= TP_ATTEN_STEP)
-		   tp_atten_start = 0;
-		 tp_atten = tp_atten_start;
-
-		 tp_freq_offset_count++;
-		 if(tp_freq_offset_count > 1)
-		   tp_freq_offset_count = 0;
-
-	     }
-       }
-     
- }
-
-else   /* The aft radar processor is in charge of the test pulse */
-  {
-      frequency = aft_vmehndshk->tpulse_freq * 1.e9;
-      attenuation = aft_vmehndshk->tpulse_atten;
-      aft_vmehndshk->tpulse_flg = 4;
   }
+}
 
 /* Set the aft test pulse frequency */
 
@@ -136,18 +160,28 @@ nr = 1;
 timeout = 0;
 do
   {
+      if(timeout > 0)taskDelay(1);
       timeout++;
   }while((test = ecbSetDDS(ecbaddr,unitnum,B,T,nr) != 0) &&
-	 timeout < 3000);
+	 timeout < 30);
 
 /* Set the aft testpulse attenuation level */
+
 ecbaddr = ECBATTEN;
 foraft = 0;
 timeout = 0;
+
 do
   {
+      if(timeout > 0)taskDelay(1);
       timeout++;
   }while((test = ecbSetAtten(ecbaddr, foraft, attenuation) != 0) &&
-	 timeout < 3000);  
+	 timeout < 30);  
 
 }
+
+
+
+
+
+
