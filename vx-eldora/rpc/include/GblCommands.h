1801dcb0fac9558483b0de9a73e081a7428036dc
Rich Neitzel <rich@ucar.edu>
1996-10-09 19:43:42 +0000

    Initial revision
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1429 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

---
+++ vx-eldora/rpc/include/GblCommands.h
@@ -0,0 +1,26 @@
+/*
+ *	$Id$
+ *
+ *	Module:		 GblCommands.h
+ *	Original Author: Richard E. K. Neitzel
+ *      Copywrited by the National Center for Atmospheric Research
+ *	Date:		 $Date$
+ *
+ * revision history
+ * ----------------
+ * $Log$
+ *
+ *
+ * description:
+ *        
+ */
+#ifndef INCGblCommandsh
+#define INCGblCommandsh
+
+#define START   0x1
+#define STOP    0x2
+#define INIT    0x4
+#define REBOOT  0x8
+#define RELOAD  0x80000000
+
+#endif /* INCGblCommandsh */
