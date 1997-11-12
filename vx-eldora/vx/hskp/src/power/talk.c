148d866e332c63e15d3124ba8f23a36447899e9d
Eric Loew <ericloew@ucar.edu>
1997-11-12 19:49:55 +0000

    Added code to handle channels independently.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1463 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/vx/hskp/src/power/talk.c
+++ vx-eldora/vx/hskp/src/power/talk.c
@@ -9,6 +9,9 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.1  1997/08/25  17:58:29  eric
+ * Initial revision
+ *
  * 
  * Initial revision
  *
@@ -26,8 +29,9 @@ static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
 #include <string.h>
 #include <pwrDef.h>
 #include <pwrFunc.h>
+#include "taskLib.h"
 
-void talk()
+void talk(int channel)
 {
 /**************  TURN MZ7500 INTO TALKER *************/
   
@@ -36,22 +40,27 @@ void talk()
 
 /*********** CHANNEL 1 **************/
 
-  *g1acr = 0x0c; /* TAKE CONTROL ASYNCHRONOUSLY */	  
-  *g1acr = 0x09; /* UNLISTEN MZ7500*/
-  *g1dido = 0x5f; /* UNTALK PPM */
-  *g1dido = 0x24; /* LISTEN ADDRESS PPM */
-  *g1adr = 0x40; /* TALK ADDRESS MZ7500 */
-  *g1acr = 0x8a; /* SET MZ7500 TO TALK */
-  *g1acr = 0x0b; /* DEASSERT ATN */
+if(channel == 1)
+  {
+    *g1acr = 0x0c; /* TAKE CONTROL ASYNCHRONOUSLY */	  
+    *g1dido = 0x5f; /* UNTALK PPM */
+    *g1acr = 0x09; /* UNLISTEN MZ7500*/
+    *g1dido = 0x24; /* LISTEN ADDRESS PPM */
+    *g1adr = 0x40; /* TALK ADDRESS MZ7500 */
+    *g1acr = 0x8a; /* SET MZ7500 TO TALK */
+    *g1acr = 0x0b; /* DEASSERT ATN */
+  }
 
 /************ CHANNEL 2 ******************/
 
-  *g2acr = 0x0c; /* TAKE CONTROL ASYNCHRONOUSLY */	  
-  *g2acr = 0x09; /* UNLISTEN MZ7500*/
-  *g2dido = 0x5f; /* UNTALK PPM */
-  *g2dido = 0x24; /* LISTEN ADDRESS PPM */
-  *g2adr = 0x40; /* TALK ADDRESS MZ7500 */
-  *g2acr = 0x8a; /* SET MZ7500 TO TALK */
-  *g2acr = 0x0b; /* DEASSERT ATN */
-
+if(channel == 2)
+  {
+    *g2acr = 0x0c; /* TAKE CONTROL ASYNCHRONOUSLY */	  
+    *g2dido = 0x5f; /* UNTALK PPM */
+    *g2acr = 0x09; /* UNLISTEN MZ7500*/
+    *g2dido = 0x24; /* LISTEN ADDRESS PPM */
+    *g2adr = 0x40; /* TALK ADDRESS MZ7500 */
+    *g2acr = 0x8a; /* SET MZ7500 TO TALK */
+    *g2acr = 0x0b; /* DEASSERT ATN */
+  }
 }
