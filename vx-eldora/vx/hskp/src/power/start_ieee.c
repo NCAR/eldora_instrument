480180d40fe6eead77ee69ff15b3750a79bc51d7
Eric Loew <ericloew@ucar.edu>
1997-11-12 19:46:57 +0000

    modified to support Graph Mode for xmit pulse. Also made it it compatible with existing housekeeper software.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1460 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/vx/hskp/src/power/start_ieee.c
+++ vx-eldora/vx/hskp/src/power/start_ieee.c
@@ -9,31 +9,36 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.1  1993/09/20  17:37:01  reif
+ * Initial revision
+ *
  * Revision 1.1  1992/09/01  16:48:23  craig
  * Initial revision
  *
- *
  * description: The purpose of this module is to set up the MZ7500 GPIB
- *              board as controller with DMA and interrupts in order to
- *              send commands to and receive data from the fore and
- *              aft WAVETEK 8502A Peak Power Meters used for the fore 
- *              and aft peak power measurements.
+ *              board as talker and start DMA and auxilliary clock interrupt
+ *              for real-time task (pwr.c). Also sets initial power meter
+ *              cursor positions.
+ *              
  */
 static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
 
-#define scope
-#include <vxWorks.h>
-#include <stdioLib.h>
-#include <math.h>
-#include <string.h>
-#include <pwrDef.h>
-#include <pwrFunc.h>
+#define scope extern
+#include "hskpAll.h"
+#include "taskLib.h"
 
- void start_ieee(float xmit_csr_delay,float testp_csr_delay)
+void start_ieee()
 {
-  unsigned char string_array[15];
-  unsigned char number_array[11];
-  int i;
+  unsigned char string_array[15], muxval, ecbaddr;
+  unsigned char number_array[11], xmit_tp, test;
+  int i,timeout;
+
+  /* Initialize some global flags and counters */
+
+  isr_count = 0;  /* Make these globals! */
+  fore_aft = 0;
+  freq_count = 1;
+  tp_cnt = 0;
 
 /************ INITIALIZE STRING AND NUMBER ARRAYS *************/
 
@@ -46,55 +51,27 @@ static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
 	string_array[i]=0;
     }
 
-/************* INITIALIZE POINTERS AND ISR VECTORS ************/
-
-  init_ptrs();
-  init_isr_vecs();
-
-/******************* INITIALIZE GPIB AND DMA **********************/
-
-  init_gpib();
-  init_dma();
+  init_gpib(1);
+  init_gpib(2);
+  xmit_csr_delay = (0.5 * (float)wave->chip_width[0] + (float)wave->chip_offset[0]) / 60.0; /* xmit pulsewidth in microseconds */
+  testp_csr_delay = 1.5 * ((float)wave->gate_dist1[1] / 60.0); /* test pulse width is 3 times gate spacing in microseconds */
 
 /**************  SEND COMMAND STRINGS TO 8502A PPM  **********************/
 
-  strncpy(string_array,"PKAB\0",5); /* Select peak dual channel mode */  
-  send_cmnd_string(XMIT_AND_TESTP,string_array); /* on both power meters */
- 
-  strncpy(string_array,"UPDC\0",5); /* Select update continuously mode */ 
-  send_cmnd_string(XMIT_AND_TESTP,string_array); /* on both power meters */
- 
-  strncpy(string_array,"DATN\0",5); /* Select data normal mode of operation */
-  send_cmnd_string(XMIT_AND_TESTP,string_array); /* for both power meters */
- 
-  strncpy(string_array,"WATT\0",5); /* Select linear power output mode for */ 
-  send_cmnd_string(XMIT,string_array); /* the fore and aft xmit power meter */
-
-  strncpy(string_array,"TRGE\0",5); /* Select external trigger for both */ 
-  send_cmnd_string(XMIT_AND_TESTP,string_array); /* power meters */
-
-  strncpy(string_array,"DBMW\0",5); /* Select dbm output mode for the fore */ 
-  send_cmnd_string(TESTP,string_array); /* and aft testpulse power meter */
-
-  strncpy(string_array,"OFFA\0",5); /* Input system losses as offset for */
-  sprintf(number_array,"%7.2f",XMIT_OFFSET_FORE); /* fore xmit power meter */
-  strncat(string_array,number_array,strlen(number_array));
-  send_cmnd_string(XMIT,string_array);
-
-  strncpy(string_array,"OFFB\0",5); /* Input system losses as offset for */
-  sprintf(number_array,"%7.2f",XMIT_OFFSET_AFT); /* aft xmit power meter */
+  strncpy(string_array,"FREQ\0",5); /* Select frequency */
+  sprintf(number_array,"%7.2f",fraddes->freq1); /* Use freq 1 as nominal */
   strncat(string_array,number_array,strlen(number_array));
   send_cmnd_string(XMIT,string_array);
 
   strncpy(string_array,"OFFA\0",5); /* Input system losses as offset for */
-  sprintf(number_array,"%7.2f",TESTP_OFFSET_FORE); /* aft xmit power meter */
+  sprintf(number_array,"%7.2f",ffrad->peak_power_offset); /* fore xmit power meter */
   strncat(string_array,number_array,strlen(number_array));
-  send_cmnd_string(TESTP,string_array);
+  send_cmnd_string(XMIT,string_array);
 
   strncpy(string_array,"OFFB\0",5); /* Input system losses as offset for */
-  sprintf(number_array,"%7.2f",TESTP_OFFSET_AFT); /* aft xmit power meter */
+  sprintf(number_array,"%7.2f",afrad->peak_power_offset); /* aft xmit power meter */
   strncat(string_array,number_array,strlen(number_array));
-  send_cmnd_string(TESTP,string_array);
+  send_cmnd_string(XMIT,string_array);
 
   strncpy(string_array,"CDLA\0",5); /*Set cursor delay for fore xmit meter */  
   sprintf(number_array,"%9.2f",xmit_csr_delay);
@@ -114,37 +91,65 @@ static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
   strncat(string_array,number_array,strlen(number_array));
   send_cmnd_string(TESTP,string_array);
 
-  strncpy(string_array,"RDLA\0",5); /* Set reference delay for fore xmit and tp meter */  
-  sprintf(number_array,"%9.2f",0.0);  
-  strncat(string_array,number_array,strlen(number_array));
-  send_cmnd_string(XMIT_AND_TESTP,string_array);
+  /************* PROGRAM Trigger MUX via ECB ****************/
 
-  strncpy(string_array,"RDLB\0",5); /* Set reference delay for aft xmit and tp meter */
-  strncat(string_array,number_array,strlen(number_array));
-  send_cmnd_string(XMIT_AND_TESTP,string_array);
+  taskDelay(100);       /* delay 1 sec to see if it fixes ecb problem */
+  ecbaddr = ECBATTEN;
+  xmit_tp = 1; /* select xmit mux */
+  muxval = 13;  /* select aft preknock as trigger */
+  timeout = 0;
+  do
+    {
+      if(timeout > 0)taskDelay(1);
+      timeout++;
+    }while((test = ecbSetMux(ecbaddr,xmit_tp,muxval) != 0) && timeout < 30);
 
-  strncpy(string_array,"SDLA\0",5); /* Set start delay for fore xmit and tp meters*/  
-  sprintf(number_array,"%9.2f",0.0);  
-  strncat(string_array,number_array,strlen(number_array));
-  send_cmnd_string(XMIT_AND_TESTP,string_array);
+  taskDelay(100);       /* delay 1 sec to see if it fixes ecb problem */
+  ecbaddr = ECBATTEN;
+  xmit_tp = 0; /* select test pulse mux */
+  muxval = 14; /* select aft testpulse as trigger */
+  timeout = 0;
+  do
+    {
+      if(timeout > 0)taskDelay(1);
+      timeout++;
+    }while((test = ecbSetMux(ecbaddr,xmit_tp,muxval) != 0) && timeout < 30);
 
-  strncpy(string_array,"SDLB\0",5); /* Set start delay for aft xmit and tp meters*/  
-  strncat(string_array,number_array,strlen(number_array));
-  send_cmnd_string(XMIT_AND_TESTP,string_array);
 
 /*********** TURN PPM INTO TALKER, MZ7500 INTO LISTENER ************/
 
-  listen();
+init_dma(1,15);
+init_dma(2,24);
+
+listen(1);
+listen(2);
+
+#ifdef CLOCK_ISR
+
+/* Enable auxilliary clock interrupt */
+
+sysAuxClkEnable();
+
+#endif
 
 /*********** START DMA CHANNEL 0 - GO! ***************/
 
   *d0csr=0xff; /* Clear channel status register by writing all ones */
+  taskDelay(1);
   *d0ccr=0xC8; /* Set start, continue, and interrupt bits in */
                /* channel control register */
 
 /*********** START DMA CHANNEL 1 - GO! **************/
 
   *d1csr=0xff; /* Clear channel status register by writing all ones */
+  taskDelay(1);
   *d1ccr=0xC8; /* Set start, continue, and interrupt bits in the */
                /* channel control register */
+
+taskRestart(xmit); /* re-start run-time power task */
+taskRestart(testp); /* re-start run-time power task */
+
 }
+
+
+
