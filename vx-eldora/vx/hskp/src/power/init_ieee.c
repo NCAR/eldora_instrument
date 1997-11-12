21bfcfb500343c3bc515328b4f93dda3c445cbed
Eric Loew <ericloew@ucar.edu>
1997-11-12 19:51:01 +0000

    modified code to be compatible with existing housekeeper software structure.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1464 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/vx/hskp/src/power/init_ieee.c
+++ vx-eldora/vx/hskp/src/power/init_ieee.c
@@ -1,7 +1,7 @@
 /*
  *	$Id$
  *
- *	Module:	start_ieee.c	 
+ *	Module:	init_ieee.c	 
  *	Original Author: Reif Heck 
  *      Copywrited by the National Center for Atmospheric Research
  *	Date:		 $Date$
@@ -9,6 +9,9 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.1  1997/08/26  15:54:42  craig
+ * Initial revision
+ *
  * Revision 1.1  1993/09/20  17:37:01  reif
  * Initial revision
  *
@@ -24,19 +27,18 @@
  */
 static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
 
-#define scope
-#include <vxWorks.h>
-#include <stdioLib.h>
-#include <math.h>
-#include <string.h>
-#include <pwrDef.h>
-#include <pwrFunc.h>
+#define scope extern
+#define NUM_AVG  5
+#include "hskpAll.h"
 
- void start_ieee(float xmit_csr_delay,float testp_csr_delay)
+ void init_ieee()
 {
   unsigned char string_array[15];
   unsigned char number_array[11];
-  int i;
+  unsigned char muxval,ecbaddr,test,xmit_tp;
+  int i,timeout;
+
+xmit_pwr_sem = semBCreate(SEM_Q_FIFO,SEM_EMPTY);
 
 /************ INITIALIZE STRING AND NUMBER ARRAYS *************/
 
@@ -53,11 +55,14 @@ static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
 
   init_ptrs();
   init_isr_vecs();
+  Debug = 1;
 
 /******************* INITIALIZE GPIB AND DMA **********************/
 
-  init_gpib();
-  init_dma();
+  init_gpib(1);
+  init_gpib(2);
+  init_dma(1,15); /* 15 bytes for xmit power meter */
+  init_dma(2,24); /* 24 bytes for testpulse power meter */
 
 /**************  SEND COMMAND STRINGS TO 8502A PPM  **********************/
 
@@ -70,12 +75,11 @@ static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
   strncpy(string_array,"UPDC\0",5); /* Select update continuously mode */ 
   send_cmnd_string(TESTP,string_array); /* on Test Pulse meter */
 
-  strncpy(string_array,"UPDT\0",5); /* Select update trigger reset mode */ 
-  send_cmnd_string(XMIT,string_array); /* on Xmit meter */
+  strncpy(string_array,"FREQ\0",5); /* Select frequency */
+  sprintf(number_array,"%7.2f",9.5); /* Use 9.5 GHz as nominal */
+  strncat(string_array,number_array,strlen(number_array));
+  send_cmnd_string(TESTP,string_array);
 
-  strncpy(string_array,"UPDN\0",5); /* Select update now mode */ 
-  send_cmnd_string(XMIT,string_array); /* on Xmit meter */
- 
   strncpy(string_array,"DATN\0",5); /* Select data normal mode of operation */
   send_cmnd_string(XMIT_AND_TESTP,string_array); /* for both power meters */
  
@@ -85,44 +89,36 @@ static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
   strncpy(string_array,"TRGE\0",5); /* Select external trigger for both */ 
   send_cmnd_string(XMIT_AND_TESTP,string_array); /* power meters */
 
+  strncpy(string_array,"WDLA\0",5);  /* Select Window size */
+  sprintf(number_array,"%7.2f",10.0); /* fore xmit power meter */
+  strncat(string_array,number_array,strlen(number_array));
+  send_cmnd_string(XMIT,string_array);
+  
+  strncpy(string_array,"PRFA\0",5);  /* Select Power Reference */
+  sprintf(number_array,"%7.2f",76.7); /* fore xmit power meter */
+  strncat(string_array,number_array,strlen(number_array));
+  send_cmnd_string(XMIT,string_array);
+
+  strncpy(string_array,"WDLB\0",5);  /* Select Window size */
+  sprintf(number_array,"%7.2f",10.0); /* aft xmit power meter */
+  strncat(string_array,number_array,strlen(number_array));
+  send_cmnd_string(XMIT,string_array);
+  
+  strncpy(string_array,"PRFB\0",5);  /* Select Power Reference */
+  sprintf(number_array,"%7.2f",76.0); /* fore xmit power meter */
+  strncat(string_array,number_array,strlen(number_array));
+  send_cmnd_string(XMIT,string_array);
+
   strncpy(string_array,"DBMW\0",5); /* Select dbm output mode for the fore */ 
   send_cmnd_string(TESTP,string_array); /* and aft testpulse power meter */
 
   strncpy(string_array,"OFFA\0",5); /* Input system losses as offset for */
-  sprintf(number_array,"%7.2f",XMIT_OFFSET_FORE); /* fore xmit power meter */
-  strncat(string_array,number_array,strlen(number_array));
-  send_cmnd_string(XMIT,string_array);
-
-  strncpy(string_array,"OFFB\0",5); /* Input system losses as offset for */
-  sprintf(number_array,"%7.2f",XMIT_OFFSET_AFT); /* aft xmit power meter */
-  strncat(string_array,number_array,strlen(number_array));
-  send_cmnd_string(XMIT,string_array);
-
-  strncpy(string_array,"OFFA\0",5); /* Input system losses as offset for */
-  sprintf(number_array,"%7.2f",TESTP_OFFSET_FORE); /* aft xmit power meter */
+  sprintf(number_array,"%7.2f",0.0); /* fore testp power meter */
   strncat(string_array,number_array,strlen(number_array));
   send_cmnd_string(TESTP,string_array);
 
   strncpy(string_array,"OFFB\0",5); /* Input system losses as offset for */
-  sprintf(number_array,"%7.2f",TESTP_OFFSET_AFT); /* aft xmit power meter */
-  strncat(string_array,number_array,strlen(number_array));
-  send_cmnd_string(TESTP,string_array);
-
-  strncpy(string_array,"CDLA\0",5); /*Set cursor delay for fore xmit meter */  
-  sprintf(number_array,"%9.2f",xmit_csr_delay);
-  strncat(string_array,number_array,strlen(number_array));
-  send_cmnd_string(XMIT,string_array);
-
-  strncpy(string_array,"CDLB\0",5); /*Set cursor delay for fore xmit meter */  
-  strncat(string_array,number_array,strlen(number_array));
-  send_cmnd_string(XMIT,string_array);
- 
-  strncpy(string_array,"CDLA\0",5); /* Set cursor delay for fore tp meter */  
-  sprintf(number_array,"%9.2f",testp_csr_delay);  
-  strncat(string_array,number_array,strlen(number_array));
-  send_cmnd_string(TESTP,string_array);
-
-  strncpy(string_array,"CDLB\0",5); /* Set cursor delay for aft tp meter */  
+  sprintf(number_array,"%7.2f",0.0); /* aft testp power meter */
   strncat(string_array,number_array,strlen(number_array));
   send_cmnd_string(TESTP,string_array);
 
@@ -144,29 +140,35 @@ static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
   strncat(string_array,number_array,strlen(number_array));
   send_cmnd_string(XMIT_AND_TESTP,string_array);
 
-  sprintf(number_array,"%d",500);  
+  Avg = NUM_AVG;
+  sprintf(number_array,"%d",Avg);   /* was 500 */  
   strncpy(string_array,"AVPK\0",5); /* Set # points to avg for xmit meter*/  
   strncat(string_array,number_array,strlen(number_array));
   send_cmnd_string(XMIT,string_array);
 
-  sprintf(number_array,"%d",999);  
+  sprintf(number_array,"%d",100);  /* was 999 ; 100 worked */
   strncpy(string_array,"AVPK\0",5); /* Set # points to avg for tp meter*/  
   strncat(string_array,number_array,strlen(number_array));
   send_cmnd_string(TESTP,string_array);
 
-/*********** TURN PPM INTO TALKER, MZ7500 INTO LISTENER ************/
+/* Spawn run-time power task */
 
-  listen();
+xmit = taskSpawn("xmit",90,VX_FP_TASK,10000,(FUNCPTR)xmit_rt,0,0,0,0,0,0,0,0,0,0);
+if(xmit == ERROR)
+  printf("Failed to Spawn run-time XMIT task!\n");
 
-/*********** START DMA CHANNEL 0 - GO! ***************/
+testp = taskSpawn("testp",80,VX_FP_TASK,10000,(FUNCPTR)testp_rt,0,0,0,0,0,0,0,0,0,0);
+if(testp == ERROR)
+  printf("Failed to Spawn run-time TESTP task!\n");
+taskSuspend(xmit);
+taskSuspend(testp);
+#ifdef CLOCK_ISR
 
-  *d0csr=0xff; /* Clear channel status register by writing all ones */
-  *d0ccr=0xC8; /* Set start, continue, and interrupt bits in */
-               /* channel control register */
+/* set up auxilliary clock for 1Hz update rate */
 
-/*********** START DMA CHANNEL 1 - GO! **************/
+  sysAuxClkRateSet(1);  
 
-  *d1csr=0xff; /* Clear channel status register by writing all ones */
-  *d1ccr=0xC8; /* Set start, continue, and interrupt bits in the */
-               /* channel control register */
+  if(sysAuxClkConnect((FUNCPTR)(xmit_pwr_isr),0) == ERROR)
+  exit(2);
+#endif
 }
