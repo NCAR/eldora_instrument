d97b90c062e4e5264597e88e37cbc4275251d649
Eric Loew <ericloew@ucar.edu>
1999-09-27 15:43:23 +0000

    modified to fix slow cal.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1473 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/vx/hskp/src/main/start_testpulse.c
+++ vx-eldora/vx/hskp/src/main/start_testpulse.c
@@ -1,3 +1,4 @@
+
 /*
  *	$Id$
  *
@@ -9,6 +10,9 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.4  1996/10/29  22:53:33  craig
+ * *** empty log message ***
+ *
  * Revision 1.3  1996/09/12  17:22:02  craig
  * Completed code
  *
@@ -37,29 +41,34 @@ float lambda;
 /* First reset all of the global variables that keep track of where we
    are in a given test pulse sequence */
 
+Firstime = 1;
 tp_atten_start = 0;
 tp_atten = 0;
 tp_freq_count = 0;
 tp_freq_offset_count = 0;
 tp_sum_count = 0;
 fore_dbz_sum = 0;
-fore_tp_level_sum = 0;
+fore_tp_level = 0;
 aft_dbz_sum = 0;
-aft_tp_level_sum = 0;
+aft_tp_level = 0;
 fore_vel_sum = 0;
 aft_vel_sum = 0;
 
 /* Calculate the radar constant for power at LNA, instead of the antenna */
 fore_lna_radar_const = fraddes->radar_const + ffrad->loss_in + 
                        ffrad->loss_rjoint;
-aft_lna_radar_const =
-
+aft_lna_radar_const = araddes->radar_const + afrad->loss_in + 
+                       afrad->loss_rjoint;
 /* Calculate the distance to halfway through the second furthest gate
    and three time the gate width (2.5 meters per 60 MHz clock period) */
 
-distance = 2.5 * (wave->gate_dist1[0] +
+/* distance = 2.5 * (wave->gate_dist1[0] +
 		  (wave->num_gates[0] - 2) * wave->gate_dist1[1]); 
-width = wave->gate_dist1[1] * 2.5 * 3;
+		  */
+distance = 2.5 * (wave->gate_dist1[0] +
+		  (wave->num_gates[0] - 4) * wave->gate_dist1[1]); 
+/* width = wave->gate_dist1[1] * 2.5 * 3; try larger pulse */
+width = wave->gate_dist1[1] * 2.5 * 5;
 
 
 /* Now round down the distance to an integer number of 15 Mhz periods
@@ -74,11 +83,16 @@ width = floor(((double)(width/10)) + 1) * 10;
 
 fore_vmehndshk->tpulse_dist = distance;
 fore_vmehndshk->tpulse_width = width;
+fore_vmehndshk->tpulse.freq_num[0] = tp_freq_count + 1; 
 aft_vmehndshk->tpulse_dist = distance;
 aft_vmehndshk->tpulse_width = width;
+aft_vmehndshk->tpulse.freq_num[0] = tp_freq_count + 1;
 
 /* Calculate the log value of the range to last gate squared */
-temp = (width/3 + distance) / 1000.0;
+/* temp = (width/3 + distance) / 1000.0; */
+/* temp = (width/5 + distance) / 1000.0; */
+/* Use actual radar processor correction! */
+temp = (2.5 * wave->num_gates[0] * wave->gate_dist1[1])/1000.0;
 log_testpulse_range2 = 20 * log10(temp);
 
 /* Clear the testpulse handshake flag back to zero */
@@ -93,17 +107,17 @@ lambda = 0.2997925/fraddes->freq1;  /* gives wavelength in meters */
 fore_freq_offset[0] = 0;
 fore_tp_velocity[0] = 0;
 fore_freq_offset[1] = 0.25/(fraddes->interpulse_per1 * .002);
-fore_tp_velocity[1] = fore_freq_offset[1] * lambda / 2;
+fore_tp_velocity[1] = -fore_freq_offset[1] * lambda / 2;
 fore_freq_offset[2] = 0.5/(fraddes->interpulse_per1 * .002);
-fore_tp_velocity[2] = fore_freq_offset[2] * lambda / 2;
+fore_tp_velocity[2] = -fore_freq_offset[2] * lambda / 2;
 fore_freq_offset[3] = 0.75/(fraddes->interpulse_per1 * .002);
-fore_tp_velocity[3] = fore_freq_offset[3] * lambda / 2;
+fore_tp_velocity[3] = -fore_freq_offset[3] * lambda / 2;
 fore_freq_offset[4] = -0.25/(fraddes->interpulse_per1 * .002);
-fore_tp_velocity[4] = fore_freq_offset[4] * lambda / 2;
+fore_tp_velocity[4] = -fore_freq_offset[4] * lambda / 2;
 fore_freq_offset[5] = -0.5/(fraddes->interpulse_per1 * .002);
-fore_tp_velocity[5] = fore_freq_offset[5] * lambda / 2;
+fore_tp_velocity[5] = -fore_freq_offset[5] * lambda / 2;
 fore_freq_offset[6] = -0.75/(fraddes->interpulse_per1 * .002);
-fore_tp_velocity[6] = fore_freq_offset[6] * lambda / 2;
+fore_tp_velocity[6] = -fore_freq_offset[6] * lambda / 2;
 
 /* Calculate the scale factor that the velocities will be scaled by the radar
    processors with - remember we will always get VS if there is dual PRT, 
@@ -116,17 +130,17 @@ lambda = 0.2997925/araddes->freq1;  /* gives wavelength in meters */
 aft_freq_offset[0] = 0;
 aft_tp_velocity[0] = 0;
 aft_freq_offset[1] = 0.25/(araddes->interpulse_per1 * .002);
-aft_tp_velocity[1] = aft_freq_offset[1] * lambda / 2;
+aft_tp_velocity[1] = -aft_freq_offset[1] * lambda / 2;
 aft_freq_offset[2] = 0.5/(araddes->interpulse_per1 * .002);
-aft_tp_velocity[2] = aft_freq_offset[2] * lambda / 2;
+aft_tp_velocity[2] = -aft_freq_offset[2] * lambda / 2;
 aft_freq_offset[3] = 0.75/(araddes->interpulse_per1 * .002);
-aft_tp_velocity[3] = aft_freq_offset[3] * lambda / 2;
+aft_tp_velocity[3] = -aft_freq_offset[3] * lambda / 2;
 aft_freq_offset[4] = -0.25/(araddes->interpulse_per1 * .002);
-aft_tp_velocity[4] = aft_freq_offset[4] * lambda / 2;
+aft_tp_velocity[4] = -aft_freq_offset[4] * lambda / 2;
 aft_freq_offset[5] = -0.5/(araddes->interpulse_per1 * .002);
-aft_tp_velocity[5] = aft_freq_offset[5] * lambda / 2;
+aft_tp_velocity[5] = -aft_freq_offset[5] * lambda / 2;
 aft_freq_offset[6] = -0.75/(araddes->interpulse_per1 * .002);
-aft_tp_velocity[6] = aft_freq_offset[6] * lambda / 2;
+aft_tp_velocity[6] = -aft_freq_offset[6] * lambda / 2;
 
 /* Calculate the scale factor that the velocities will be scaled by the radar
    processors with - remember we will always get VS if there is dual PRT, 
@@ -142,5 +156,5 @@ tp_dwell_count = testpulse_max_count;
 
 /* Set the starting and ending dwell count to sum over */
 tp_sum_start = 2000.0/dwelltime_msec; /* start 2 seconds into calibration */
-tp_sum_start = 9000.0/dwelltime_msec; /* end 9 seconds into calibration */
+tp_sum_end = 9000.0/dwelltime_msec; /* end 9 seconds into calibration */
 }
--- vx-eldora/vx/hskp/src/main/update_testpulse.c
+++ vx-eldora/vx/hskp/src/main/update_testpulse.c
@@ -9,6 +9,11 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.5  1997/08/20  17:09:33  eric
+ * corrected log power computation.
+ * modified testpulse frequency so that only doppler offset
+ * is recorded on tape.
+ *
  * Revision 1.4  1996/10/29  22:53:43  craig
  * *** empty log message ***
  *
@@ -28,7 +33,6 @@ static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
 
 #define scope extern
 #include "hskpAll.h"
-
 void update_testpulse()
 {
 /* Define some general purpose variables */
@@ -38,54 +42,93 @@ double frequency, temp;
 unsigned char unitnum, ecbaddr,test,attenuation, foraft;
 int timeout;
 float dbz, log_power, received_lna_pwr, injected_lna_pwr, received_vel;
-float diff;
+float diff, tdiff;
 
 
 /* Zero the testpulse dwell counter */
 tp_dwell_count = 0;
 
+if(!Firstime)
+  {
 /* Check to see if the previous testpulse was within the proper error band */
 
 dbz = (((fore_dbz_sum / tp_sum_count)) - 280.0) / 8.0;
 log_power = dbz + fore_lna_radar_const - log_testpulse_range2;
 received_lna_pwr = log_power;
-injected_lna_pwr = fore_tp_level_sum / tp_sum_count + 
-                   ffrad->test_pulse_offset - fore_vmehndshk->tpulse_atten;
-diff = received_lna_pwr - injected_lna_pwr;
+injected_lna_pwr = fore_tp_level - fore_vmehndshk->tpulse_atten + ffrad->test_pulse_offset + 0.3;
+
+tdiff = diff = received_lna_pwr - injected_lna_pwr;
+/* printf("TD %2.2f",tdiff); */
 if(diff < 0) diff *= -1;
-if(diff > MAX_TP_PWR_DIFF) printf("FP "); 
+if(diff > MAX_TP_PWR_DIFF) 
+  {
+    printf("ATF = %d\n",fore_vmehndshk->tpulse_atten);
+    printf("FP%d: %2.2f\n",tp_freq_count+1,tdiff);
+/*    printf("Dbz = %f; logP = %f\n",dbz,received_lna_pwr); */
+/*    printf("Pwr = %f\n",injected_lna_pwr); */
+  }
 received_vel = ((fore_vel_sum / tp_sum_count) - 8191.0) / f_vscale;
 diff = received_vel - fore_tp_velocity[tp_freq_offset_count];
+/* printf("Vel = %f\n",received_vel); */
 if(diff < 0) diff *= -1;
-if(diff > MAX_TP_VEL_DIFF) printf("FV "); 
+if(diff > MAX_TP_VEL_DIFF) printf("FV%d %1.2f",tp_freq_count+1,diff); 
 
 dbz = (((aft_dbz_sum / tp_sum_count)) - 280.0) / 8.0;
 log_power = dbz + aft_lna_radar_const - log_testpulse_range2;
 received_lna_pwr = log_power;
-injected_lna_pwr = aft_tp_level_sum / tp_sum_count + 
-                   afrad->test_pulse_offset - aft_vmehndshk->tpulse_atten;
-diff = received_lna_pwr - injected_lna_pwr;
+injected_lna_pwr = aft_tp_level - aft_vmehndshk->tpulse_atten + afrad->test_pulse_offset + 0.4;
+tdiff = diff = received_lna_pwr - injected_lna_pwr;
 if(diff < 0) diff *= -1;
-if(diff > MAX_TP_PWR_DIFF) printf("AP "); 
+if(diff > MAX_TP_PWR_DIFF) 
+   {
+     printf("ATA = %d\n",aft_vmehndshk->tpulse_atten);
+     printf("AP%d: %2.2f\n",tp_freq_count+1,tdiff); 
+/*	printf("Dbz = %f; logP = %f\n",dbz,received_lna_pwr); */
+/*	printf("Pwr = %f\n",injected_lna_pwr); */
+   }
 received_vel = ((aft_vel_sum / tp_sum_count) - 8191.0) / a_vscale;
+/* printf("Vel = %f\n",received_vel); */
 diff = received_vel - aft_tp_velocity[tp_freq_offset_count];
 if(diff < 0) diff *= -1;
-if(diff > MAX_TP_VEL_DIFF) printf("AV "); 
+if(diff > MAX_TP_VEL_DIFF) printf("AV%d  %1.2f",tp_freq_count+1,diff); 
 
 tp_sum_count = 0;
 fore_dbz_sum = 0;
-fore_tp_level_sum = 0;
+fore_tp_level = 0;
 aft_dbz_sum = 0;
-aft_tp_level_sum = 0;
+aft_tp_level = 0;
 fore_vel_sum = 0;
 aft_vel_sum = 0;
 
+
+/* Update the counters for next time */
+
+tp_freq_count += 1;
+if((tp_freq_count + 1) > fraddes->num_freq_trans)
+{
+  tp_freq_count = 0;
+  tp_atten += TP_ATTEN_STEP;
+  if(tp_atten > TP_ATTEN_MAX)
+  {
+    tp_atten_start += 1;
+    if(tp_atten_start >= TP_ATTEN_STEP)
+    tp_atten_start = 0;
+    tp_atten = tp_atten_start;
+
+    tp_freq_offset_count += 1;
+    if(tp_freq_offset_count > 6)
+    tp_freq_offset_count = 0;
+
+  }
+}
+
+  }
 /* Now re-program the fore testpulse with a new value */
 
-fore_vmehndshk->tpulse_freq_num = tp_freq_count + 1;
+fore_vmehndshk->tpulse.freq_num[0] = tp_freq_count + 1;
 frequency = fore_freqs[tp_freq_count] +
 (double)fore_freq_offset[tp_freq_offset_count];
-printf("Fore frequency: %lf\n",frequency);
+/* printf("Fore frequency: %lf\n",frequency); */
 attenuation = tp_atten;
 fore_vmehndshk->tpulse_freq = fore_freq_offset[tp_freq_offset_count]; /* use offset */
 fore_vmehndshk->tpulse_atten = tp_atten;
@@ -121,38 +164,18 @@ do
 	  timeout < 30);  
 
 
+/* Now re-program the aft testpulse with a new value */
 
-/* Now re-program the fore testpulse with a new value */
-
-aft_vmehndshk->tpulse_freq_num = tp_freq_count + 1;
+aft_vmehndshk->tpulse.freq_num[0] = tp_freq_count + 1;
 frequency = aft_freqs[tp_freq_count] +
 (double)aft_freq_offset[tp_freq_offset_count];
-printf("Aft frequency: %lf\n",frequency);
+/* printf("Aft frequency: %lf\n",frequency); */
 attenuation = tp_atten;
 aft_vmehndshk->tpulse_atten = tp_atten;
 aft_vmehndshk->tpulse_freq = aft_freq_offset[tp_freq_offset_count]; /* use offset */
 aft_vmehndshk->tpulse_flg = 1;
 
-/* Update the counters for next time */
-
-tp_freq_count += 1;
-if((tp_freq_count + 1) > fraddes->num_freq_trans)
-{
-  tp_freq_count = 0;
-  tp_atten += TP_ATTEN_STEP;
-  if(tp_atten > TP_ATTEN_MAX)
-  {
-    tp_atten_start += 1;
-    if(tp_atten_start >= TP_ATTEN_STEP)
-    tp_atten_start = 0;
-    tp_atten = tp_atten_start;
-
-    tp_freq_offset_count += 1;
-    if(tp_freq_offset_count > 6)
-    tp_freq_offset_count = 0;
-
-  }
-}
+/* Counters used to be updated Here !! */
 
 /* Set the aft test pulse frequency */
 
@@ -183,6 +206,10 @@ do
       timeout++;
   }while((test = ecbSetAtten(ecbaddr, foraft, attenuation) != 0) &&
 	 timeout < 30);  
+if(Firstime)
+  Firstime = 0;
+New_tp = 1;           /* handshake with Test Pulse Power Meter */
+semGive(xmit_pwr_sem); /* handshake with Transmit Power Meter */
 
 }
 
