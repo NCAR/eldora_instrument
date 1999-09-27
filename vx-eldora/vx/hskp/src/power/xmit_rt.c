bcd86877fd31e3e1f19e22266f744d676538d7a5
Eric Loew <ericloew@ucar.edu>
1999-09-27 16:45:07 +0000

    split task into a program task (pgm_rt.c) and a data task. this is the data task now.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1494 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/vx/hskp/src/power/xmit_rt.c
+++ vx-eldora/vx/hskp/src/power/xmit_rt.c
@@ -9,6 +9,9 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.2  1999/07/13  16:49:51  thor
+ * *** empty log message ***
+ *
  * Revision 1.1  1997/11/12 19:45:12  eric
  * Initial revision
  *
@@ -16,8 +19,7 @@
  * Initial revision
  *
  *
- * description: The purpose of this module is to update power meter
- *              graphical displays and cursor position and also to 
+ * description: The purpose of this module is 
  *              handle ISRs and read xmit power
  *
  */
@@ -35,169 +37,61 @@ void xmit_rt()
 {
   unsigned char string_array[15];
   unsigned char number_array[11];
+  unsigned char *g1serp, status;
   int i,prev;
   int sem_status;
-
-
-/************ INITIALIZE STRING AND NUMBER ARRAYS *************/
-
-  for(i=0; i<11; i++)
-    {
-        number_array[i]=0;
-    }
-  for(i=0; i<15; i++)
-    {
-        string_array[i]=0;
-    }
-  while(1)
-    {
-    /* Transmit Power Meter */
-
-      sem_status = semTake(xmit_pwr_sem,20);
-      if(sem_status == OK)
-	{
-	/* change Power Meter curser position to point to the next freq */
-	/* Also switch between Fore and Aft at appropriate time */
-	
-	  *d0ccr = 0x10; /* Abort channel 1 DMA operation */
-	  taskDelay(1);
-	  *d0csr = 0xff; /* Clear DMA status register */
-	  
-	  /* try clearing GPIB Interface */
-	  
-	  *g1acr = 0x00; /* CLEAR SOFTWARE RESET */
-	  *g1acr = 0x8f; /* SEND INTERFACE CLEAR (sic)*/
-	  taskDelay(1);  /* SET DELAY */
-	  *g1acr = 0x0f; /* CLEAR sic */          
-	  
-	  /* handle any pending interrupts */
-	  
-	  sem_status = semTake(xmit_data_sem,NO_WAIT);
-	  sem_status = semTake(xmit_err_sem,NO_WAIT);
-	  init_gpib(1); /* try this here to re-establish comms */
-
-
-	  if(freq_count > fraddes->num_freq_trans)
-	    {
-	    /* Swap pwr meter source from Fore to Aft */
-	    
-	      freq_count = 1;
-	      xmit_csr_delay = (0.5 * (float)wave->chip_width[0] + (float)wave->chip_offset[0]) / 60.0; /* xmit pulsewidth in microseconds */
-	      if(fore_aft == 1)
-		{
-		  strncpy(string_array,"GRFA\0",5);     /* Select graph mode */  
-		  send_cmnd_string(XMIT,string_array);
-
-		  strncpy(string_array,"FREQ\0",5); /* Select frequency */
-		  sprintf(number_array,"%7.2f",fraddes->freq1); /* Use freq 1 as nominal */
-		  strncat(string_array,number_array,strlen(number_array));
-		  send_cmnd_string(XMIT,string_array);
-
-		  send_cmnd_string(XMIT,string_array);  /* on Xmit meter */
-		  strncpy(string_array,"CDLA\0",5); /*Set cursor delay for fore xmit meter */  
-		  sprintf(number_array,"%9.2f",xmit_csr_delay);
-		  strncat(string_array,number_array,strlen(number_array));
-		  send_cmnd_string(XMIT,string_array);
-		  
-		}
-	      if(fore_aft == 0)
-		{
-		  strncpy(string_array,"GRFB\0",5);    /* Select graph mode */  
-		  send_cmnd_string(XMIT,string_array);
-
-		  strncpy(string_array,"FREQ\0",5); /* Select frequency */
-		  sprintf(number_array,"%7.2f",araddes->freq1); /* Use freq 1 as nominal */
-		  strncat(string_array,number_array,strlen(number_array));
-		  send_cmnd_string(XMIT,string_array);
-
-		  send_cmnd_string(XMIT,string_array); /* on Xmit meter */
-		  strncpy(string_array,"CDLB\0",5); /*Set cursor delay for aft xmit meter */  
-		  sprintf(number_array,"%9.2f",xmit_csr_delay);
-		  strncat(string_array,number_array,strlen(number_array));
-		  send_cmnd_string(XMIT,string_array);
-
-		}
-	      prev = fore_aft;
-	      fore_aft = !prev;
-	    }
-	  else
-	    {
-	      /* Just update Curser to point to next frequency */
-
-	      xmit_csr_delay = ((0.5 + freq_count-1) * (float)wave->chip_width[0] + (float)wave->chip_offset[0])/ 60.0; /* xmit pulsewidth in microseconds */
-	      if(fore_aft == 0)
-		{
-		  strncpy(string_array,"GRFA\0",5);     /* Select graph mode */  
-		  send_cmnd_string(XMIT,string_array);
-
-		  strncpy(string_array,"CDLA\0",5); /*Set cursor delay for fore xmit meter */  
-		  sprintf(number_array,"%9.2f",xmit_csr_delay);
-		strncat(string_array,number_array,strlen(number_array));
-		send_cmnd_string(XMIT,string_array);
-		
-		}
-	      if(fore_aft == 1)
-		{
-		  strncpy(string_array,"GRFB\0",5);     /* Select graph mode */  
-		  send_cmnd_string(XMIT,string_array);
-
-		  strncpy(string_array,"CDLB\0",5); /*Set cursor delay for aft xmit meter */  
-		  sprintf(number_array,"%9.2f",xmit_csr_delay);
-		  strncat(string_array,number_array,strlen(number_array));
-		  send_cmnd_string(XMIT,string_array);
-
-		}
-	    }
-
-	  strncpy(string_array,"UPDT\0",5); /* Select update trigger reset mode */ 
-	  send_cmnd_string(XMIT,string_array); /* on Xmit meter */
-	  
-	  strncpy(string_array,"UPDN\0",5); /* Select update now mode */ 
-	  send_cmnd_string(XMIT,string_array); /* on Xmit meter */
-	  taskDelay(1);
-	  freq_count++;	      
-	  init_dma(1,15);                         
-	  taskDelay(1);
-	  listener(1);      /* turn MZ7500 into listener and power meter into tallker */
-	  *d0csr = 0xff;
-	  taskDelay(1);
-	  *d0ccr = 0xC8;  /* start DMA */
-      
-	  sem_status = semTake(xmit_data_sem,1000);
-	  if(sem_status == OK)
-	    {
-	      flt_pt(1);  /* Update Now Mode */
-	      if(fore_aft == 0)
-		{
-		  if(Debug)
-		    printf("FORE_XMIT_PWR=%-10.3E watts\n",fore_xmit_pwr);
-		  fore_xmit_pwr = fore_xmit_pwr/1000.0; /* xmit power in kWatts */
-		  aft_xmit_pwr = -999.0;
-		}
-	      if(fore_aft == 1)
-		{
-		  if(Debug)
-		    printf("AFT_XMIT_PWR=%-10.3E watts\n",fore_xmit_pwr);
-		  aft_xmit_pwr = fore_xmit_pwr/1000.0; /* xmit power in kWatts */
-		  fore_xmit_pwr = -999.0;
-		}
-	      xmit_isr_done=0;
-	    }
-	  else
+  
+  g1serp = (unsigned char *)(GPIBCHAN1 + SER_POLL_REG);
+while(1)
+  {
+    sem_status = semTake(xmit_data_sem,1000);
+    if(sem_status == OK)
+      {
+	flt_pt(1);  /* Update Now Mode */
+	if(fore_aft == 0)
+	  {
+	    if(Debug)
+	      logMsg("FORE_XMIT_PWR=%-10.3E watts\n",fore_xmit_pwr,0,0,0,0);
+	    fore_xmit_pwr = fore_xmit_pwr/1000.0; /* xmit power in kWatts */
+	    aft_xmit_pwr = -999.0;
+	    fore_vmehndshk->tpulse.freq_num[1] = freq_count; 
+	  }
+	if(fore_aft == 1)
+	  {
+	    if(Debug)
+	      logMsg("AFT_XMIT_PWR=%-10.3E watts\n",fore_xmit_pwr,0,0,0,0);
+	    aft_xmit_pwr = fore_xmit_pwr/1000.0; /* xmit power in kWatts */
+	    fore_xmit_pwr = -999.0;
+	    aft_vmehndshk->tpulse.freq_num[1] = freq_count;
+	  }
+	status = *g1serp;
+/*
+	if(Debug)
+	  printf("Status = %8X\n",status); 
+*/
+	xmit_isr_done=0;
+      }
+    else
+      {
+	if(autocal)
+	  {
 	    printf("NO XMIT DATA INTERUPT\n");
-	  sem_status = semTake(xmit_err_sem,NO_WAIT);
-	  if(sem_status == OK)
-	    {
-	      if(Debug)
-		printf("XMIT ERROR: %d \n",dma_err1);
-	      xmit_isr_done=0;
-	      init_dma(1,15);
-	      *d0csr = 0xff;
-	      taskDelay(1);
-	      *d0ccr = 0xC8;  /* start DMA */
-	    }
-	}
-    }
+	    fore_xmit_pwr = -999.0;
+	    aft_xmit_pwr = -999.0;
+	  }
+      }
+    sem_status = semTake(xmit_err_sem,NO_WAIT);
+    if(sem_status == OK)
+      {
+	if(Debug)
+	  printf("XMIT ERROR: %d \n",dma_err1);
+	xmit_isr_done=0;
+	init_dma(1,15);
+	*d0csr = 0xff;
+	taskDelay(1);
+	*d0ccr = 0xC8;  /* start DMA */
+      }
+  }
 }
 #ifdef CLOCK_ISR
 void xmit_pwr_isr()
