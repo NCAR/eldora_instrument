cb379009eaa81fa95a6cf809ebe615f0822f93a8
Eric Loew <ericloew@ucar.edu>
2000-05-15 18:56:02 +0000

    Added GPIB handshake and timeout.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1509 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/vx/hskp/src/power/talk.c
+++ vx-eldora/vx/hskp/src/power/talk.c
@@ -9,6 +9,9 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.2  1997/11/12  19:49:55  eric
+ * Added code to handle channels independently.
+ *
  * Revision 1.1  1997/08/25  17:58:29  eric
  * Initial revision
  *
@@ -33,6 +36,7 @@ static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
 
 void talk(int channel)
 {
+  int timeout;
 /**************  TURN MZ7500 INTO TALKER *************/
   
   /* TURN BUS AROUND TO TALK */
@@ -44,8 +48,20 @@ if(channel == 1)
   {
     *g1acr = 0x0c; /* TAKE CONTROL ASYNCHRONOUSLY */	  
     *g1dido = 0x5f; /* UNTALK PPM */
+    timeout = 0;
+    while(*g1ism0 != 0x10 && timeout < 30)
+      {
+	timeout++;
+	taskDelay(1);
+      }
     *g1acr = 0x09; /* UNLISTEN MZ7500*/
     *g1dido = 0x24; /* LISTEN ADDRESS PPM */
+    timeout = 0;
+    while(*g1ism0 != 0x10 && timeout < 30)
+      {
+	timeout++;
+	taskDelay(1);
+      }
     *g1adr = 0x40; /* TALK ADDRESS MZ7500 */
     *g1acr = 0x8a; /* SET MZ7500 TO TALK */
     *g1acr = 0x0b; /* DEASSERT ATN */
@@ -57,10 +73,24 @@ if(channel == 2)
   {
     *g2acr = 0x0c; /* TAKE CONTROL ASYNCHRONOUSLY */	  
     *g2dido = 0x5f; /* UNTALK PPM */
+    timeout = 0;
+    while(*g2ism0 != 0x10 && timeout < 30)
+      {
+	timeout++;
+	taskDelay(1);
+      }
     *g2acr = 0x09; /* UNLISTEN MZ7500*/
     *g2dido = 0x24; /* LISTEN ADDRESS PPM */
+    timeout = 0;
+    while(*g2ism0 != 0x10 && timeout < 30)
+      {
+	timeout++;
+	taskDelay(1);
+      }
     *g2adr = 0x40; /* TALK ADDRESS MZ7500 */
     *g2acr = 0x8a; /* SET MZ7500 TO TALK */
     *g2acr = 0x0b; /* DEASSERT ATN */
   }
 }
+
+
