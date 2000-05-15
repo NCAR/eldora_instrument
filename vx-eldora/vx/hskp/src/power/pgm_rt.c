408a08805a2f1f12cd74eb26a6516b058a716742
Eric Loew <ericloew@ucar.edu>
2000-05-15 18:45:04 +0000

    Removed need to reset GPIB between listen and talk transitions, also added sync semaphore support for cleaner stops.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1504 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/vx/hskp/src/power/pgm_rt.c
+++ vx-eldora/vx/hskp/src/power/pgm_rt.c
@@ -9,6 +9,9 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.1  1999/09/27  16:35:45  eric
+ * Initial revision
+ *
  *
  * Initial revision
  *
@@ -29,9 +32,9 @@ static int isr_count;
 
 void pgm_rt()
 {
-  unsigned char string_array[15];
+  unsigned char string_array[15], status;
   unsigned char number_array[11];
-  int i,prev;
+  int i,prev, cmnd_stat;
   int sem_status;
 
 
@@ -59,6 +62,8 @@ void pgm_rt()
 	  taskDelay(1);
 	  *d0csr = 0xff; /* Clear DMA status register */
 	  
+#ifdef DESPERATE
+
 	  /* try clearing GPIB Interface */
 	  
 	  *g1acr = 0x00; /* CLEAR SOFTWARE RESET */
@@ -71,7 +76,9 @@ void pgm_rt()
 	  sem_status = semTake(xmit_data_sem,NO_WAIT);
 	  sem_status = semTake(xmit_err_sem,NO_WAIT);
 	  init_gpib(1); /* try this here to re-establish comms */
-
+#endif
+	  
+	  talk(1);
 
 	  if(freq_count > fraddes->num_freq_trans)
 	      freq_count = 1;
@@ -81,46 +88,82 @@ void pgm_rt()
 	  if(fore_aft == 1)
 	    {
 	      strncpy(string_array,"GRFA\0",5);     /* Select graph mode */  
-	      send_cmnd_string(XMIT,string_array);
-	      
+	      cmnd_stat = send_cmnd_string(XMIT,string_array);
+	      if(cmnd_stat == -1)
+		{
+		  status = ReadStatusByte(1);
+		  printf("SRQ: %2X; Command: %s\n",status,string_array);
+		}
 	      strncpy(string_array,"FREQ\0",5); /* Select frequency */
 	      sprintf(number_array,"%7.2f",fraddes->freq1); /* Use freq 1 as nominal */
 	      strncat(string_array,number_array,strlen(number_array));
-	      send_cmnd_string(XMIT,string_array);
+	      cmnd_stat = send_cmnd_string(XMIT,string_array);
+	      if(cmnd_stat == -1)
+		{
+		  status = ReadStatusByte(1);
+		  printf("SRQ: %2X; Command: %s\n",status,string_array);
+		}
 	      
-	      send_cmnd_string(XMIT,string_array);  /* on Xmit meter */
 	      strncpy(string_array,"CDLA\0",5); /*Set cursor delay for fore xmit meter */  
 	      sprintf(number_array,"%9.2f",xmit_csr_delay);
 	      strncat(string_array,number_array,strlen(number_array));
-	      send_cmnd_string(XMIT,string_array);
-	      
+	      cmnd_stat = send_cmnd_string(XMIT,string_array);
+	      if(cmnd_stat == -1)
+		{
+		  status = ReadStatusByte(1);
+		  printf("SRQ: %2X; Command: %s\n",status,string_array);
+		}	      
 	    }
 	  if(fore_aft == 0)
 	    {
 	      strncpy(string_array,"GRFB\0",5);    /* Select graph mode */  
-	      send_cmnd_string(XMIT,string_array);
-	      
+	      cmnd_stat = send_cmnd_string(XMIT,string_array);
+	      if(cmnd_stat == -1)
+		{
+		  status = ReadStatusByte(1);
+		  printf("SRQ: %2X; Command: %s\n",status,string_array);
+		}	      
 	      
 	      strncpy(string_array,"FREQ\0",5); /* Select frequency */
 	      sprintf(number_array,"%7.2f",araddes->freq1); /* Use freq 1 as nominal */
 	      strncat(string_array,number_array,strlen(number_array));
-	      send_cmnd_string(XMIT,string_array);
+	      cmnd_stat = send_cmnd_string(XMIT,string_array);
+	      if(cmnd_stat == -1)
+		{
+		  status = ReadStatusByte(1);
+		  printf("SRQ: %2X; Command: %s\n",status,string_array);
+		}	      
 	      
-	      send_cmnd_string(XMIT,string_array); /* on Xmit meter */
 	      strncpy(string_array,"CDLB\0",5); /*Set cursor delay for aft xmit meter */  
 	      sprintf(number_array,"%9.2f",xmit_csr_delay);
 	      strncat(string_array,number_array,strlen(number_array));
-	      send_cmnd_string(XMIT,string_array);
+	      cmnd_stat = send_cmnd_string(XMIT,string_array);
+	      if(cmnd_stat == -1)
+		{
+		  status = ReadStatusByte(1);
+		  printf("SRQ: %2X; Command: %s\n",status,string_array);
+		}	      
 	      
 	      freq_count++;	      
 	    }
 	  prev = fore_aft;
 	  fore_aft = !prev;
 	  strncpy(string_array,"UPDT\0",5); /* Select update trigger reset mode */ 
-	  send_cmnd_string(XMIT,string_array); /* on Xmit meter */
+	  cmnd_stat = send_cmnd_string(XMIT,string_array); /* on Xmit meter */
+	  if(cmnd_stat == -1)
+	    {
+	      status = ReadStatusByte(1);
+	      printf("SRQ: %2X; Command: %s\n",status,string_array);
+	    }	      
 	  
 	  strncpy(string_array,"UPDN\0",5); /* Select update now mode */ 
-	  send_cmnd_string(XMIT,string_array); /* on Xmit meter */
+	  cmnd_stat = send_cmnd_string(XMIT,string_array); /* on Xmit meter */
+	  if(cmnd_stat == -1)
+	    {
+	      status = ReadStatusByte(1);
+	      printf("SRQ: %2X; Command: %s\n",status,string_array);
+	    }	      
+
 	  taskDelay(1);
 	  init_dma(1,15);                         
 	  taskDelay(1);
@@ -129,8 +172,13 @@ void pgm_rt()
 	  taskDelay(1);
 	  *d0ccr = 0xC8;  /* start DMA */
 	}
+      semGive(cal_sync_sem); /* set cal_sync semiphore for stop synchronization */
     }
 }
+
+
+
+
 
 
 
