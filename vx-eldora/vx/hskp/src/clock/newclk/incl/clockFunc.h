431aae1b733e7619d3b56c64d2fb52259854fc2f
Aditi Kapoor <kapoor@ucar.edu>
2003-09-23 19:39:28 +0000

    Initial revision
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1543 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

---
+++ vx-eldora/vx/hskp/src/clock/newclk/incl/clockFunc.h
@@ -0,0 +1,35 @@
+/*
+ *      $Id$
+ *
+ *      Module:  clockFunc.h
+ *      Original Author: Aditi Kapoor
+ *      Copyrighted by the National Center for Atmospheric Research
+ *     $Date$
+ *
+ *
+ * Description: This include module declares the functions used for the
+ *              Bancomm clock card in ELDORA. (replaced the TOD card)
+ *
+ * revision history
+ * ----------------
+ * $Log$
+ *
+ */
+
+#ifndef INCclkFunch
+#define INCclkFunch
+
+
+/* Bancomm clock card */
+void clock_menu(void);         /* Clock card debug menu */
+void get_time(void);           /* Read time from the Clock card */
+void get_year(void);           /* Read year from the Clock card */
+void init_clock(void);         /* Initialize the Clock card */
+void send_packet(char *);      /* Send command packet to the Clock card */
+void set_time(int,int,int,int,int,int,char *);
+                               /* Encode packet to set time in the Clock card*/
+void start_clock(char *);        /* Load time into the Clock card */
+
+#endif /* INC */
+
+
