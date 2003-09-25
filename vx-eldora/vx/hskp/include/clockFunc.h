2c1595cabddfe38851f8a8b88f3e7cb401fd3415
Aditi Kapoor <kapoor@ucar.edu>
2003-09-25 20:58:50 +0000

    added keyword "scope"
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1568 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/vx/hskp/include/clockFunc.h
+++ vx-eldora/vx/hskp/include/clockFunc.h
@@ -13,6 +13,9 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.1  2003/09/25  20:56:57  kapoor
+ * Initial revision
+ *
  * Revision 1.1  2003/09/23  19:39:28  kapoor
  * Initial revision
  *
@@ -24,14 +27,14 @@
 
 
 /* Bancomm clock card */
-void clock_menu(void);         /* Clock card debug menu */
-void get_time(void);           /* Read time from the Clock card */
-void get_year(void);           /* Read year from the Clock card */
-void init_clock(void);         /* Initialize the Clock card */
-void send_packet(char *);      /* Send command packet to the Clock card */
-void set_time(int,int,int,int,int,int,char *);
+scope void clock_menu(void);         /* Clock card debug menu */
+scope void get_time(void);           /* Read time from the Clock card */
+scope void get_year(void);           /* Read year from the Clock card */
+scope void init_clock(void);         /* Initialize the Clock card */
+scope void send_packet(char *);      /* Send command packet to the Clock card*/
+scope void set_time(int,int,int,int,int,int,char *);
                                /* Encode packet to set time in the Clock card*/
-void start_clock(char *);        /* Load time into the Clock card */
+scope void start_clock(char *);        /* Load time into the Clock card */
 
 #endif /* INC */
 
