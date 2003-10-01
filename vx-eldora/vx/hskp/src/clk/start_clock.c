952b253160671b7846f366d9a8d523e26a2242fc
Aditi Kapoor <kapoor@ucar.edu>
2003-10-01 20:32:10 +0000

    Initial revision
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1577 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

---
+++ vx-eldora/vx/hskp/src/clk/start_clock.c
@@ -0,0 +1,49 @@
+/*
+ *	$Id$
+ *
+ *	Module:	start_clock.c	 
+ *	Original Author: Craig Walther 
+ *      Copywrited by the National Center for Atmospheric Research
+ *	Date:		 $Date$
+ *
+ * revision history
+ * ----------------
+ * $Log$
+ * Revision 1.2  2003/09/23  22:58:15  kapoor
+ * changed include to "hskpAll.h"
+ *
+ * Revision 1.1  2003/09/23  16:43:42  kapoor
+ * Initial revision
+ *
+ * Revision 2.0  2001/06/12 19:32:43  thor
+ * *** empty log message ***
+ *
+ * Revision 1.2  2001/05/31 20:45:37  thor
+ * Syncing source tree to ACE-ASIA
+ *
+ * Revision 1.3  1995/09/25  17:44:28  martin
+ * updated from Craig's current code
+ *
+ * Revision 1.1  1995/07/11  21:04:57  craig
+ * Initial revision
+ *
+ *
+ *
+ * description: The purpose of this module is to start the clock when called.
+ */
+
+
+static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
+
+#define scope extern
+#include "hskpAll.h"
+
+void start_clock(char *time_date)
+{
+
+*clk_intstat = 0x08;                 /* Clear the 1 PPS bit */
+while(!(*clk_intstat & 0x08));       /* wait for next second to roll around */
+send_packet(time_date);
+
+return;
+}
