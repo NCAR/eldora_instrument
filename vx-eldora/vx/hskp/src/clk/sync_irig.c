6efbfc2794ceccbbcf31b34cc5fd02de8d3a5a83
Aditi Kapoor <kapoor@ucar.edu>
2003-10-02 15:29:45 +0000

    *** empty log message ***
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1581 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- vx-eldora/vx/hskp/src/clk/sync_irig.c
+++ vx-eldora/vx/hskp/src/clk/sync_irig.c
@@ -9,6 +9,9 @@
  * revision history
  * ----------------
  * $Log$
+ * Revision 1.1  2003/10/01  20:18:14  kapoor
+ * Initial revision
+ *
  * Revision 1.4  1994/05/20  20:36:45  craig
  * *** empty log message ***
  *
@@ -30,52 +33,16 @@ static char rcsid[] = "$Date$ $RCSfile$ $Revision$";
 char sync_irig() 
 {
 
-unsigned char hseconds; 
-short timeout;
-short i;
-
-/********** ENABLE LOADING OF IRIG REGISTER **************/
-
-*irigb=0x00; /* Enable IRIGB data into registers */
-
-/* Wait for last IRIG-B register to be loaded */
-
-timeout = 0;
-do{
-    timeout++;
-    taskDelay(1);
-    }while(((*strg & LDDAY2) != LDDAY2) && (timeout < 90));
-if(timeout >= 90) return((char)0);
-
-
-/************** LOAD CLOCK REGISTERS WITH IRIG DATA ****************/
-
-*tod_cmnd=0x04; /* Stop clock for loading,leave in 24 hr mode */
-*hsec_reg=0x00;     /* Init hundreths to zero */
-*sec_reg=1+((unsigned char)0x0F&*isec)+((*isec>>4)*10); /* ADD 1 second */
-
-*min_reg=((unsigned char)0x0F&*imin)+((*imin>>4)*10); /* Load minute register
-						     with IRIG minutes */
-
-*hr_reg=((unsigned char)0x0F&*ihr)+((*ihr>>4)*10); /* Load hours register with
-						  IRIG hours */
-
-/*********** WAIT FOR TIME MARK BEFORE STARTING CLOCK ********/
-
-*irigb=0x00;
-timeout = 0;
-do{
-      timeout++;
-      taskDelay(1);
-  }while(((*strg & TMRKFND) != TMRKFND) && (timeout < 90));
-
-
-/********* START CLOCK (Whether we were successful or not *******************/
-
-*tod_cmnd=0x1c; /* Normal Mode, Ints enabled, 12 hr mode, 32.768kHz crystal */
-if(timeout >= 90) return((char)0);
+  /* This function is going to do nothing, because, 
+     this function has nothing to do.
+  *******/
 
 return((char)1);
 }
+
+
+
+
+
 
 
