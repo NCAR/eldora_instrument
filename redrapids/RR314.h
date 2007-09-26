972846253af5596e52f88847eb270881b9191fe9
Charles Martin <martinc@ucar.edu>
2007-09-26 00:58:06 +0000

    add comment
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@40 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- redrapids/RR314.h
+++ redrapids/RR314.h
@@ -27,28 +27,14 @@
 #define BUFFERPOOLSIZE    2*DMANUMGROUPS
 #define DMA_GROUPS_PER_INT 10
 
+/// Representation of the Red Rapids 314 4 channel receiver.
+/// The class manages the configuration of the receiver card,
+/// allocation of the DMA buffers, and responding to DMA buffer
+/// full interrupts.
 ///
-/// RRSnarfer implements a bandwidth limited data source for the Red River radio cards. 
-/// It uses Qt services for interaction with consumers of the digital receiver
-/// data.
-///
-/// The founding consideration for RRSnarfer is that the data stream from the Pentek 
-/// digital receiver is usually very "bursty". The agregate data rates from the receiver
-/// can be very high, and this data may arrive in large chunks spaced irregularly in time.
-/// Conversly, the data consumers that are receiving this data are usually unable to 
-/// process all of it. As an example, a Qt based plotting program would not be able
-/// to render MBs of timerseries data, and even if it could, the human eye would not be
-/// able to make use of it.
-//
-///  Thus RRSnarfer has two functions: to discard a fraction of
-/// the incoming data stream, and to deliver segments of the remaining data stream in a
-/// more or less regular timing. These functions are accomplished through the use of two 
-/// timers and a memory based FIFO. One timer is used to periodically check to see if
-/// data is available from the card. If there is data, it is read in large chunks,
-/// broken into smaller pieces, and placed in the FIFO. The second timer is used to 
-/// peridically remove a single entry from the FIFO and send it to the data consumer. 
-/// The same timer that periodically sends entries to the consumers also trims the FIFO
-/// to a fixed size, discard the excess entries.
+/// The filled DMA buffers are passed to a holding buffer.
+/// A condition variable will be set to allow a reading thread
+/// to empty the holding buffer.
 ///
 class RR314
 {
