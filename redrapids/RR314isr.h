11136febb5d2017d9273cafc4b66bf4f14b60553
Charles Martin <martinc@ucar.edu>
2007-09-28 19:30:50 +0000

    adding.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@61 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

---
+++ redrapids/RR314isr.h
@@ -0,0 +1,58 @@
+#include "RR314.h"
+
+//////////////////////////////////////////////////////////////////////
+/// @section RR314 C routines
+///
+/// The interrupt service routine that is called by windrvr6. It will
+/// be called on any interrupt that the RR314 has been configured
+/// to generate. Usually these will be the DMA group transfer complete
+/// interrupt. 
+/// 
+/// Note that these are not true interrupts, but rather a repackaging
+/// of interrupts from the windrvr6 driver, and deliverd (I think) as
+/// a signal. Since signals are delivered and handled asynchronously
+/// by the process, they can appear as interrupts. Also note that this
+/// signal processing will have semantics similar to a multithreaded
+/// application.
+///
+/// The Red River channel adapter library provides a C API for manipulating
+/// the 314 card. The library manages a descriptor of type s_ChannelAdapter 
+/// for each physical card in use. Access to any functionality of 
+/// a given card is always done through the descriptor.
+///
+/// The RR314 class is designed to provide a C++ representation of a
+/// 314 card. It holds the s_ChannelAdapter variable. However, since the 
+/// interrupt service routine is a C function, it is not natively aware
+/// of the RR314 class instance associated with a given 314 card. To allow
+/// the ISR code to access that instance, a static map is provided by
+/// the RR314 class which associates the pointer to the s_ChannelAdapter
+/// instance with the instance of the RR314 calls. The ISR can retrieve this
+/// pointer and thus access the controlling RR314 instance.
+///
+/// @param pCA The channel adapter structure associated with the card 
+/// providing the interrupt. This pointer has been associated with the card 
+/// via the Adapter_Open(&s_ChannelAdapter) function in the Red River 
+/// channel adapter library.
+void Adapter_ISR(s_ChannelAdapter *pCA);
+
+/// Process completed DMA groups for one channel. The data will be
+/// transferred from each DMA group buffer into the buffer queues
+/// in the RR314 class.
+/// @param pCA The channel adapter descriptor
+/// @param chan The channel number
+/// @param pRR314 The RR314 instance associated with pCA
+void processDMAGroups(s_ChannelAdapter *pCA, int chan, RedRapids::RR314* pRR314);
+
+/// Transfer one DMA group to a destination.
+/// @param dest Destination.
+/// @param pCA The channel adapter.
+/// @param chan The channel number.
+/// @param group The group number.
+void dmaCopy(UINT32* dest, s_ChannelAdapter* pCA, int chan, int group);
+
+/// A handler that is called for varoiuos shutdown signals.
+/// It's job is to force windrvr6 to return memory.
+/// It appears that this doesn't really work. After
+/// successively running RRSnarfer, Linux runs out of memory.
+void shutdownSignalHandler(int signo);
+
