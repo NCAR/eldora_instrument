0169766248fcfe16058e248418c3e45d4e6dd18f
Charles Martin <martinc@ucar.edu>
2007-10-15 16:07:02 +0000

    move configuration to central location, and write IOR to home directory
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@107 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- testDDS/runTest.sh
+++ testDDS/runTest.sh
@@ -4,27 +4,32 @@ pkill publisher
 pkill subscriber
 pkill DCPSInfoRepo
 
+CONF=~/eldora/conf
+REPOFILE=~/eldoraRepo.ior
+
+rm -f $REPOFILE
+
 sleep 1
 
 trap "pkill subscriber; pkill publisher; pkill DCPSInfoRepo" SIGINT SIGTERM
 
 echo "Starting DCPSInfoRepo"
-$DDS_ROOT/bin/DCPSInfoRepo  -ORBSvcConf conf/tcp.conf -o repo.ior -d conf/domain_ids &
+$DDS_ROOT/bin/DCPSInfoRepo  -ORBSvcConf $CONF/tcp.conf -d $CONF/domain_ids  -o $REPOFILE &
 
 sleep 2
 
 echo "Starting publisher"
-./publisher -ORBSvcConf conf/tcp.conf -DCPSConfigFile conf/simpleConf.ini &
+./publisher -ORBSvcConf $CONF/tcp.conf -DCPSConfigFile $CONF/simpleConf.ini &
 
 sleep 2
 
 echo "Starting subscriber 1"
-./subscriber -ORBSvcConf conf/tcp.conf -DCPSConfigFile conf/simpleConf.ini &
+./subscriber -ORBSvcConf $CONF/tcp.conf -DCPSConfigFile $CONF/simpleConf.ini  &
 
 echo "Starting subscriber 2"
-./subscriber -ORBSvcConf conf/tcp.conf -DCPSConfigFile conf/simpleConf.ini &
+./subscriber -ORBSvcConf $CONF/tcp.conf -DCPSConfigFile $CONF/simpleConf.ini  &
 
 echo "Starting subscriber 3"
-./subscriber -ORBSvcConf conf/tcp.conf -DCPSConfigFile conf/simpleConf.ini 
+./subscriber -ORBSvcConf $CONF/tcp.conf -DCPSConfigFile $CONF/simpleConf.ini
 
 
