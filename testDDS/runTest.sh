a0bbf40de4b0e1987cd0e93ba01b0ad6470bc0c1
Charles Martin <martinc@ucar.edu>
2007-10-20 02:25:29 +0000

    Bump up the sample and throughput rate.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@156 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- testDDS/runTest.sh
+++ testDDS/runTest.sh
@@ -7,7 +7,7 @@ CONF=~/eldora/conf
 REPOFILE=~/eldoraRepo.ior
 
 # The publisher invocation
-publish="./publisher --ORB $CONF/tcp.conf --DCPS $CONF/simpleConf.ini --delta 10000 --topic testtopic"
+publish="./publisher --ORB $CONF/tcp.conf --DCPS $CONF/simpleConf.ini --delta 5000 --topic testtopic"
 
 # The subscriber invocation
 subscribe="./subscriber --ORB $CONF/tcp.conf --DCPS $CONF/simpleConf.ini --topic testtopic"
