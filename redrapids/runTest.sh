216b6d301d3c4655facfaaa6450537927edb745e
Chris Burghart <burghart@ucar.edu>
2007-10-31 23:06:00 +0000

    Remove fixed paths and instead try to get everything from directories relative to the directory the script lives in.  Also, any args passed to the script are now handed to testrr314, mainly so we can run using "--simulate".
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@220 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- redrapids/runTest.sh
+++ redrapids/runTest.sh
@@ -1,14 +1,29 @@
 #! /bin/sh
+#
+# Usage runTest.sh [<testrr314_arg> ...]
+#
+
+#
+# Find the Eldora top directory (one directory up from where this script lives), 
+# so that we can find the config files
+#
+topdir="${0%/*}/.."
+# If we weren't run using an absolute path, the prepend the working directory
+# to get an absolute path
+if [[ ${topdir:0:1} != '/' ]]; then topdir="$PWD/$topdir"; fi
 
 # location of DDS configurtion files
-CONF=~/eldora/conf
+CONF=$topdir/conf
 
 # where to save the IOR
-REPOFILE=~/eldoraRepo.ior
+REPOFILE=/tmp/eldoraRepo.ior
 
 # The consumer invocation
-consume="/home/eldora/eldora/testdds/consumer --ORB $CONF/tcp.conf --DCPS $CONF/simpleConf.ini --pulse EldoraPulses --ts EldoraTS"
-produce="./testrr314 --ORB $CONF/tcp.conf --DCPS $CONF/simpleConf.ini --pub"
+consume="$topdir/testdds/consumer --ORB $CONF/tcp.conf \
+	--DCPS $CONF/simpleConf.ini --pulse EldoraPulses --ts EldoraTS"
+produce="$topdir/redrapids/testrr314 --ORB $CONF/tcp.conf --DCPS \
+	$CONF/simpleConf.ini --pub $*"
+
 # kill existing jobs
 pkill consumer
 pkill DCPSInfoRepo
@@ -17,8 +32,9 @@ pkill DCPSInfoRepo
 rm -f $REPOFILE
 
 # trap the signals on this script, and kill the jobs
-trap "echo \"killing jobs\"; pkill consumer; pkill publisher; pkill DCPSInfoRepo" SIGINT SIGTERM
-
+trap 'echo -e "$0: SHUTTING DOWN"; trap "" ERR; \
+	pkill consumer; pkill publisher; pkill DCPSInfoRepo' ERR SIGINT SIGTERM
+	
 echo "Starting DCPSInfoRepo"
 $DDS_ROOT/bin/DCPSInfoRepo  -ORBSvcConf $CONF/tcp.conf -d $CONF/domain_ids  -o $REPOFILE &
 
