95eefc4e8588967627e9bddf789ef1667a18d226
Charles Martin <martinc@ucar.edu>
2007-11-05 01:07:36 +0000

    Start DCPS, producer and consumers from separate scripts. If this script is run, all processes run correctly. However, if they are started in different shells with the individual run scripts, they cannot establish connections. This may lead to a solution for the problem where a DDS connection cannot yet be made between two machines. It is probably related to the switch to the corbaloc specification, since the individual run scripts worked before that.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@232 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- testdds/runTest.sh
+++ testdds/runTest.sh
@@ -1,38 +1,5 @@
 #! /bin/sh
 
-#
-# Find the Eldora top directory (one directory up from where this script lives), 
-# so that we can find the config files
-#
-topdir="${0%/*}/.."
-# If we weren't run using an absolute path, the prepend the working directory
-# to get an absolute path
-if [[ ${topdir:0:1} != '/' ]]; then topdir="$PWD/$topdir"; fi
-
-# location of DDS configurtion files
-CONF=$topdir/conf
-
-# where to save the IOR
-REPOFILE=/tmp/eldoraRepo.ior
-
-# The DCPSInforRepo invocation
-dcpsinforepo="$DDS_ROOT/bin/DCPSInfoRepo  \
-   -ORBSvcConf $CONF/tcp.conf \
-   -ORBListenEndpoints iiop://192.168.2.2:50000 \
-   -d $CONF/domain_ids"
-
-# The producer invocation
-producer="$topdir/testdds/producer \
-        --ORB $CONF/tcp.conf \
-        --DCPS $CONF/simpleConf.ini \
-        --delta 10000 \
-        --pulse EldoraPulses --ts EldoraTS"
-
-# The consumer invocation
-consume="$topdir/testdds/consumer --ORB $CONF/tcp.conf \
-         --DCPS $CONF/simpleConf.ini \
-         --pulse EldoraPulses --ts EldoraTS"
-
 # kill existing jobs
 pkill producer
 pkill consumer
@@ -41,21 +8,17 @@ pkill DCPSInfoRepo
 # trap the signals on this script, and kill the jobs
 trap "echo \"killing jobs\"; pkill consumer; pkill producer; pkill DCPSInfoRepo" SIGINT SIGTERM
 
-echo "Starting DCPSInfoRepo"
-$dcpsinforepo &
+./runDCPS.sh &
 sleep 2
 
-echo "Starting producer"
-$producer &
+./runProducer.sh &
+sleep 1
 
-echo "Starting consumer 1"
-$consume &
+./runConsumer.sh &
 
-echo "Starting consumer 2"
-$consume &
+./runConsumer.sh &
 
-echo "Starting consumer 3"
-$consume
+./runConsumer.sh
 
 
 
