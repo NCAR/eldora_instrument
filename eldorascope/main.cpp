bb803df4b459888de3014d22054187f05f965db6
Charles Martin <martinc@ucar.edu>
2007-11-13 00:34:38 +0000

    Add readers, and get it to build.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@255 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- eldorascope/main.cpp
+++ eldorascope/main.cpp
@@ -3,27 +3,78 @@
 #include <qapplication.h>
 #include <qstring.h>
 #include <iostream>
-#include "Reader.h"
+#include <boost/program_options.hpp>
 
-int
-main(int argc, char** argv)
-{
+#include "DDSReader.h"
+#include "ArgvParams.h"
+
+namespace po = boost::program_options;
+void parseArgs(
+        int argc,
+            char** argv,
+            std::string& pulseTopic,
+            std::string& tsTopic,
+            std::string& ORB,
+            std::string& DCPS) {
+
+    // get the options
+    po::options_description descripts("Options");
+
+    descripts.add_options() ("help", "describe options") ("pulsetopic",
+            po::value<std::string>(&pulseTopic), "DDS pulse topic")
+    ("tstopic", po::value<std::string>(&tsTopic), "DDS time series topic")
+    ("ORB", po::value<std::string>(&ORB), "ORB service configuration file (Corba ORBSvcConf arg)")
+    ("DCPS", po::value<std::string>(&DCPS), "DCPS configuration file (OpenDDS DCPSConfigFile arg)")
+    ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, descripts), vm);
+    po::notify(vm);
+
+    if (vm.count("help") || !vm.count("ORB") || !vm.count("DCPS") || !vm.count("pulsetopic") || !vm.count("tstopic")) {
+        std::cout << descripts << "\n";
+        exit(1);
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+int main(int argc, char** argv) {
+
+    std::string pulseTopic;
+    std::string tsTopic;
+    std::string ORB;
+    std::string DCPS;
+
+    parseArgs(argc, argv, pulseTopic, tsTopic, ORB, DCPS);
+
+    // we have to do this bit of translation since the 
+    // DDS routines want arguments starting with a single dash,
+    // whereas boost::program_options uses double dashes.
+    ArgvParams subParams(argv[0]);
+    subParams["-ORBSvcConf"] = ORB;
+    subParams["-DCPSConfigFile"] = DCPS;
+
+    // create the subscriber
+    DDSSubscriber subscriber(subParams.argc(), subParams.argv());
+    int subStatus = subscriber.status();
+    if (subStatus)
+        return subStatus;
 
     // create the readers
-    TestReader<PulseReader> pulseReader(subscriber, pulseTopic);
-    TestReader<TSReader> tsReader(subscriber, tsTopic);
+    PulseReader pulseReader(subscriber, pulseTopic);
+    TSReader tsReader(subscriber, tsTopic);
 
-   QApplication app(argc, argv);
-   QDialog* dialog = new QDialog;
-   
-   // create our test dialog. It will contain an SdrScope
-   SdrScope s(dialog);
+    QApplication app(argc, argv);
+    QDialog* dialog = new QDialog;
 
-  // if we don't show() the dialog, nothing appears!
-   dialog->show();
+    // create our test dialog. It will contain an SdrScope
+    SdrScope s(dialog);
 
-  // run the whole thing
-   app.exec();
+    // if we don't show() the dialog, nothing appears!
+    dialog->show();
 
-   return 0;
+    // run the whole thing
+    app.exec();
+
+    return 0;
 }
