a32659a22aa7c3f206a7bee3f4a4b8d36c0f453a
Charles Martin <martinc@ucar.edu>
2007-11-17 00:40:42 +0000

    A script for running just testrr314.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@272 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

---
+++ redrapids/runRR.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+#
+# Usage runRR.sh [<testrr314_arg> ...]
+#
+
+#
+# Find the Eldora top directory (one directory up from where this script lives), 
+# so that we can find the config files
+#
+topdir="${0%/*}/.."
+# If we weren't run using an absolute path, the prepend the working directory
+# to get an absolute path
+if [[ ${topdir:0:1} != '/' ]]; then topdir="$PWD/$topdir"; fi
+
+# location of DDS configurtion files
+CONF=$topdir/conf
+
+produce="$topdir/redrapids/testrr314 \
+   --ORB $CONF/tcp.conf \
+   --DCPS $CONF/producer.ini \
+   --pub $*"
+ 
+# kill existing jobs
+pkill testrr314
+
+# trap the signals on this script, and kill the jobs
+trap 'echo -e "$0: SHUTTING DOWN"; trap "" ERR; \
+	pkill testrr314' ERR SIGINT SIGTERM
+	
+echo "starting rr314 producer"
+$produce
+
+
