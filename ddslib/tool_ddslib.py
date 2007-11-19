4e7f31830ac0fe9ceb6fe9418187cd1566be94ce
Chris Burghart <burghart@ucar.edu>
2007-11-19 18:11:55 +0000

    comments and cleaning
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@279 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- ddslib/tool_ddslib.py
+++ ddslib/tool_ddslib.py
@@ -4,16 +4,17 @@
 tools = ['opendds', 'doxygen']
 env = Environment(tools = ['default'] + tools)
 
+libEldoraDds, sources, headers = env.DdsLibrary('EldoraDds.idl', env)
+
+html = env.Apidocs(sources + headers, DOXYFILE_FILE = "#/Doxyfile")
+
+Default([libEldoraDds, html])
+
+# Actually define and export the ddslib tool
 def ddslib(env):
     env.AppendLibrary('EldoraDds')
     env.AppendDoxref('EldoraDds')
     env.AppendUnique(CPPPATH = ['#/ddslib',])
     env.Require(tools)
-
+    
 Export('ddslib')
-
-libEldoraDds, sources, headers = env.DdsLibrary('EldoraDds.idl', env)
-
-html = env.Apidocs(sources + headers, DOXYFILE_FILE = "#/Doxyfile")
-
-Default(libEldoraDds)
