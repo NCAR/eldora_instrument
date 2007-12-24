0b936e0719650876344698032e479893e21cbbd0
Charles Martin <martinc@ucar.edu>
2007-12-24 04:25:07 +0000

    When creating the transport implementation, check first to see if it already exists, and reuse if true. Apparently only one is permitted per process. This fix allows a subscriber and a publisher to both be present in a process. Was discovered while building eldoraprod, which uses a subscriber to bring the abp values in, and a publisher to distribute the computed products.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@398 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- ddssvc/DDSPublisher.cpp
+++ ddssvc/DDSPublisher.cpp
@@ -7,102 +7,111 @@
 
 ////////////////////////////////////////////////////////////
 
-DDSPublisher::DDSPublisher(int argc, char* argv[]):
-transport_impl_id(1)
-{
+DDSPublisher::DDSPublisher(
+        int argc,
+            char* argv[]) :
+    transport_impl_id(1) {
 
-	_runStatus = run(argc, argv);
+    _runStatus = run(argc, argv);
 }
 
 ////////////////////////////////////////////////////////////
 
 DDSPublisher::~DDSPublisher() {
 
-	// Cleanup
-    
+    // Cleanup
+
     std::cout << "Cleaning up in ~DDSPublisher()\n";
-    
-	_participant->delete_contained_entities();
 
-	_dpf->delete_participant(_participant.in());
+    _participant->delete_contained_entities();
 
-	TheTransportFactory->release();
+    _dpf->delete_participant(_participant.in());
 
-	TheServiceParticipant->shutdown();
+    TheTransportFactory->release();
+
+    TheServiceParticipant->shutdown();
 
 }
 
 ////////////////////////////////////////////////////////////
 
 int DDSPublisher::status() {
-	return _runStatus;
+    return _runStatus;
 }
 
 ///////////////////////////////////////////////////////////
 
-int DDSPublisher::run(int argc, char *argv[]) {
-	try
-	{
-		_dpf = TheParticipantFactoryWithArgs(argc, argv);
+int DDSPublisher::run(
+        int argc,
+            char *argv[]) {
+    try
+    {
+        _dpf = TheParticipantFactoryWithArgs(argc, argv);
 
-		_participant = _dpf->create_participant(411,
-				PARTICIPANT_QOS_DEFAULT,
-				DDS::DomainParticipantListener::_nil());
-		if (CORBA::is_nil (_participant.in ())) {
-			cerr << "create_participant failed." << endl;
-			return 1;
-		}
+        _participant = _dpf->create_participant(411,
+                PARTICIPANT_QOS_DEFAULT,
+                DDS::DomainParticipantListener::_nil());
+        if (CORBA::is_nil (_participant.in ())) {
+            cerr << "create_participant failed." << endl;
+            return 1;
+        }
 
-	OpenDDS::DCPS::TransportImpl_rch tcp_impl =
-		TheTransportFactory->create_transport_impl (transport_impl_id,
-				::OpenDDS::DCPS::AUTO_CONFIG);
+        // Initialize the transport. Use the existing one if possible.
+        OpenDDS::DCPS::TransportImpl_rch tcp_impl;
+        try {
+            tcp_impl = TheTransportFactory->obtain(transport_impl_id);
+        } catch (...) {
+            tcp_impl =
+            TheTransportFactory->create_transport_impl (transport_impl_id,
+                    ::OpenDDS::DCPS::AUTO_CONFIG);
+        }
 
-		_publisher = _participant->create_publisher(PUBLISHER_QOS_DEFAULT,
-				DDS::PublisherListener::_nil());
-		if (CORBA::is_nil (_publisher.in ())) {
-			cerr << "create_publisher failed." << endl;
-			exit(1);
-		}
+        _publisher = _participant->create_publisher(PUBLISHER_QOS_DEFAULT,
+                DDS::PublisherListener::_nil());
+        if (CORBA::is_nil (_publisher.in ())) {
+            cerr << "create_publisher failed." << endl;
+            exit(1);
+        }
 
-		// Attach the publisher to the transport.
-		OpenDDS::DCPS::PublisherImpl* pub_impl =
-		OpenDDS::DCPS::reference_to_servant<OpenDDS::DCPS::PublisherImpl> (_publisher.in ());
-		if (0 == pub_impl) {
-			cerr << "Failed to obtain publisher servant" << endl;
-			exit(1);
-		}
+        // Attach the publisher to the transport.
+        OpenDDS::DCPS::PublisherImpl* pub_impl =
+        OpenDDS::DCPS::reference_to_servant<OpenDDS::DCPS::PublisherImpl> (_publisher.in ());
+        if (0 == pub_impl) {
+            cerr << "Failed to obtain publisher servant" << endl;
+            exit(1);
+        }
 
-		OpenDDS::DCPS::AttachStatus status = pub_impl->attach_transport(tcp_impl.in());
-		if (status != OpenDDS::DCPS::ATTACH_OK) {
-			std::string status_str;
-			switch (status) {
-				case OpenDDS::DCPS::ATTACH_BAD_TRANSPORT:
-				status_str = "ATTACH_BAD_TRANSPORT";
-				break;
-				case OpenDDS::DCPS::ATTACH_ERROR:
-				status_str = "ATTACH_ERROR";
-				break;
-				case OpenDDS::DCPS::ATTACH_INCOMPATIBLE_QOS:
-				status_str = "ATTACH_INCOMPATIBLE_QOS";
-				break;
-				default:
-				status_str = "Unknown Status";
-				break;
-			}
-			cerr << "Failed to attach to the transport. Status == "
-			<< status_str.c_str() << endl;
-			exit(1);
-		}
+        OpenDDS::DCPS::AttachStatus status = pub_impl->attach_transport(tcp_impl.in());
+        if (status != OpenDDS::DCPS::ATTACH_OK) {
+            std::string status_str;
+            switch (status) {
+                case OpenDDS::DCPS::ATTACH_BAD_TRANSPORT:
+                status_str = "ATTACH_BAD_TRANSPORT";
+                break;
+                case OpenDDS::DCPS::ATTACH_ERROR:
+                status_str = "ATTACH_ERROR";
+                break;
+                case OpenDDS::DCPS::ATTACH_INCOMPATIBLE_QOS:
+                status_str = "ATTACH_INCOMPATIBLE_QOS";
+                break;
+                default:
+                status_str = "Unknown Status";
+                break;
+            }
+            cerr << "Failed to attach to the transport. Status == "
+            << status_str.c_str() << endl;
+            exit(1);
+        }
 
-	}
-	catch (CORBA::Exception& e)
-	{
-		cerr << "PUB: Exception caught in main.cpp:" << endl
-		<< e << endl;
-		exit(1);
-	}
+    }
+    catch (CORBA::Exception& e)
+    {
+        cerr << "PUB: Exception caught in main.cpp:" << endl
+        << e << endl;
+        exit(1);
+    }
 
-	return 0;
+    return 0;
 }
 
 ////////////////////////////////////////////////////////////
--- ddssvc/DDSSubscriber.cpp
+++ ddssvc/DDSSubscriber.cpp
@@ -25,9 +25,11 @@ OpenDDS::DCPS::TransportIdType transport_impl_id = 1;
 
 ////////////////////////////////////////////////////////////
 
-DDSSubscriber::DDSSubscriber(int argc, char* argv[]) {
-	
-	_runStatus = run(argc, argv);
+DDSSubscriber::DDSSubscriber(
+        int argc,
+            char* argv[]) {
+
+    _runStatus = run(argc, argv);
 
 }
 
@@ -36,103 +38,108 @@ DDSSubscriber::DDSSubscriber(int argc, char* argv[]) {
 DDSSubscriber::~DDSSubscriber() {
 
     std::cout << "DDSSubscriber shutting down\n";
-    
-	_participant->delete_contained_entities();
 
-	_dpf->delete_participant(_participant.in());
+    _participant->delete_contained_entities();
 
-	TheTransportFactory->release();
+    _dpf->delete_participant(_participant.in());
 
-	TheServiceParticipant->shutdown();
+    TheTransportFactory->release();
+
+    TheServiceParticipant->shutdown();
 
 }
 
 ////////////////////////////////////////////////////////////
 
 int DDSSubscriber::status() {
-	return _runStatus;
+    return _runStatus;
 }
 
 ////////////////////////////////////////////////////////////
 
-int DDSSubscriber::run(int argc, char *argv[]) {
-	try
-	{
-		_dpf = TheParticipantFactoryWithArgs(argc, argv);
-		_participant = _dpf->create_participant(411,
-				PARTICIPANT_QOS_DEFAULT,
-				DDS::DomainParticipantListener::_nil());
-		if (CORBA::is_nil (_participant.in ())) {
-			cerr << "create_participant failed." << endl;
-			return 1;
-		}
+int DDSSubscriber::run(
+        int argc,
+            char *argv[]) {
+    try
+    {
+        _dpf = TheParticipantFactoryWithArgs(argc, argv);
+        _participant = _dpf->create_participant(411,
+                PARTICIPANT_QOS_DEFAULT,
+                DDS::DomainParticipantListener::_nil());
+        if (CORBA::is_nil (_participant.in ())) {
+            cerr << "create_participant failed." << endl;
+            return 1;
+        }
 
-		// Initialize the transport
-		OpenDDS::DCPS::TransportImpl_rch transport_impl =
-		TheTransportFactory->create_transport_impl (transport_impl_id,
-				::OpenDDS::DCPS::AUTO_CONFIG);
+        // Initialize the transport. Use the existing one if possible.
+        OpenDDS::DCPS::TransportImpl_rch transport_impl;
+        try {
+            transport_impl = TheTransportFactory->obtain(transport_impl_id);
+        } catch (...) {
+            transport_impl =
+            TheTransportFactory->create_transport_impl (transport_impl_id,
+                    ::OpenDDS::DCPS::AUTO_CONFIG);
+        }
 
-		// Create the subscriber and attach to the corresponding
-		// transport.
-		_subscriber =
-		_participant->create_subscriber(SUBSCRIBER_QOS_DEFAULT,
-				DDS::SubscriberListener::_nil());
-		if (CORBA::is_nil (_subscriber.in ())) {
-			cerr << "Failed to create_subscriber." << endl;
-			exit(1);
-		}
+        // Create the subscriber and attach to the corresponding
+        // transport.
+        _subscriber =
+        _participant->create_subscriber(SUBSCRIBER_QOS_DEFAULT,
+                DDS::SubscriberListener::_nil());
+        if (CORBA::is_nil (_subscriber.in ())) {
+            cerr << "Failed to create_subscriber." << endl;
+            exit(1);
+        }
 
-		// Attach the subscriber to the transport.
-		OpenDDS::DCPS::SubscriberImpl* sub_impl =
-		OpenDDS::DCPS::reference_to_servant<OpenDDS::DCPS::SubscriberImpl> (_subscriber.in ());
-		if (0 == sub_impl) {
-			cerr << "Failed to obtain subscriber servant\n" << endl;
-			exit(1);
-		}
+        // Attach the subscriber to the transport.
+        OpenDDS::DCPS::SubscriberImpl* sub_impl =
+        OpenDDS::DCPS::reference_to_servant<OpenDDS::DCPS::SubscriberImpl> (_subscriber.in ());
+        if (0 == sub_impl) {
+            cerr << "Failed to obtain subscriber servant\n" << endl;
+            exit(1);
+        }
 
-		OpenDDS::DCPS::AttachStatus status = sub_impl->attach_transport(transport_impl.in());
-		if (status != OpenDDS::DCPS::ATTACH_OK) {
-			std::string status_str;
-			switch (status) {
-				case OpenDDS::DCPS::ATTACH_BAD_TRANSPORT:
-				status_str = "ATTACH_BAD_TRANSPORT";
-				break;
-				case OpenDDS::DCPS::ATTACH_ERROR:
-				status_str = "ATTACH_ERROR";
-				break;
-				case OpenDDS::DCPS::ATTACH_INCOMPATIBLE_QOS:
-				status_str = "ATTACH_INCOMPATIBLE_QOS";
-				break;
-				default:
-				status_str = "Unknown Status";
-				break;
-			}
-			cerr << "Failed to attach to the transport. Status == "
-			<< status_str.c_str() << endl;
-			exit(1);
-		}
+        OpenDDS::DCPS::AttachStatus status = sub_impl->attach_transport(transport_impl.in());
+        if (status != OpenDDS::DCPS::ATTACH_OK) {
+            std::string status_str;
+            switch (status) {
+                case OpenDDS::DCPS::ATTACH_BAD_TRANSPORT:
+                status_str = "ATTACH_BAD_TRANSPORT";
+                break;
+                case OpenDDS::DCPS::ATTACH_ERROR:
+                status_str = "ATTACH_ERROR";
+                break;
+                case OpenDDS::DCPS::ATTACH_INCOMPATIBLE_QOS:
+                status_str = "ATTACH_INCOMPATIBLE_QOS";
+                break;
+                default:
+                status_str = "Unknown Status";
+                break;
+            }
+            cerr << "Failed to attach to the transport. Status == "
+            << status_str.c_str() << endl;
+            exit(1);
+        }
 
-	}
-	catch (CORBA::Exception& e)
-	{
-		cerr << "SUB: Exception caught in main ():" << endl << e << endl;
-		return 1;
-	}
+    }
+    catch (CORBA::Exception& e)
+    {
+        cerr << "SUB: Exception caught in main ():" << endl << e << endl;
+        return 1;
+    }
 
-	return 0;
+    return 0;
 }
 
 ////////////////////////////////////////////////////////////
 
 DDS::DomainParticipant_var& DDSSubscriber::getParticipant() {
-	return _participant;
+    return _participant;
 }
-
 
 ////////////////////////////////////////////////////////////
 
 DDS::Subscriber_var& DDSSubscriber::getSubscriber() {
-	return _subscriber;
+    return _subscriber;
 }
-
 
