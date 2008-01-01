9d59b13246b961717b12d2cceffbbedab4b08537
Charles Martin <martinc@ucar.edu>
2008-01-01 00:25:25 +0000

    remove products type definition from EldoraScope.h, since it will also be used in other applications such as the PPI display.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@429 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

---
+++ eldorascope/ProductTypes.h
@@ -0,0 +1,18 @@
+#ifndef PRODUCTTYPES_INC_
+#define PRODUCTTYPES_INC_
+
+/// Product types.
+enum PRODUCT_TYPES {
+    PROD_P1, ///< P1 (from ABP chan 1)
+    PROD_P2, ///< P1 (from ABP chan 1)
+    PROD_P3, ///< P1 (from ABP chan 1)
+    PROD_P4, ///< P1 (from ABP chan 1)
+    PROD_DBM, ///< dBm 
+    PROD_DBZ, ///< dBz
+    PROD_SNR, ///< SNR
+    PROD_VEL, ///< velocity
+    PROD_WIDTH, ///< spectral width
+    PROD_NCP,   ///< Normailzed coherent power
+};
+
+#endif
