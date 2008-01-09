157121fa4cd6a088189b05f9db299b774b55a8e6
Charles Martin <martinc@ucar.edu>
2008-01-09 13:06:55 +0000

    Add the ddsqt tool.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@504 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

---
+++ ddsqt/tool_ddsqt.py
@@ -0,0 +1,34 @@
+#
+# Rules to build ddsqt and export it as a SCons tool
+#
+import os
+
+tools = ['ddssvc', 'qt4']
+env = Environment(tools = ['default'] + tools)
+
+qt4modules = ['QtCore',]
+env.EnableQt4Modules(qt4modules)
+
+#tooldir = env.Dir('.')
+
+libsources = Split("""
+EldoraQtSource.cpp
+EldoraQtTSSource.cpp
+EldoraQtProductsSource.cpp
+""")
+
+libddsqt = env.Library('ddsqt', libsources)
+
+Default(libddsqt)
+
+def ddsqt(env):
+    # The ddsqt library
+    env.AppendUnique(CPPPATH = ['#/ddsqt',])
+    env.AppendLibrary('ddsqt')
+    
+    tools = ['ddssvc']
+    env.Require(tools)
+
+Export('ddsqt')
+
+
