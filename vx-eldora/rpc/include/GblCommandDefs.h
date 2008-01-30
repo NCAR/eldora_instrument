075feeecfc0fa436d3d386cbdb28e9e71d65a7c6
Chris Burghart <burghart@ucar.edu>
2008-01-30 22:51:50 +0000

    moved Hskp* headers to rpc/hskp, GblCommands.h renamed to GblCommandDefs.h, moved TapeHeader.h to rpc/header, added a TODO file
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1651 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

---
+++ vx-eldora/rpc/include/GblCommandDefs.h
@@ -0,0 +1,17 @@
+/*
+ *	$Id$
+ */
+#ifndef _GBLCOMMANDDEFS_H_
+#define _GBLCOMMANDDEFS_H_
+
+#define START   0x1
+#define STOP    0x2
+#define INIT    0x4
+#define REBOOT  0x8
+#define RELOAD  0x80000000
+#define HEADER  0xf0000000
+#define SEND_STATUS  0xffffffff
+
+#define ALIGN_MINI   0x10
+
+#endif /* _GBLCOMMANDDEFS_H_ */
