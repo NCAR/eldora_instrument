cbc0bc1bfea73fdf795d2470193f7c65ab62914a
Charles Martin <martinc@ucar.edu>
2008-05-08 05:27:50 +0000

    add verify decode to EldoraHeader class
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@1954 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- src/eldoragui/EldoraHeader.py
+++ src/eldoragui/EldoraHeader.py
@@ -6,7 +6,7 @@ import time
 import re
 import subprocess
 
-class EldoraHeaderBlock(dict):
+class EldoraBlock(dict):
     ''' 
     Represent a single header block.
     The fields within the block are dictionary entries
@@ -24,20 +24,29 @@ class EldoraHeader(list):
     an eldora header file. An external header dump command
     is used to dump a specified header, and the output from
     that command is parsed.
+    The header blocks are collected in our list.
+    
+    This class also runs a header verify command, and collects 
+    that output in a list of blocks. This list is the data
+    member verifyBlocks.
     '''
-    def __init__(self, headerDumpCmd, headerFile):
-        self.headerDumpCmd = headerDumpCmd
+    def __init__(self, hdrDumpCmd, hdrVerifyCmd, headerFile):
+        self.hdrDumpCmd = hdrDumpCmd
+        self.hdrVerifyCmd = hdrVerifyCmd
         self.headerFile = headerFile
-        self.blocks = list()
+        # collect the header blocks
         self.readHeader()
+        self.verifyBlocks = []
+        self.verify()
         
 ###############################################################
     def readHeader(self):
         ''' 
-        Read and parse the header file, and save it in self.blocks
-        self.blocks is a list of EldoraBlock
+        Read and parse the header file, collect the
+        header blocks, and save it in our list of
+        EldoraBlocks
         '''
-        cmd = self.headerDumpCmd + [self.headerFile,]
+        cmd = self.hdrDumpCmd + [self.headerFile,]
         c = subprocess.Popen(args=cmd, stdout=subprocess.PIPE)
         self[:] = list()
         blockstart = False
@@ -51,14 +60,45 @@ class EldoraHeader(list):
             else:
                 mainkey, subkey, comment, value = self.parseLine(line)
                 if mainkey != '':
-                    if blockstart:
-                        block = EldoraHeaderBlock(mainkey)
-                        block.addField(subkey, comment, value)
-                        self.append(block)
-                        blockstart = False
-                    else:
-                        block = self[-1]
-                        block.addField(subkey, comment, value)
+                    if mainkey[0:1] != 'v':
+                        if blockstart:
+                            block = EldoraBlock(mainkey)
+                            block.addField(subkey, comment, value)
+                            self.append(block)
+                            blockstart = False
+                        else:
+                            block = self[-1]
+                            block.addField(subkey, comment, value)
+                            
+###############################################################
+    def verify(self):
+        ''' 
+        Read and parse the header file verify output, and save it 
+        as ldoraBlocks in self.verifyBlocks
+        '''
+        cmd = self.hdrVerifyCmd + [self.headerFile,]
+        c = subprocess.Popen(args=cmd, stdout=subprocess.PIPE)
+        self.verifyBlocks[:] = list()
+        blockstart = False
+        while 1:
+            line = c.stdout.readline()
+            if len(line) == 0:
+                break
+            if line.find('****') != -1:
+                # the next non-comment line will be the start of a block
+                blockstart = True
+            else:
+                mainkey, subkey, comment, value = self.parseLine(line)
+                if mainkey != '':
+                    if mainkey[0:1] == 'V':
+                        if blockstart:
+                            block = EldoraBlock(mainkey)
+                            block.addField(subkey, comment, value)
+                            self.verifyBlocks.append(block)
+                            blockstart = False
+                        else:
+                            block = self.verifyBlocks[-1]
+                            block.addField(subkey, comment, value)
                         
            
 ###############################################################
@@ -128,9 +168,14 @@ class EldoraHeader(list):
         print 'enter the header file name: '
         line = sys.stdin.readline()
         file = re.sub('\n', '', line)
-        cmd = ['../headermaker/dumpheader','-h']
-        eldoraHdr = EldoraHeader(headerDumpCmd=cmd, headerFile=file)
+        dumpcmd = ['../headermaker/dumpheader','-h']
+        verifycmd = ['../headermaker/dumpheader','-v']
+        eldoraHdr = EldoraHeader(hdrDumpCmd=dumpcmd, hdrVerifyCmd=verifycmd, headerFile=file)
         for b in eldoraHdr:
+            print '*************************** ', b.type, '*************************** '
+            for f in b.items():
+                print f
+        for b in eldoraHdr.verifyBlocks:
             print '*************************** ', b.type, '*************************** '
             for f in b.items():
                 print f
