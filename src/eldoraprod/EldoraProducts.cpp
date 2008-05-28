503d483b727c2b78ad15af6e3c819e48dacc48a6
Charles Martin <martinc@ucar.edu>
2008-05-28 01:04:26 +0000

    Extending the momments term pre-calculations.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@2116 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- src/eldoraprod/EldoraProducts.cpp
+++ src/eldoraprod/EldoraProducts.cpp
@@ -4,40 +4,54 @@
 #define TOSHORT(data, gain, offset) ((short)(((data)*(gain))-(offset)))
 
 ////////////////////////////////////////////////////
-EldoraProducts::EldoraProducts(
-        DDSPublisher& publisher,
-            std::string productsTopic,
-            bool dualPrt) :
+EldoraProducts::EldoraProducts(DDSPublisher& publisher,
+                               std::string productsTopic,
+                               bool dualPrt) :
     _rays(0), _publisher(publisher), _productsTopic(productsTopic),
-            _productsWriter(publisher, productsTopic), _droppedRays(0), _dualPrt(dualPrt) {
-            
-}
-
-////////////////////////////////////////////////////
-EldoraProducts::~EldoraProducts() {
+            _productsWriter(publisher, productsTopic), _droppedRays(0),
+            _dualPrt(dualPrt), _termsInitialized(false)
+{
 
 }
 
 ////////////////////////////////////////////////////
-void EldoraProducts::newRayData(
-        std::vector<std::vector<EldoraDDS::Ray*> >& rays) {
+EldoraProducts::~EldoraProducts()
+{
+
+}
+
+////////////////////////////////////////////////////
+void EldoraProducts::newRayData(std::vector<std::vector<EldoraDDS::Ray*> >& rays)
+{
 
     _rays++;
 
-    // We have four matched rays. 
+    // make sure that we got the right number of prts
+    if ((_dualPrt && rays.size() != 2) || (!_dualPrt && rays.size() != 1)) {
+        std::cout << "prt mode does not match the number of prt ids received\n";
+        _droppedRays++;
+        return;
+    }
+
+    _gates = rays[0][0]->abp.length()/3;
+    initTerms(rays);
+
+    // We have four matched rays for each prt 
     Products* products = _productsWriter.getEmptyItem();
     if (products) {
-        
+
         // initiallize the fixed fields in Products.
         initProducts(products);
+
+        rawPower(rays);
         
         int prt = 0;
-        
+
         // transfer ray metadata
         products->radarId = rays[prt][0]->radarId;
         products->timestamp = rays[prt][0]->hskp.rayNum;
         products->rotAngle = rays[prt][0]->hskp.radarRotAngle;
-        
+
         // The abp rays are three times as long as the
         // product beams. 
         int productsLength = rays[prt][0]->abp.length()/3;
@@ -53,36 +67,36 @@ void EldoraProducts::newRayData(
         products->dbz.length(productsLength);
         products->sw.length(productsLength);
         products->ncp.length(productsLength);
-        
+
         // Compute the products.
-         for (int i = 0; i < productsLength; i++) {
-             int p = 3*i;
+        for (int gate = 0; gate < productsLength; gate++) {
+            int p = 3*gate;
             // temprary hack: Sum all four channel powers to create
             // dbz.
             double dbz = 0.0;
             for (unsigned int f = 0; f < 4; f++) {
                 dbz += rays[prt][f]->abp[p+2];
             }
-            
-            products->dm[i] = TOSHORT(dbz/(((i+1.0)*(i+1.0))/100.0), products->dmGain, products->dmOffset);
-                
-            products->dbz[i] = TOSHORT(dbz, products->dbzGain, products->dbzOffset)*100-70;
 
-            double pdbm1 = 10.0*log10(rays[prt][0]->abp[p+2]);
-            double pdbm2 = 10.0*log10(rays[prt][1]->abp[p+2]);
-            double pdbm3 = 10.0*log10(rays[prt][2]->abp[p+2]);
-            double pdbm4 = 10.0*log10(rays[prt][3]->abp[p+2]);
-            
-            products->p1[i] = TOSHORT(pdbm1, products->p1Gain, products->p1Offset);
-            products->p2[i] = TOSHORT(pdbm2, products->p2Gain, products->p2Offset);
-            products->p3[i] = TOSHORT(pdbm3, products->p3Gain, products->p3Offset);
-            products->p4[i] = TOSHORT(pdbm4, products->p4Gain, products->p4Offset);
-           
-            products->vr[i] = TOSHORT((i-productsLength/2.0), products->vrGain, products->vrOffset);
-            products->vs[i] = TOSHORT((i-productsLength/2.0)/10.0, products->vsGain, products->vsOffset);
-            products->vl[i] = TOSHORT(i*(i-productsLength/2.0)/productsLength, products->vlGain, products->vlOffset);
-            products->sw[i] = TOSHORT(1.0*i, products->swGain, products->swOffset);
-            products->ncp[i] = TOSHORT(1.0/((i+1.0)*(i+1.0)), products->ncpGain, products->ncpOffset);
+            products->dm[gate]
+                    = TOSHORT(dbz/(((gate+1.0)*(gate+1.0))/100.0), products->dmGain, products->dmOffset);
+
+            products->dbz[gate] = TOSHORT(dbz, products->dbzGain, products->dbzOffset)*100-70;
+
+            products->p1[gate] = TOSHORT(_terms.Praw_k[0][gate], products->p1Gain, products->p1Offset);
+            products->p2[gate] = TOSHORT(_terms.Praw_k[1][gate], products->p2Gain, products->p2Offset);
+            products->p3[gate] = TOSHORT(_terms.Praw_k[2][gate], products->p3Gain, products->p3Offset);
+            products->p4[gate] = TOSHORT(_terms.Praw_k[3][gate], products->p4Gain, products->p4Offset);
+
+            products->vr[gate]
+                    = TOSHORT((gate-productsLength/2.0), products->vrGain, products->vrOffset);
+            products->vs[gate]
+                    = TOSHORT((gate-productsLength/2.0)/10.0, products->vsGain, products->vsOffset);
+            products->vl[gate]
+                    = TOSHORT(gate*(gate-productsLength/2.0)/productsLength, products->vlGain, products->vlOffset);
+            products->sw[gate] = TOSHORT(1.0*gate, products->swGain, products->swOffset);
+            products->ncp[gate]
+                    = TOSHORT(1.0/((gate+1.0)*(gate+1.0)), products->ncpGain, products->ncpOffset);
         }
         // Publish the products.
         _productsWriter.publishItem(products);
@@ -94,47 +108,186 @@ void EldoraProducts::newRayData(
     return;
 }
 ////////////////////////////////////////////////////
+void EldoraProducts::rawPower(RayData& rays)
+{
+    switch (_dualPrt)
+    {
+    case false:
+        // single prt
+        for (int g = 0; g < _gates; g++) {
+            for (unsigned int k = 0; k < 4; k++) {
+                // pick the p out of abp
+                double p = rays[0][k]->abp[3*g+2];
+                // calculate Praw_k
+                _terms.Praw_k[k][g] = 10*log10(p);
+            }
+        }
+        break;
+        
+    case true:
+        // dual prt
+        for (int g = 0; g < _gates; g++) {
+            for (unsigned int k = 0; k < 4; k++) {
+                // pick the long and short p out of abp
+                double pks = rays[0][k]->abp[3*g+2];
+                double pkl = rays[1][k]->abp[3*g+2];
+                // calculate Praw_k
+                _terms.Praw_k[k][g] = 10*log10((pks+pkl)/2.0);
+            }
+        }
+        break;
+    }
+}
 
-int EldoraProducts::numRays() {
+////////////////////////////////////////////////////
+void EldoraProducts::antennaPower(RayData& rays)
+{
+    switch (_dualPrt)
+    {
+    case false:
+        // single prt
+        for (int g = 0; g < _gates; g++) {
+            for (unsigned int k = 0; k < 4; k++) {
+                // pick the p out of abp
+                double p = rays[0][k]->abp[3*g+2];
+                // calculate Praw_k
+                _terms.Praw_k[k][g] = 10*log10(p);
+            }
+        }
+        break;
+        
+    case true:
+        // dual prt
+        for (int g = 0; g < _gates; g++) {
+            for (unsigned int k = 0; k < 4; k++) {
+                // pick the long and short p out of abp
+                double pks = rays[0][k]->abp[3*g+2];
+                double pkl = rays[1][k]->abp[3*g+2];
+                // calculate Praw_k
+                _terms.Praw_k[k][g] = 10*log10((pks+pkl)/2.0);
+            }
+        }
+        break;
+    }
+}
+
+////////////////////////////////////////////////////
+
+int EldoraProducts::numRays()
+{
     int n = _rays;
     _rays = 0;
     return n;
 }
+
 ////////////////////////////////////////////////////
-void EldoraProducts::initProducts(EldoraDDS::Products* p) {
+void EldoraProducts::initTerms(RayData& rays)
+{
+    if (_termsInitialized)
+        return;
     
+    /// The x band gain.
+    double xBandGain;
+    /// The channel frequencies.
+    double freqs[4];
+    /// The receiver gains.
+    double rxGain[4];
+    /// The input losses.
+    double lossIn[4];
+    /// The prt in single mode.
+    double prt;
+    /// The short prt in dual mode.
+    double prtShort;
+    /// The long prt in dual mode.
+    double prtLong;
+
+    // Initialize the terms
+    _terms.init(_gates,
+                xBandGain,
+                freqs,
+                rxGain,
+                lossIn,
+                prt,
+                prtShort,
+                prtLong);
+    
+    _termsInitialized = true;
+}
+
+////////////////////////////////////////////////////
+void EldoraProducts::initProducts(EldoraDDS::Products* p)
+{
+
     p->p1Gain = 1.0e2;
     p->p1Offset = 9000.0;
-    
+
     p->p2Gain = 1.0e2;
     p->p2Offset = 9000.0;
-    
+
     p->p3Gain = 1.0e2;
     p->p3Offset = 9000.0;
-    
+
     p->p4Gain = 1.0e2;
     p->p4Offset = 9000.0;
-    
+
     p->dmGain = 1000.0;
     p->dmOffset = 0.0;
-    
+
     p->dbzGain = 1000.0;
     p->dbzOffset = 0.0;
-    
+
     p->vrGain = 100.0;
     p->vrOffset = 0.0;
-    
+
     p->vsGain = 100.0;
     p->vsOffset = 0.0;
-    
+
     p->vlGain = 100.0;
     p->vlOffset = 0.0;
-     
+
     p->swGain = 100.0;
     p->swOffset = 32768.0;
-    
+
     p->ncpGain = 1000.0;
     p->ncpOffset = 32768.0;
+}
+////////////////////////////////////////////////////
+void ProductsTerms::init(int gates, 
+                         double xBandGain,
+                         double freqs[4], 
+                         double rxGain[4], 
+                         double lossIn[4],
+                         double prt,
+                         double prtShort,
+                         double prtLong) {
+
+    Praw_k.resize(4);
+    Pant_k.resize(4);
+    Pant.resize(gates);
+    Psig_k.resize(4);
+    Psig.resize(gates);
+    V_k.resize(4);
+    Psigs_k.resize(4);
+    Psigl_k.resize(4);
+    Psigs.resize(gates);
+    Psigl.resize(gates);
+    Thetas.resize(gates);
+    Thetal.resize(gates);
+    DeltaTheta.resize(gates);
+    Dbz.resize(gates);
+    W.resize(gates);
+    Ncp.resize(gates);
+    Vs.resize(gates);
+    Vr.resize(gates);
+
+    for (unsigned int c = 0; c < 4; c++) {
+        Praw_k[c].resize(gates);
+        Pant_k[c].resize(gates);
+        Psig_k[c].resize(gates);
+        Psigs_k[c].resize(gates);
+        Psigl_k[c].resize(gates);
+    }
+ 
 }
 ////////////////////////////////////////////////////
 
--- src/eldoraprod/EldoraProducts.h
+++ src/eldoraprod/EldoraProducts.h
@@ -6,20 +6,80 @@
 #include "DDSPublisher.h"
 #include "DDSWriter.h"
 
-class EldoraProducts {
+/// A structure to hold intermediate terms and final values 
+/// for the moments calculations. A vector is used which can
+/// be resized for the number of gates being processed. For
+/// some variables, the vector of gates is contained within a vector
+/// of channels. These variables have _k suffixed to the member name.
+/// Indexing into the fields observes the following conventions:
+/// <br> Praw_k[channel][gate]
+/// <br> Praw[gate]
+/// <br> Some of the precomputed factors are indexed only by channel, such as:
+/// <br> lambda_k[channel]
+struct ProductsTerms
+{
+        void init(int gates,
+                  double xBandGain,
+                  double freqs[4],
+                  double rxGain[4],
+                  double lossIn[4],
+                  double prt,
+                  double prtShort,
+                  double prtLong);
+        /// Data accross all gates or all channels
+        typedef std::vector<double> TermData1D;
+        /// Data across all gates and all channels
+        typedef std::vector<TermData1D> TermData2D;
+        TermData2D Praw_k; ///< Raw power at receiver, by channel.
+        TermData2D Pant_k; ///< Total power at antenna, by channel
+        TermData1D Pant; ///< Total power at antenna
+        TermData2D Psig_k; ///< Signal power at receiver, by channel
+        TermData1D Psig; ///< Signal power at receiver
+        TermData2D V_k; ///< Velocity, by channel
+        TermData2D Psigs_k; ///< Signal power at receiver, short pulse, by channel
+        TermData2D Psigl_k; ///< Signal power at receiver, long pulse, by channel
+        TermData1D Psigs; ///< Signal power at receiver, short pulse
+        TermData1D Psigl; ///< Signal power at receiver, long pulse
+        TermData1D Thetas; ///< Accumulated phase change of short prts.
+        TermData1D Thetal; ///< Accumulated phase change of long prt
+        TermData1D DeltaTheta; ///< Phase difference between short and long prts.
+        TermData1D Dbz; ///< Reflectivity
+        TermData1D W; ///< Spectral width
+        TermData1D Ncp; ///< Normalized coherent power
+        TermData1D Vs; ///< Short pulse velocity
+        TermData1D Vr; ///< Long pulse velocity
+
+};
+
+/// A number of calculation factors are precomputed once, when the first ray is received.
+/// If operating in dual prt mode, we will need a short prt ray and a long prt ray 
+/// in order to calculate these constants.
+class EldoraProducts
+{
+        typedef std::vector<std::vector<EldoraDDS::Ray*> > RayData;
     public:
-        EldoraProducts(DDSPublisher& publisher, std::string productsTopic, bool dualPrt);
+        EldoraProducts(DDSPublisher& publisher,
+                       std::string productsTopic,
+                       bool dualPrt);
         virtual ~EldoraProducts();
 
-        void newRayData(
-                std::vector<std::vector<EldoraDDS::Ray*> >& pRay);
-        
+        void newRayData(RayData& ray);
+
         int numRays();
 
     protected:
         /// Initialize the fixed fields in a Products item.
-        /// @param p te products item to be initialized.
+        /// @param p the products item to be initialized.
         void initProducts(EldoraDDS::Products* p);
+        /// Initalize the entries in the Productsterms structure.
+        /// Precomputed factors are initialized and vectors are resized.
+        /// The ray data provides the header information.
+        /// @rays Ray data with housekeeping.
+        void initTerms(RayData& rays);
+        /// Calculate raw power at receiver
+        void rawPower(RayData& rays);
+        /// Calculate power at antenna
+        void antennaPower(RayData& rays);
         /// The number of rays that have been received.
         int _rays;
         /// The DDS publisher
@@ -33,6 +93,13 @@ class EldoraProducts {
         int _droppedRays;
         /// True if we are in dual prt mode
         bool _dualPrt;
-
+        /// Is filled in with calculation results as each 
+        /// step in the products computations are completed.
+        ProductsTerms _terms;
+        /// Contains the number of gates in the current set of rays
+        int _gates;
+        /// set to false if _terms still needs to be initialized.
+        /// Used by initterms().
+        bool _termsInitialized;
 };
 #endif
