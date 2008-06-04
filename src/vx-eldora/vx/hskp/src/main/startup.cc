0aa97740023ba65645f0b915d2f0230a74c87c7c
Chris Burghart <burghart@ucar.edu>
2008-06-04 21:35:52 +0000

    Remove checkin history from the initial comments, since it's available from Subversion. Update some prints, and change where currStatus gets initialized.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@2178 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- src/vx-eldora/vx/hskp/src/main/startup.cc
+++ src/vx-eldora/vx/hskp/src/main/startup.cc
@@ -2,48 +2,7 @@
  *	$Id$
  *
  * Module: startup  
- * Original Author: Craig Walther 
- *      Copywrited by the National Center for Atmospheric Research
- *	Date:		 $Date$
- *
- * revision history
- * ----------------
- * $Log: startup.cc,v $
- * Revision 1.13  2008/01/30 22:43:33  burghart
- * Hskp.hh is now HskpControl.h
- *
- * Revision 1.12  2008/01/29 17:37:11  burghart
- * Merged rpc/include/Header.hh into rpc/include/Header.h to create a single
- * header file to be used for both C and C++ source.
- *
- * Revision 1.11  2005/08/04 22:27:37  granger
- * commit vx/hskp as copied from /net/eldora/eldora, except a few obsolete
- * (afaik) directories were removed, like hskp/src/clock/newclk and
- * hskp/src/arinc_NCAR
- *
- * Revision 1.10  2003/10/02  15:15:21  kapoor
- * Modified to work with both TOD and Bancomm function calls
- *
- * Revision 1.9  2003/09/25  17:30:51  kapoor
- * updated for bancomm clock use - change of get_time, julian day, sync_ir
- * and start_clock
- *
- * Revision 1.8  1999/09/27  15:53:59  thor
- * Switched over to new mode of handling commands.
- *
- * Revision 1.7  1996/09/12  17:21:50  craig
- * *** empty log message ***
- *
- * Revision 1.6  1996/02/09  18:29:50  craig
- * *** empty log message ***
- *
- * Revision 1.5  1994/05/20  20:37:22  craig
- * *** empty log message ***
- *
- * Revision 1.1  1992/08/19  17:27:19  craig
- * Initial revision
- *
- *
+ * 
  * description: This module initializes RPC in Housekeeping Processor.
  *              
  */
@@ -87,8 +46,8 @@ void startup(void)
   cerr.sync_with_stdio(0);
 
   Hdr = (Header *)NULL;
+  currStatus = &status;
 
-  cerr << "spawning startServer" << endl;
   if(taskSpawn("CmdLoop",9,0,12000,(FUNCPTR)startServer,0,0,0,0,0,0,0,0,0,0)
      == ERROR)
     cout << "Failed to start cmd loop!!!" << endl;;
@@ -125,7 +84,7 @@ public:
     {
         stop_flag = 1;
         result = 0;
-        std::cout << "got start command" << std::endl;
+        std::cout << "got stop command" << std::endl;
     }
 } stopMethod(&Svr);
 
@@ -151,154 +110,19 @@ public:
         // Return the CRC-32 checksum (cast into a signed int) as our result.
         // We hijack zero to use as a failure indicator.
         result = *(int*)&crc;
-        std::cout << "Got CRC value " << crc << " for file " << 
-            filename << std::endl;
+        
+        std::cout << "New header loaded, with cksum value " << crc << std::endl;
     }
 } headerMethod(&Svr);
 
 static void startServer(void)
 {
-    currStatus = &status;
     // bind to our port and listen for commands
     Svr.bindAndListen(60001);
     // Enable introspection
     Svr.enableIntrospection(true);
     // Wait for requests indefinitely
     Svr.work(-1.0);
-
-#if 0  
-    char string[20];
-
-
-  struct HskpCommand cmdBlk;
-
-  while (1)
-    {
-      int bytes = svr.recvfrom((void *)&cmdBlk,sizeof(cmdBlk.cmd),MSG_PEEK);
-
-      if ((cmdBlk.cmd == STOP) || (cmdBlk.cmd == START) ||
-   (cmdBlk.cmd == HEADER) || (cmdBlk.cmd == SEND_STATUS))
- svr.recvfrom((void *)&cmdBlk,sizeof(cmdBlk.cmd));
-      else
- {
-   bytes = svr.recvfrom((void *)&cmdBlk,sizeof(cmdBlk));
-
-   if ( bytes < sizeof(cmdBlk))
-     {
-       if (bytes < 0)
-  {
-    cerr << "udpSvr: " << strerror(errno) << endl;
-    continue;
-  }
-     }
- }
-      u_long cmd = cmdBlk.cmd;
-
-      hex(cout);
-      cout << "command received: " << cmd << endl;
-
-      if (cmd == SEND_STATUS)
- cmd = 0;
-
-      if (cmd & INIT)
- ;
-      else if (cmd == HEADER)
- {
-   //char *file = "/usr/local/vxbin/headers/current.hdr";
-          char *file = "/vxroot/headers/current.hdr";
-   int i = 0;
-
-   if (Hdr == (Header *)NULL)
-     Hdr = new Header(file);
-   else
-     {
-       if (Hdr->readFile(file))
-  {
-    cout << "Bad open of file: " << file << endl;
-    i = 1;
-  }
-     }
-   svr.setAddr((struct sockaddr_in *)svr.getAddr());
-
-   svr.sendto((void *)&i,sizeof(i));
-   continue;
- }
-      else
- {
-   /***********************************************/
-   /**** Start of code inserted by Craig Walther **/
-   /***********************************************/
-   if(cmd & START_CLOCK)  /* Has Control processor told us to
-        start clock? */
-     {
-       clkstart = 1;
-       start_clock(); 
-       currStatus->clock = 0;
-     }
-	  if(cmd & START)  /* Has control Processor told us to start? */
-	    {
-	      stop_flag = 0;
-	    }
-	  if(cmd & STOP)   /* Has control Processor told us to stop? */
-	    {
-	      stop_flag = 1;
-	    }
-	  if(cmd & RELOAD)   /* Has control Processor told us to reload? */
-	    {
-       reload_flag = 1;
-     }
-   if(cmd & REBOOT)   /* Has control Processor told us to reboot? */
-     reboot((int)BOOT_NORMAL);
-   if(cmd & GPS_START)  /* Has the control processor told us to
-      program the TANS II GPS receiver? */
-     {
-       command_gps((short)2);
-     }
-   if(cmd & IRIGB_SYNC) /* Has the control processor told us to sync
-      the time of day card to the ADS's
-      IRIG-B signal? */
-     {
-       
-       currStatus->clock = IRIGB_SYNCING;
-       char test = sync_irig();
-       if(test)          /* Were we successful? */
-  currStatus->clock = 0;
-       else
-   currStatus->clock = IRIGB_SYNC_FAILED; 
-     }
-   if(cmd & SET_TIME)  /* Has the Control Processor told us to set
-     the time? */
-     {
-       char year = cmdBlk.year;
-       char month = cmdBlk.month;
-       char date = cmdBlk.day;
-       char hour = cmdBlk.hour;
-       char minute = cmdBlk.minute;
-       char second = cmdBlk.second;
-       set_time(hour,minute,second,month,date,year); 
-
-             /******* This is for TOD card *******/
-       /* Check for leap year, update the jday_calc array if it is *
-       if((((int)(year/4))*4 == year) && (jday_calc[2] == 60))
-  {
-    for(int i = 2; i < 12; i++)
-      jday_calc[i]++;
-  }
-       */
-
-       /* Checks for leap year, updates the day of the mon array */
-       if(((int)(year/4))*4 == year)
-  day_mon_calc[1] = 29; 
-
-
-       currStatus->clock = TIME_SET_READY;
-     }
- }
-      svr.setAddr((struct sockaddr_in *)svr.getAddr());
-
-      svr.sendto((void *)currStatus,sizeof(status));
-    }
-#endif
 }
 
 
