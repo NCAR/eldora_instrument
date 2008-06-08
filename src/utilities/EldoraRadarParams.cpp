13f760f6048ba009731afd2f14459b7ef9de09cf
Charles Martin <martinc@ucar.edu>
2008-06-08 06:20:35 +0000

    Added a dump() method to print out the radar parameters.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@2206 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- src/utilities/EldoraRadarParams.cpp
+++ src/utilities/EldoraRadarParams.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "EldoraRadarParams.h"
 
 EldoraRadarParams::EldoraRadarParams()
@@ -6,21 +7,21 @@ EldoraRadarParams::EldoraRadarParams()
     for (unsigned int i = 0; i < sizeof(wave_nchips); i++)
         wave_nchips[i] = 1;
     wave_nchips[4] = 0;
-/*
-    wave_chpoff[0] = 17;
-    wave_chpoff[1] = 62;
-    wave_chpoff[2] = 107;
-    wave_chpoff[3] = 152;
-    wave_chpoff[4] = 0;
-    wave_chpoff[5] = 0;
-*/    
+    /*
+     wave_chpoff[0] = 17;
+     wave_chpoff[1] = 62;
+     wave_chpoff[2] = 107;
+     wave_chpoff[3] = 152;
+     wave_chpoff[4] = 0;
+     wave_chpoff[5] = 0;
+     */
 
     wave_chpoff[0] = 17;
-        wave_chpoff[1] = 77;
-        wave_chpoff[2] = 137;
-        wave_chpoff[3] = 197;
-        wave_chpoff[4] = 0;
-        wave_chpoff[5] = 0;
+    wave_chpoff[1] = 77;
+    wave_chpoff[2] = 137;
+    wave_chpoff[3] = 197;
+    wave_chpoff[4] = 0;
+    wave_chpoff[5] = 0;
 
     for (unsigned int i = 0; i < sizeof(wave_chpwid); i++)
         //wave_chpwid[i] = 45;
@@ -32,24 +33,23 @@ EldoraRadarParams::EldoraRadarParams()
     for (unsigned int i = 0; i < sizeof(wave_ngates); i++)
         wave_ngates[i] = 400;
     wave_ngates[5] = 0;
-    
 
-/*
-    wave_gate1[0] = 75;
-    wave_gate1[1] = 45;
+    /*
+     wave_gate1[0] = 75;
+     wave_gate1[1] = 45;
 
-    wave_gate2[0] = 75;
-    wave_gate2[1] = 45;
+     wave_gate2[0] = 75;
+     wave_gate2[1] = 45;
 
-    wave_gate3[0] = 75;
-    wave_gate3[1] = 45;
+     wave_gate3[0] = 75;
+     wave_gate3[1] = 45;
 
-    wave_gate4[0] = 75;
-    wave_gate4[1] = 45;
+     wave_gate4[0] = 75;
+     wave_gate4[1] = 45;
 
-    wave_gate5[0] = 75;
-    wave_gate5[1] = 45;
-*/
+     wave_gate5[0] = 75;
+     wave_gate5[1] = 45;
+     */
 
     wave_gate1[0] = 90;
     wave_gate1[1] = 60;
@@ -81,13 +81,13 @@ EldoraRadarParams::EldoraRadarParams()
     radd_nfreq = 4;
     //radd_nipp = 1;
     radd_nipp = 2;
-        
+
     radd_freq1 = 9.709332;
     radd_freq2 = 9.700000;
     radd_freq3 = 9.690667;
     radd_freq4 = 9.718666;
     radd_freq5 = -999.0;
-    
+
     //radd_ipp1 = 0.33333333;
     radd_ipp1 = 0.5;
     //radd_ipp2 = -999.0;
@@ -106,24 +106,123 @@ EldoraRadarParams::EldoraRadarParams()
     frib_xgain = 22.1;
     frib_tsgat = 68;
     frib_frqgat= 10;
-    
+
     // scale and bias for the DDS data compression
-    parm_dbz_scale= 10.0; 
+    parm_dbz_scale= 10.0;
     parm_dbz_bias = 350.0;
-    
+
     parm_sw_scale = 400.0;
-    parm_sw_bias  = 0.0;
-    
+    parm_sw_bias = 0.0;
+
     parm_ncp_scale= 1024.0;
     parm_ncp_bias = 0.0;
-    
+
     parm_vs_scale = 400.0;
-    parm_vs_bias  = 0.0;
-     
+    parm_vs_bias = 0.0;
+
     parm_vl_scale = 400.0;
-    parm_vl_bias  = 0.0;
-    
+    parm_vl_bias = 0.0;
+
     parm_vr_scale = 400.0;
-    parm_vr_bias  = 0.0;
+    parm_vr_bias = 0.0;
+
+}
+
+/////////////////////////////////////////////////////////////////////////
+void EldoraRadarParams::dump()
+{
+
+    std::cout << "wave_nchips: ";
+    for (int i = 0; i < 6; i++)
+        std::cout << wave_nchips[i] << " ";
+    std::cout << "\n";
+
+    std::cout << "wave_chpoff: ";
+    for (int i = 0; i < 6; i++)
+        std::cout << wave_chpoff[i] << " ";
+    std::cout << "\n";
+
+    std::cout << "wave_chpwid: ";
+    for (int i = 0; i < 6; i++)
+        std::cout << wave_chpwid[i] << " ";
+    std::cout << "\n";
+
+    std::cout << "wave_ngates: ";
+    for (int i = 0; i < 5; i++)
+        std::cout << wave_ngates[i] << " ";
+    std::cout << "\n";
+
+    std::cout << "wave_gate1: ";
+    for (int i = 0; i < 2; i++)
+        std::cout << wave_gate1[i] << " ";
+    std::cout << "\n";
+
+    std::cout << "wave_gate2: ";
+    for (int i = 0; i < 2; i++)
+        std::cout << wave_gate2[i] << " ";
+    std::cout << "\n";
+
+    std::cout << "wave_gate3: ";
+    for (int i = 0; i < 2; i++)
+        std::cout << wave_gate3[i] << " ";
+    std::cout << "\n";
+
+    std::cout << "wave_gate4: ";
+    for (int i = 0; i < 2; i++)
+        std::cout << wave_gate4[i] << " ";
+    std::cout << "\n";
+
+    std::cout << "wave_gate5: ";
+    for (int i = 0; i < 2; i++)
+        std::cout << wave_gate5[i] << " ";
+    std::cout << "\n";
+
+    std::cout << "wave_msrep: " << wave_msrep << "\n";
+    std::cout << "wave_seqrep: "<< wave_seqrep << "\n";
+
+    std::cout << "radd_name:";
+    for (int i = 0; i < 4; i++)
+        std::cout << radd_name[i];
+    std::cout << "\n";
+
+    std::cout << "radd_const: " << radd_const << "\n";
+    std::cout << "radd_noipow: " << radd_noipow << "\n";
+    std::cout << "radd_unvel: " << radd_unvel << "\n";
+    std::cout << "radd_unrng: " << radd_unrng << "\n";
+    std::cout << "radd_nfreq: " << radd_nfreq << "\n";
+    std::cout << "radd_nipp: " << radd_nipp << "\n";
+    std::cout << "radd_freq1: " << radd_freq1 << "\n";
+    std::cout << "radd_freq2: " << radd_freq2 << "\n";
+    std::cout << "radd_freq3: " << radd_freq3 << "\n";
+    std::cout << "radd_freq4: " << radd_freq4 << "\n";
+    std::cout << "radd_freq5: " << radd_freq5 << "\n";
+    std::cout << "radd_ipp1: " << radd_ipp1 << "\n";
+    std::cout << "radd_ipp2: " << radd_ipp2 << "\n";
+    std::cout << "radd_ipp3: " << radd_ipp3 << "\n";
+    std::cout << "radd_ipp4: " << radd_ipp4 << "\n";
+    std::cout << "radd_ipp5: " << radd_ipp5 << "\n";
+
+    std::cout << "frib_rxgain: ";
+    for (int i = 0; i < 5; i++)
+        std::cout <<frib_rxgain[i] << " ";
+    std::cout << "\n";
+
+    std::cout << "frib_xgain: " << frib_xgain << "\n";
+    std::cout << "frib_lnalos: " << frib_lnalos << "\n";
+    std::cout << "frib_tsgat: " << frib_tsgat << "\n";
+    std::cout << "frib_frqgat: " << frib_frqgat << "\n";
+
+    std::cout << "parm_dbz_scale: " << parm_dbz_scale << "\n";
+    std::cout << "parm_dbz_bias: " << parm_dbz_bias << "\n";
+    std::cout << "parm_sw_scale: " << parm_sw_scale << "\n";
+    std::cout << "parm_sw_bias: " << parm_sw_bias << "\n";
+    std::cout << "parm_ncp_scale: " << parm_ncp_scale << "\n";
+    std::cout << "parm_ncp_bias: " << parm_ncp_bias << "\n";
+    std::cout << "parm_vs_scale: " << parm_vs_scale << "\n";
+    std::cout << "parm_vs_bias: " << parm_vs_bias << "\n";
+    std::cout << "parm_vl_scale: " << parm_vl_scale << "\n";
+    std::cout << "parm_vl_bias: " << parm_vl_bias << "\n";
+    std::cout << "parm_vr_scale: " << parm_vr_scale << "\n";
+    std::cout << "parm_vr_bias: " << parm_vr_bias << "\n";
 
 }
--- src/utilities/EldoraRadarParams.h
+++ src/utilities/EldoraRadarParams.h
@@ -8,63 +8,67 @@
 /// headermaker, which assigns keys to each header parameter.
 struct EldoraRadarParams
 {
-    EldoraRadarParams();
+        EldoraRadarParams();
+
+        // dump the fields to std::cout 
+        void dump();
+
         // fields from the header waveform block (WAVE)
-        int    wave_nchips[6];///< waveform number of chips
-        int    wave_chpoff[6];///< waveform chip offset (60 Mhz counts)
-        int    wave_chpwid[6];///< waveform chip width (60 Mhz counts)
-        int    wave_ngates[5];///< waveform number of sampled gates
-        int    wave_gate1[2]; ///< waveform gate distance 1 (60 Mhz counts)
-        int    wave_gate2[2]; ///< waveform gate distance 2 (60 Mhz counts)
-        int    wave_gate3[2]; ///< waveform gate distance 3 (60 Mhz counts)
-        int    wave_gate4[2]; ///< waveform gate distance 4 (60 Mhz counts)
-        int    wave_gate5[2]; ///< waveform gate distance 5 (60 Mhz counts)
-        double wave_msrep;    ///< waveform number of milliseconds per repeat
-        int    wave_seqrep;   ///< waveform number of repeat sequences in a dwell time
-        
+        int wave_nchips[6];///< waveform number of chips
+        int wave_chpoff[6];///< waveform chip offset (60 Mhz counts)
+        int wave_chpwid[6];///< waveform chip width (60 Mhz counts)
+        int wave_ngates[5];///< waveform number of sampled gates
+        int wave_gate1[2]; ///< waveform gate distance 1 (60 Mhz counts)
+        int wave_gate2[2]; ///< waveform gate distance 2 (60 Mhz counts)
+        int wave_gate3[2]; ///< waveform gate distance 3 (60 Mhz counts)
+        int wave_gate4[2]; ///< waveform gate distance 4 (60 Mhz counts)
+        int wave_gate5[2]; ///< waveform gate distance 5 (60 Mhz counts)
+        double wave_msrep; ///< waveform number of milliseconds per repeat
+        int wave_seqrep; ///< waveform number of repeat sequences in a dwell time
+
         // fields from the header radar descriptor block (RADD)
-        char   radd_name[4];  ///< radar name
-        double radd_const;    ///< radar constant (db)
-        double radd_noipow;   ///< noise power (dbm)
-        double radd_unvel;    ///< radar effective unambiguous velocity
-        double radd_unrng;    ///< radar effective unambiguous range
-        int    radd_nfreq;    ///< radar number of tx frequencies
-        int    radd_nipp;     ///< radar number of diff ipps
-        double radd_freq1;    ///< radar feequency 1 (GHz)
-        double radd_freq2;    ///< radar feequency 2 (GHz)
-        double radd_freq3;    ///< radar feequency 3 (GHz)
-        double radd_freq4;    ///< radar feequency 4 (GHz)
-        double radd_freq5;    ///< radar feequency 5 (GHz)   
-        double radd_ipp1;     ///< radar interpulse period 1 (ms)
-        double radd_ipp2;     ///< radar interpulse period 2 (ms)
-        double radd_ipp3;     ///< radar interpulse period 3 (ms)
-        double radd_ipp4;     ///< radar interpulse period 4 (ms)
-        double radd_ipp5;     ///< radar interpulse period 5 (ms)
-        
+        char radd_name[4]; ///< radar name
+        double radd_const; ///< radar constant (db)
+        double radd_noipow; ///< noise power (dbm)
+        double radd_unvel; ///< radar effective unambiguous velocity
+        double radd_unrng; ///< radar effective unambiguous range
+        int radd_nfreq; ///< radar number of tx frequencies
+        int radd_nipp; ///< radar number of diff ipps
+        double radd_freq1; ///< radar feequency 1 (GHz)
+        double radd_freq2; ///< radar feequency 2 (GHz)
+        double radd_freq3; ///< radar feequency 3 (GHz)
+        double radd_freq4; ///< radar feequency 4 (GHz)
+        double radd_freq5; ///< radar feequency 5 (GHz)   
+        double radd_ipp1; ///< radar interpulse period 1 (ms)
+        double radd_ipp2; ///< radar interpulse period 2 (ms)
+        double radd_ipp3; ///< radar interpulse period 3 (ms)
+        double radd_ipp4; ///< radar interpulse period 4 (ms)
+        double radd_ipp5; ///< radar interpulse period 5 (ms)
+
         // fields from the header field radar info block (FRIB)
         double frib_rxgain[5];///< field radar receiver gain by channel (db)
-        double frib_xgain;    ///< field radar x band gain (db)
-        double frib_lnalos;   ///< field radar losses between antenna and lna (db)
-        int    frib_tsgat;    ///< field radar time series gate
-        int    frib_frqgat;   ///< field radar number of time series gate (this field hijacked from independent frequency gate parameter) 
-        
+        double frib_xgain; ///< field radar x band gain (db)
+        double frib_lnalos; ///< field radar losses between antenna and lna (db)
+        int frib_tsgat; ///< field radar time series gate
+        int frib_frqgat; ///< field radar number of time series gate (this field hijacked from independent frequency gate parameter) 
+
         double parm_dbz_scale;///< scale for dbz
         double parm_dbz_bias; ///< bias for dbz
-        
+
         double parm_sw_scale; ///< scale for sw
-        double parm_sw_bias;  ///< bias for sw
-        
+        double parm_sw_bias; ///< bias for sw
+
         double parm_ncp_scale;///< scale for ncp
         double parm_ncp_bias; ///< bias for ncp
-        
+
         double parm_vs_scale; ///< scale for vs
-        double parm_vs_bias;  ///< bias for vs
-        
+        double parm_vs_bias; ///< bias for vs
+
         double parm_vl_scale; ///< scale for vl
-        double parm_vl_bias;  ///< bias for vl
-        
+        double parm_vl_bias; ///< bias for vl
+
         double parm_vr_scale; ///< scale for vr
-        double parm_vr_bias;  ///< bias for vr
+        double parm_vr_bias; ///< bias for vr
 
 };
 
