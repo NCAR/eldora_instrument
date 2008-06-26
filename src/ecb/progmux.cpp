a06bcb3e41f8432c9cc36302f73b1c1873f967b0
Charles Martin <martinc@ucar.edu>
2008-06-26 14:22:08 +0000

    add command line mux programming app
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@2376 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

---
+++ src/ecb/progmux.cpp
@@ -0,0 +1,103 @@
+#include <string>
+#include <iostream>
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+#include "trigmux.h"
+
+void parseOptions(int argc,
+                  char* argv[],
+                  std::string& ip,
+                  int& port,
+                  bool& aftRadar,
+                  int& channel)
+{
+
+    std::string radar;
+    char choice;
+    bool err = false;
+
+    // get the options
+    po::options_description descripts("Options");
+
+    descripts.add_options() 
+    ("help", "describe options") 
+    ("ip", po::value<std::string>(&ip), "IP address")
+    ("port", po::value<int>(&port), "Port number")
+    ("radar", po::value<std::string>(&radar), "radar (forward|aft)")
+    ("channel", po::value<char>(&choice), "channel 1|2|3|4|5|P|T")
+    ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, descripts), vm);
+    po::notify(vm);
+
+    // make sure required parameters are specified
+    if (!vm.count("ip")
+    || !vm.count("port")
+    || !vm.count("radar")
+    || !vm.count("channel") ) {
+        std::cerr << descripts << "\n";
+        exit(1);
+    }
+
+    // do some bounds checking
+    if (!radar.compare("forward")) {
+        aftRadar = false;
+    } else {
+        if (!radar.compare("aft")) {
+            aftRadar = true;
+        } else {
+            std::cerr << "radar must be forward|aft\n";
+            err = true;
+        }
+    }
+
+    // decode and convert channel number to 0-13
+    switch (choice) {
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+        channel = choice - '1';
+        break;
+    case 'P':
+        channel = 5;
+        break;
+    case 'T':
+        channel = 6;
+        break;
+    default:
+        std::cerr << "channel must be 1|2|3|4|5|P|T\n";
+        err = true;
+    }
+    channel = aftRadar? (channel+7):channel;
+    
+    if (err || vm.count("help")) {
+        std::cerr << descripts << "\n";
+        exit(1);
+    }
+
+}
+
+int main(int argc,
+         char** argv)
+{
+    std::string ip;   // ip address
+    int port;         // ip port
+    bool aftRadar;    // false if forward radar, true if aft
+    int channel;      // channel 0-13
+    
+    parseOptions(argc, argv,
+              ip,
+              port,
+              aftRadar,
+              channel);
+    
+    // our stepped attenuator
+    TrigMux mux(ip, port, aftRadar? 1:0);
+    
+    mux.SetMux(channel, 2);
+
+}
