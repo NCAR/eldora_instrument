483736a4680381d3155dd6b770474ce8a1cad3da
Charles Martin <martinc@ucar.edu>
2008-06-26 15:42:53 +0000

    Add function to grab frequencies from an EldoraHeader.
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@2382 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- src/eldoragui/EldoraUtil.py
+++ src/eldoragui/EldoraUtil.py
@@ -32,3 +32,43 @@ class DiskStats:
     
     def numDisks(self):
         return self.num
+       
+def RadarFreqs(header):
+	''' Extract the forward and aft radar frequencies from the header
+	param header - An EldoraHeader
+	returns - A dictionary containing the radar frequencies, in GHz:
+	   'forward': [f1,f2,f3,f4,f5]
+	   'aft': [f1,f2,f3,f4,f5]
+	'''
+	result = dict()
+	# go through the blocks and find the radd blocks
+	radarChoice = None
+	for block in header:
+		# examine the RADD blocks
+		if block.type == 'RADD':
+			freqs = list(range(0,5))
+			# go through all of the fields. Find the
+			# NAME field so that we can select forward or aft
+			# Find the FREQn fields and extract the frequencies.
+			for field in block:
+				if field[0] == 'NAME':
+					if field[2] == 'FORE':
+						radarChoice = 'forward'
+					else:
+						radarChoice = 'aft'
+				if field[0] == 'FREQ1':
+					freqs[0] = field[2]
+				elif field[0] == 'FREQ2':
+					freqs[1] = field[2]
+				elif field[0] == 'FREQ3':
+					freqs[2] = field[2]
+				elif field[0] == 'FREQ4':
+					freqs[3] = field[2]
+				elif field[0] == 'FREQ5':
+					freqs[4] = field[2]
+			# save the frquencies for the selected radar
+			result[radarChoice] = freqs
+				
+	return result
+
+	
