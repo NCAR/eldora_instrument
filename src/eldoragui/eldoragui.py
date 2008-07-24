9118e7fb1068947a7200bbd03922e6b734a442c1
Chris Burghart <burghart@ucar.edu>
2008-07-24 23:33:50 +0000

    Sleep between starting each archiver, so that they are not trying to do their configuration at the same time, as that seems to sometimes lead to overwriting a good custom configuration file with a new default one...
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@2556 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- src/eldoragui/eldoragui.py
+++ src/eldoragui/eldoragui.py
@@ -480,8 +480,8 @@ def startArchivers():
         proc = ourProcesses[key]
         QObject.connect(proc, SIGNAL("text"), main.logText)
         proc.start()
-    # Give them a moment to actually start
-    time.sleep(1)
+        # Give it a moment to actually start before we start another one
+        time.sleep(1)
 
 ####################################################################################
 def sendHeader():
