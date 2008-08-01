f986b7cd5d3fadf7a2552f91cbf501861aa5cbc6
Joe VanAndel <vanandel@ucar.edu>
2008-08-01 14:53:34 +0000

    added StrMapDoubleIter typedef
    git-svn-id: http://svn.eol.ucar.edu/svn/eldora/trunk@2599 15f24ed5-0439-0410-88a4-bd1d0fc06cdd

--- src/utilities/EldoraTypes.h
+++ src/utilities/EldoraTypes.h
@@ -7,6 +7,7 @@
 typedef std::vector<std::string> StringVec;
 typedef std::vector<std::string>::iterator StringVecIter;
 typedef std::map<std::string, double> StrMapDouble;
+typedef std::map<std::string, double>::iterator StrMapDoubleIter;
 
 #include "netcdf-3/netcdfcpp.h"
 typedef std::map<std::string,NcVar *> NcVarMap;
